2dcaaed68244aa89cc48e5a9413f73304e0d6896
Mike Perry <mikeperry-git@torproject.org>
2013-09-10 16:21:47 -0700

    Bug 2176: Rebrand Firefox to TorBrowser
    See also Bugs #5194, #7187, #8115, #8219.
    
    This patch does some basic renaming of Firefox to TorBrowser. The rest of the
    branding is done by images and icons.
    
    Also fix bug 27905.
    
    Bug 25702: Update Tor Browser icon to follow design guidelines
    
    - Updated all of the branding in /browser/branding/official with new 'stable'
    icon series.
    - Updated /extensions/onboarding/content/img/tor-watermark.png with new icon and
    add the source svg in the same directory
    - Copied /browser/branding/official over /browser/branding/nightly and the new
    /browser/branding/alpha directories. Replaced content with 'nightly' and
    'alpha' icon series.
    Updated VisualElements_70.png and VisualElements_150.png with updated icons in
    each branding directory (fixes #22654)
    - Updated firefox.VisualElementsManfiest.xml with updated colors in each
    branding directory
    - Added firefox.svg to each branding directory from which all the other icons
    are derived (apart from document.icns and document.ico)
    - Added default256.png and default512.png icons
    - Updated aboutTBUpdate.css to point to branding-aware icon128.png and removed
    original icon
    - Use the Tor Browser icon within devtools/client/themes/images/.
    
    Bug 30631: Blurry Tor Browser icon on macOS app switcher
    
    It would seem the png2icns tool does not generate correct icns files and
    so on macOS the larger icons were missing resulting in blurry icons in
    the OS chrome. Regenerated the padded icons in a macOS VM using
    iconutil.
    
    Bug 28196: preparations for using torbutton tor-browser-brand.ftl
    
    A small change to Fluent FileSource class is required so that we
    can register a new source without its supported locales being
    counted as available locales for the browser.
    
    Bug 31803: Replaced about:debugging logo with flat version
    
    Bug 21724: Make Firefox and Tor Browser distinct macOS apps
    
    When macOS opens a document or selects a default browser, it sometimes
    uses the CFBundleSignature. Changing from the Firefox MOZB signature to
    a different signature TORB allows macOS to distinguish between Firefox
    and Tor Browser.
    
    Bug 32092: Fix Tor Browser Support link in preferences
    
    For bug 40562, we moved onionPattern* from bug 27476 to here, as
    about:tor needs these files but it is included earlier.
    
    Bug 41278: Create Tor Browser styled pdf logo similar to the vanilla Firefox one
    
    Bug 42088: New application icons (used in-app and on linux).
    
    Bug 42087: New application icons (windows).

--- browser/base/content/utilityOverlay.js
+++ browser/base/content/utilityOverlay.js
@@ -590,6 +590,9 @@ function makeURLAbsolute(aBase, aUrl) {
 }
 
 function getHelpLinkURL(aHelpTopic) {
+  if (aHelpTopic === "firefox-help" || aHelpTopic === "firefox-osxkey") {
+    return "about:manual";
+  }
   var url = Services.urlFormatter.formatURLPref("app.support.baseURL");
   return url + aHelpTopic;
 }
