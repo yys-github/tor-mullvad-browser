bcb0b432c778348799bdbef2ffc7091393581322
Mike Perry <mikeperry-git@torproject.org>
2013-09-10 18:20:43 -0700

    Firefox preference overrides.

--- eslint-ignores.config.mjs
+++ eslint-ignores.config.mjs
@@ -307,4 +307,6 @@ export default [
   "dom/base/test/jsmodules/import_circular_1.mjs",
   "dom/base/test/jsmodules/importmaps/multiple/import_circular.mjs",
   "dom/base/test/jsmodules/importmaps/multiple/import_circular_1.mjs",
+
+  "browser/app/profile/001-base-profile.js",
 ];
