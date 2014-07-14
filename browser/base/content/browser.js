d2e8b008fe1770a237ba62c8b2fe18899f5585b7
Arthur Edelstein <arthuredelstein@gmail.com>
2014-07-14 00:40:13 -0700

    TB 3455: Add DomainIsolator, for isolating circuit by domain.
    Add an XPCOM component that registers a ProtocolProxyChannelFilter
    which sets the username/password for each web request according to
    url bar domain.
    
    Bug 9442: Add New Circuit button
    
    Bug 13766: Set a 10 minute circuit dirty timeout for the catch-all circ.
    
    Bug 19206: Include a 128 bit random tag as part of the domain isolator nonce.
    
    Bug 19206: Clear out the domain isolator state on `New Identity`.
    
    Bug 21201.2: Isolate by firstPartyDomain from OriginAttributes
    
    Bug 21745: Fix handling of catch-all circuit
    
    Bug 41741: Refactor the domain isolator and new circuit

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -105,6 +105,7 @@ ChromeUtils.defineESModuleGetters(this, {
   ToolbarDropHandler:
     "moz-src:///browser/components/customizableui/ToolbarDropHandler.sys.mjs",
   ToolbarIconColor: "moz-src:///browser/themes/ToolbarIconColor.sys.mjs",
+  TorDomainIsolator: "resource://gre/modules/TorDomainIsolator.sys.mjs",
   TorUIUtils: "resource:///modules/TorUIUtils.sys.mjs",
   TranslationsParent: "resource://gre/actors/TranslationsParent.sys.mjs",
   UITour: "moz-src:///browser/components/uitour/UITour.sys.mjs",
