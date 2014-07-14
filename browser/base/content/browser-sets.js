616f112082f649ecdbbcc6c1218a5495c6159728
Arthur Edelstein <arthuredelstein@gmail.com>
2014-07-14 00:40:13 -0700

    TB 3455: Add DomainIsolator, for isolating circuit by domain.
    Add an XPCOM component that registers a ProtocolProxyChannelFilter
    which sets the username/password for each web request according to
    url bar domain.
    
    Bug 9442: Add New Circuit button
    
    Bug 13766: Set a 10 minute circuit dirty timeout for the catch-all circ.
    
    Bug 19206: Include a 128 bit random tag as part of the domain isolator nonce.
    
    Bug 19206: Clear out the domain isolator state on `New Identity`.
    
    Bug 21201.2: Isolate by firstPartyDomain from OriginAttributes
    
    Bug 21745: Fix handling of catch-all circuit
    
    Bug 41741: Refactor the domain isolator and new circuit

--- browser/base/content/browser-sets.js
+++ browser/base/content/browser-sets.js
@@ -285,6 +285,9 @@ document.addEventListener(
           case "cmd_newIdentity":
             NewIdentityButton.onCommand(event);
             break;
+          case "cmd_newCircuit":
+            TorDomainIsolator.newCircuitForBrowser(gBrowser.selectedBrowser);
+            break;
         }
       });
 
