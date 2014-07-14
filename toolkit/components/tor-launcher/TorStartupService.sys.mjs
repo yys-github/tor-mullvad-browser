51894294cb9e87ce26933d3b0866dfbb83df4ba9
Arthur Edelstein <arthuredelstein@gmail.com>
2014-07-14 00:40:13 -0700

    Bug 3455: Add DomainIsolator, for isolating circuit by domain.
    Add an XPCOM component that registers a ProtocolProxyChannelFilter
    which sets the username/password for each web request according to
    url bar domain.
    
    Bug 9442: Add New Circuit button
    
    Bug 13766: Set a 10 minute circuit dirty timeout for the catch-all circ.
    
    Bug 19206: Include a 128 bit random tag as part of the domain isolator nonce.
    
    Bug 19206: Clear out the domain isolator state on `New Identity`.
    
    Bug 21201.2: Isolate by firstPartyDomain from OriginAttributes
    
    Bug 21745: Fix handling of catch-all circuit
    
    Bug 41741: Refactor the domain isolator and new circuit

--- toolkit/components/tor-launcher/TorStartupService.sys.mjs
+++ toolkit/components/tor-launcher/TorStartupService.sys.mjs
@@ -4,6 +4,7 @@ const lazy = {};
 // loading
 ChromeUtils.defineESModuleGetters(lazy, {
   TorConnect: "resource://gre/modules/TorConnect.sys.mjs",
+  TorDomainIsolator: "resource://gre/modules/TorDomainIsolator.sys.mjs",
   TorLauncherUtil: "resource://gre/modules/TorLauncherUtil.sys.mjs",
   TorProviderBuilder: "resource://gre/modules/TorProviderBuilder.sys.mjs",
   TorSettings: "resource://gre/modules/TorSettings.sys.mjs",
@@ -42,11 +43,15 @@ export class TorStartupService {
 
     lazy.TorConnect.init();
 
+    lazy.TorDomainIsolator.init();
+
     gInited = true;
   }
 
   #uninit() {
     Services.obs.removeObserver(this, BrowserTopics.QuitApplicationGranted);
+
+    lazy.TorDomainIsolator.uninit();
 
     lazy.TorProviderBuilder.uninit();
     lazy.TorLauncherUtil.cleanupTempDirectories();
