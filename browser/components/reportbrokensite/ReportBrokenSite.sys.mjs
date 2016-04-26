d1e46c1396d9e5d2e07d4c4abdc272b22166277b
Arthur Edelstein <arthuredelstein@gmail.com>
2016-04-26 15:59:36 -0700

    BB 18905: Hide unwanted items from help menu
    Bug 25660: Remove the "New Private Window" option

--- browser/components/reportbrokensite/ReportBrokenSite.sys.mjs
+++ browser/components/reportbrokensite/ReportBrokenSite.sys.mjs
@@ -704,7 +704,8 @@ export var ReportBrokenSite = new (class ReportBrokenSite {
     // Altering the disabled attribute on the command does not propagate
     // the change to the related menuitems (see bug 805653), so we change them all.
     const cmd = document.getElementById("cmd_reportBrokenSite");
-    const allowedByPolicy = Services.policies.isAllowed("feedbackCommands");
+    // Hide the items in base-browser. tor-browser#43903.
+    const allowedByPolicy = false;
     cmd.toggleAttribute("hidden", !allowedByPolicy);
     const app = document.documentGlobal.PanelMultiView.getViewNode(
       document,
