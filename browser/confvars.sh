22f3588aeb067e2ca5b73aecdd969f3c320baee6
Kathy Brade <brade@pearlcrescent.com>
2017-01-13 11:40:24 -0500

    BB 4234: Use the Firefox Update Process for Base Browser.
    Windows: disable "runas" code path in updater (15201).
    Windows: avoid writing to the registry (16236).
    Also includes fixes for tickets 13047, 13301, 13356, 13594, 15406,
      16014, 16909, 24476, and 25909.
    
    Also fix bug 27221: purge the startup cache if the Base Browser
    version changed (even if the Firefox version and build ID did
    not change), e.g., after a minor Base Browser update.
    
    Also fix 32616: Disable GetSecureOutputDirectoryPath() functionality.
    
    Bug 26048: potentially confusing "restart to update" message
    
    Within the update doorhanger, remove the misleading message that mentions
    that windows will be restored after an update is applied, and replace the
    "Restart and Restore" button label with an existing
    "Restart to update Tor Browser" string.
    
    Bug 28885: notify users that update is downloading
    
    Add a "Downloading Base Browser update" item which appears in the
    hamburger (app) menu while the update service is downloading a MAR
    file. Before this change, the browser did not indicate to the user
    that an update was in progress, which is especially confusing in
    Tor Browser because downloads often take some time. If the user
    clicks on the new menu item, the about dialog is opened to allow
    the user to see download progress.
    
    As part of this fix, the update service was changed to always show
    update-related messages in the hamburger menu, even if the update
    was started in the foreground via the about dialog or via the
    "Check for Tor Browser Update" toolbar menu item. This change is
    consistent with the Tor Browser goal of making sure users are
    informed about the update process.
    
    Removed #28885 parts of this patch which have been uplifted to Firefox.

--- browser/confvars.sh
+++ browser/confvars.sh
@@ -11,3 +11,18 @@
 # For the mozilla-aurora repository, use "aurora".
 MOZ_BRANDING_DIRECTORY=browser/branding/unofficial
 MOZ_OFFICIAL_BRANDING_DIRECTORY=browser/branding/official
+
+# ACCEPTED_MAR_CHANNEL_IDS should usually be the same as the value MAR_CHANNEL_ID.
+# If more than one ID is needed, then you should use a comma separated list
+# of values.
+# The MAR_CHANNEL_ID must not contain the following 3 characters: ",\t "
+if test "$MOZ_UPDATE_CHANNEL" = "alpha"; then
+  ACCEPTED_MAR_CHANNEL_IDS=basebrowser-torproject-alpha
+  MAR_CHANNEL_ID=basebrowser-torproject-alpha
+elif test "$MOZ_UPDATE_CHANNEL" = "nightly"; then
+  ACCEPTED_MAR_CHANNEL_IDS=basebrowser-torproject-nightly
+  MAR_CHANNEL_ID=basebrowser-torproject-nightly
+else
+  ACCEPTED_MAR_CHANNEL_IDS=basebrowser-torproject-release
+  MAR_CHANNEL_ID=basebrowser-torproject-release
+fi
