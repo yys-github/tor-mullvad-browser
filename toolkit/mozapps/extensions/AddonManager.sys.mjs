5c6c36af01b1fbccfc97b44179c958c3f80a96d7
Kathy Brade <brade@pearlcrescent.com>
2017-01-13 11:40:24 -0500

    BB 4234: Use the Firefox Update Process for Base Browser.
    Windows: disable "runas" code path in updater (15201).
    Windows: avoid writing to the registry (16236).
    Also includes fixes for tickets 13047, 13301, 13356, 13594, 15406,
      16014, 16909, 24476, and 25909.
    
    Also fix bug 27221: purge the startup cache if the Base Browser
    version changed (even if the Firefox version and build ID did
    not change), e.g., after a minor Base Browser update.
    
    Also fix 32616: Disable GetSecureOutputDirectoryPath() functionality.
    
    Bug 26048: potentially confusing "restart to update" message
    
    Within the update doorhanger, remove the misleading message that mentions
    that windows will be restored after an update is applied, and replace the
    "Restart and Restore" button label with an existing
    "Restart to update Tor Browser" string.
    
    Bug 28885: notify users that update is downloading
    
    Add a "Downloading Base Browser update" item which appears in the
    hamburger (app) menu while the update service is downloading a MAR
    file. Before this change, the browser did not indicate to the user
    that an update was in progress, which is especially confusing in
    Tor Browser because downloads often take some time. If the user
    clicks on the new menu item, the about dialog is opened to allow
    the user to see download progress.
    
    As part of this fix, the update service was changed to always show
    update-related messages in the hamburger menu, even if the update
    was started in the foreground via the about dialog or via the
    "Check for Tor Browser Update" toolbar menu item. This change is
    consistent with the Tor Browser goal of making sure users are
    informed about the update process.
    
    Removed #28885 parts of this patch which have been uplifted to Firefox.

--- toolkit/mozapps/extensions/AddonManager.sys.mjs
+++ toolkit/mozapps/extensions/AddonManager.sys.mjs
@@ -40,6 +40,7 @@ const PREF_REMOTESETTINGS_DISABLED = "extensions.remoteSettings.disabled";
 const PREF_USE_REMOTE = "extensions.webextensions.remote";
 const PREF_AMTELEMETRY_ADDONS_BUILDER =
   "extensions.telemetry.EnvironmentAddonBuilder";
+const PREF_EM_LAST_FORK_VERSION = "extensions.lastBaseBrowserVersion";
 
 const PREF_MIN_WEBEXT_PLATFORM_VERSION =
   "extensions.webExtensionsMinPlatformVersion";
@@ -652,6 +653,24 @@ var AddonManagerInternal = {
         Services.prefs.setIntPref(
           PREF_BLOCKLIST_PINGCOUNTVERSION,
           appChanged === undefined ? 0 : -1
+        );
+      }
+
+      // To ensure that extension and plugin code gets a chance to run after
+      // each browser update, set appChanged = true when BASE_BROWSER_VERSION
+      // has changed even if the Mozilla app version has not changed.
+      const forkChanged =
+        AppConstants.BASE_BROWSER_VERSION !==
+        Services.prefs.getCharPref(PREF_EM_LAST_FORK_VERSION, "");
+      if (forkChanged) {
+        // appChanged could be undefined (in case of a new profile)
+        if (appChanged === false) {
+          appChanged = true;
+        }
+
+        Services.prefs.setCharPref(
+          PREF_EM_LAST_FORK_VERSION,
+          AppConstants.BASE_BROWSER_VERSION
         );
       }
 
