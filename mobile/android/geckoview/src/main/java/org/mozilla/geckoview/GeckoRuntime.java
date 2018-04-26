2022315d5ff537c4099a313a0b90f1aa704c1fa7
Matthew Finkel <Matthew.Finkel@gmail.com>
2018-04-26 22:22:51 +0000

    TB 25741: TBA: Disable GeckoNetworkManager
    The browser should not need information related to the network
    interface or network state, tor should take care of that.

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -178,7 +178,11 @@ public final class GeckoRuntime implements Parcelable {
       GeckoAppShell.resumeLocation();
       // Monitor network status and send change notifications to Gecko
       // while active.
-      GeckoNetworkManager.getInstance().start(GeckoAppShell.getApplicationContext());
+      if (!BuildConfig.TOR_BROWSER) {
+        GeckoNetworkManager.getInstance().start(GeckoAppShell.getApplicationContext());
+      } else {
+        Log.d(LOGTAG, "Tor Browser: skip GeckoNetworkManager startup");
+      }
 
       // Set settings that may have changed between last app opening
       GeckoAppShell.setIs24HourFormat(
@@ -192,7 +196,9 @@ public final class GeckoRuntime implements Parcelable {
       // Pause listening for locations when in background
       GeckoAppShell.pauseLocation();
       // Stop monitoring network status while inactive.
-      GeckoNetworkManager.getInstance().stop();
+      if (!BuildConfig.TOR_BROWSER) {
+        GeckoNetworkManager.getInstance().stop();
+      }
       GeckoThread.onPause();
     }
   }
