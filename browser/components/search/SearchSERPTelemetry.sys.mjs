1a6e02a9e3b6fcb498228c3d22d49f663315ab74
Alex Catarineu <acat@torproject.org>
2019-10-16 23:01:12 +0200

    BB 31740: Remove some unnecessary RemoteSettings instances
    More concretely, SearchService.jsm 'hijack-blocklists' and
    url-classifier-skip-urls.
    
    Avoid creating instance for 'anti-tracking-url-decoration'.
    
    If prefs are disabling their usage, avoid creating instances for
    'cert-revocations' and 'intermediates'.
    
    Do not ship JSON dumps for collections we do not expect to need. For
    the ones in the 'main' bucket, this prevents them from being synced
    unnecessarily (the code in remote-settings does so for collections
    in the main bucket for which a dump or local data exists). For the
    collections in the other buckets, we just save some size by not
    shipping their dumps.
    
    We also clear the collections database on the v2 -> v3 migration.

--- browser/components/search/SearchSERPTelemetry.sys.mjs
+++ browser/components/search/SearchSERPTelemetry.sys.mjs
@@ -278,7 +278,8 @@ class TelemetryHandler {
    * can be tracked.
    */
   async init() {
-    if (this._initialized) {
+    // eslint-disable-next-line no-constant-condition
+    if (this._initialized || true) {
       return;
     }
 
@@ -995,7 +996,7 @@ class TelemetryHandler {
    *   Returns a provider or undefined if no provider was found for the url.
    */
   _getProviderInfoForURL(url) {
-    return this._searchProviderInfo.find(info =>
+    return this._searchProviderInfo?.find(info =>
       info.searchPageRegexp.test(url)
     );
   }
