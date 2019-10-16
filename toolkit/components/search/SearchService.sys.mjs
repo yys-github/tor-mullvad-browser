98792f83815824a805b6f6f1913772ee5925721b
Alex Catarineu <acat@torproject.org>
2019-10-16 23:01:12 +0200

    BB 31740: Remove some unnecessary RemoteSettings instances
    More concretely, SearchService.jsm 'hijack-blocklists' and
    url-classifier-skip-urls.
    
    Avoid creating instance for 'anti-tracking-url-decoration'.
    
    If prefs are disabling their usage, avoid creating instances for
    'cert-revocations' and 'intermediates'.
    
    Do not ship JSON dumps for collections we do not expect to need. For
    the ones in the 'main' bucket, this prevents them from being synced
    unnecessarily (the code in remote-settings does so for collections
    in the main bucket for which a dump or local data exists). For the
    collections in the other buckets, we just save some size by not
    shipping their dumps.
    
    We also clear the collections database on the v2 -> v3 migration.

--- toolkit/components/search/SearchService.sys.mjs
+++ toolkit/components/search/SearchService.sys.mjs
@@ -1471,10 +1471,6 @@ export class SearchService {
       this.#earlyObserversAdded = true;
     }
 
-    this.#getIgnoreListAndSubscribe().catch(ex =>
-      console.error(ex, "Search Service could not get the ignore list.")
-    );
-
     this.#engineSelector = new lazy.SearchEngineSelector(
       this.#handleConfigurationUpdated.bind(this)
     );
@@ -1614,6 +1610,7 @@ export class SearchService {
    * handled via a sync listener.
    *
    */
+  // eslint-disable-next-line no-unused-private-class-members
   async #getIgnoreListAndSubscribe() {
     let listener = this.#handleIgnoreListUpdated.bind(this);
     const current = await lazy.IgnoreLists.getAndSubscribe(listener);
