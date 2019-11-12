e7562407a1e2032892224f38162dba7cff38f0f3
Kathy Brade <brade@pearlcrescent.com>
2019-11-12 16:11:05 -0500

    TB 30237: Add v3 onion services client authentication prompt
    When Tor informs the browser that client authentication is needed,
    temporarily load about:blank instead of about:neterror and prompt
    for the user's key.
    
    If a correctly formatted key is entered, use Tor's ONION_CLIENT_AUTH_ADD
    control port command to add the key (via Torbutton's control port
    module) and reload the page.
    
    If the user cancels the prompt, display the standard about:neterror
    "Unable to connect" page. This requires a small change to
    browser/actors/NetErrorChild.jsm to account for the fact that the
    docShell no longer has the failedChannel information. The failedChannel
    is used to extract TLS-related error info, which is not applicable
    in the case of a canceled .onion authentication prompt.
    
    Add a leaveOpen option to PopupNotifications.show so we can display
    error messages within the popup notification doorhanger without
    closing the prompt.
    
    Add support for onion services strings to the TorStrings module.
    
    Add support for Tor extended SOCKS errors (Tor proposal 304) to the
    socket transport and SOCKS layers. Improved display of all of these
    errors will be implemented as part of bug 30025.
    
    Also fixes bug 19757:
     Add a "Remember this key" checkbox to the client auth prompt.
    
     Add an "Onion Services Authentication" section within the
     about:preferences "Privacy & Security section" to allow
     viewing and removal of v3 onion client auth keys that have
     been stored on disk.
    
    Also fixes bug 19251: use enhanced error pages for onion service errors.

--- toolkit/content/.eslintrc.js
+++ toolkit/content/.eslintrc.js
@@ -19,6 +19,6 @@ module.exports = {
   rules: {
     // XXX Bug 1358949 - This should be reduced down - probably to 20 or to
     // be removed & synced with the mozilla/recommended value.
-    complexity: ["error", 49],
+    complexity: ["error", 50],
   },
 };
--- toolkit/modules/PopupNotifications.sys.mjs
+++ toolkit/modules/PopupNotifications.sys.mjs
@@ -439,6 +439,8 @@ PopupNotifications.prototype = {
    *            will be dismissed instead of removed after running the callback.
    *          - [optional] disabled (boolean): If this is true, the button
    *            will be disabled.
+   *          - [optional] leaveOpen (boolean): If this is true, the notification
+   *            will not be removed after running the callback.
    *        If null, the notification will have a default "OK" action button
    *        that can be used to dismiss the popup and secondaryActions will be ignored.
    * @param secondaryActions
@@ -1985,6 +1987,10 @@ PopupNotifications.prototype = {
 
       if (action.dismiss) {
         this._dismiss();
+        return;
+      }
+
+      if (action.leaveOpen) {
         return;
       }
     }
