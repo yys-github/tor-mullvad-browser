e911562a94a90783d824a17d4c37808b5136f73d
Matthew Finkel <sysrqb@torproject.org>
2020-07-24 03:42:19 +0000

    [android] Disable features and functionality
    Bug 33594: Disable data collection by default (Glean)
    
    Bug 40019: Adjust is disabled on Release when data collection is disabled
    
    Bug 34338: Disable the crash reporter
    
    Bug 40014: Neuter Google Advertising ID
    
    Bug 40018: Disable Push service
    
    Bug 40034: Disable PWA onboading
    
    Bug 40072: Disable Tracking Protection
    
    Bug 40061: Do not show "Send to device" in sharing menu
    
    Bug 40109: Reduce requested permissions
    
    Exclude LOCATION and NETWORK_STATE

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
@@ -168,7 +168,7 @@ import org.mozilla.fenix.components.FenixAutocompletePrompt
 import org.mozilla.fenix.components.FenixSuggestStrongPasswordPrompt
 import org.mozilla.fenix.components.FindInPageIntegration
 import org.mozilla.fenix.components.StoreProvider
-import org.mozilla.fenix.components.accounts.FxaWebChannelIntegration
+// import org.mozilla.fenix.components.accounts.FxaWebChannelIntegration
 import org.mozilla.fenix.components.appstate.AppAction
 import org.mozilla.fenix.components.appstate.AppAction.MessagingAction
 import org.mozilla.fenix.components.appstate.AppAction.MessagingAction.MicrosurveyAction
@@ -315,7 +315,7 @@ abstract class BaseBrowserFragment :
     private val sitePermissionsFeature = ViewBoundFeatureWrapper<SitePermissionsFeature>()
     private val fullScreenFeature = ViewBoundFeatureWrapper<FullScreenFeature>()
     private val swipeRefreshFeature = ViewBoundFeatureWrapper<SwipeRefreshFeature>()
-    private val webchannelIntegration = ViewBoundFeatureWrapper<FxaWebChannelIntegration>()
+    // private val webchannelIntegration = ViewBoundFeatureWrapper<FxaWebChannelIntegration>()
     private val sitePermissionWifiIntegration =
         ViewBoundFeatureWrapper<SitePermissionsWifiIntegration>()
     private val secureWindowFeature = ViewBoundFeatureWrapper<SecureWindowFeature>()
@@ -1249,6 +1249,7 @@ abstract class BaseBrowserFragment :
             )
         }
 
+/*
         webchannelIntegration.set(
             feature = FxaWebChannelIntegration(
                 customTabSessionId = customTabSessionId,
@@ -1261,6 +1262,7 @@ abstract class BaseBrowserFragment :
             owner = this,
             view = view,
         )
+*/
 
         initializeEngineView(
             topToolbarHeight = context.settings().getTopToolbarHeight(
