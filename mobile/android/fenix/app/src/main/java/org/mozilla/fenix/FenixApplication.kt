bada9b9de94ae76e948c0053534b8c4458e3fe0c
Matthew Finkel <sysrqb@torproject.org>
2020-07-24 03:42:19 +0000

    [android] Disable features and functionality
    Bug 33594: Disable data collection by default (Glean)
    
    Bug 40019: Adjust is disabled on Release when data collection is disabled
    
    Bug 34338: Disable the crash reporter
    
    Bug 40014: Neuter Google Advertising ID
    
    Bug 40018: Disable Push service
    
    Bug 40034: Disable PWA onboading
    
    Bug 40072: Disable Tracking Protection
    
    Bug 40061: Do not show "Send to device" in sharing menu
    
    Bug 40109: Reduce requested permissions
    
    Exclude LOCATION and NETWORK_STATE

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
@@ -185,6 +185,11 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
     @OptIn(DelicateCoroutinesApi::class) // GlobalScope usage
     @VisibleForTesting
     protected open fun initializeGlean() {
+        if (!settings().isTelemetryEnabled) {
+            logger.debug("Preventing Glean from initializing, since telemetry is disabled")
+            return
+        }
+
         val settings = settings()
         // We delay the Glean initialization until, we have user consent (After onboarding).
         // If onboarding is disabled (when in local builds), continue to initialize Glean.
@@ -1047,8 +1052,6 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
 
     @OptIn(DelicateCoroutinesApi::class)
     open fun downloadWallpapers() {
-        GlobalScope.launch {
-            components.useCases.wallpaperUseCases.initialize()
-        }
+        // IN TOR BROWSER: we don't download wallpapers.
     }
 }
