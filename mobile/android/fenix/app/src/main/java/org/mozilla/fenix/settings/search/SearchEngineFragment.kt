a491d323d52c1d1f6efbc37256550c239b8985d9
Matthew Finkel <sysrqb@torproject.org>
2020-07-26 18:24:39 +0000

    TB 34403 [android]: Disable Normal mode by default.
    Originally, fenix#34403.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/search/SearchEngineFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/search/SearchEngineFragment.kt
@@ -38,6 +38,14 @@ class SearchEngineFragment : PreferenceFragmentCompat() {
             rootKey,
         )
 
+        findPreference<CheckBoxPreference>(getString(R.string.pref_key_show_search_suggestions_in_private))?.apply {
+            isVisible = !context.settings().shouldDisableNormalMode
+        }
+
+        findPreference<SwitchPreference>(getString(R.string.pref_key_search_browsing_history))?.apply {
+            isVisible = !context.settings().shouldDisableNormalMode
+        }
+
         // requirePreference<SwitchPreference>(R.string.pref_key_show_sponsored_suggestions).apply {
         //     isVisible = context.settings().enableFxSuggest
         // }
@@ -151,9 +159,10 @@ class SearchEngineFragment : PreferenceFragmentCompat() {
         autocompleteURLsPreference.onPreferenceChangeListener = SharedPreferenceUpdater()
 
         searchSuggestionsPreference.setOnPreferenceClickListener {
-            searchSuggestionsInPrivatePreference.isEnabled = searchSuggestionsPreference.isChecked
+            searchSuggestionsInPrivatePreference.isEnabled = searchSuggestionsPreference.isChecked && !requireContext().settings().shouldDisableNormalMode
             trendingSearchSuggestionsPreference.isEnabled =
                 getSelectedSearchEngine(requireContext())?.trendingUrl != null && searchSuggestionsPreference.isChecked
+                    && !requireContext().settings().shouldDisableNormalMode
             true
         }
 
