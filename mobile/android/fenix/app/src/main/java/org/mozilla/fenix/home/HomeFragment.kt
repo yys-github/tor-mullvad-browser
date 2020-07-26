e6e956c2a400ba6fa1a390f9bef47e70df46ffc2
Matthew Finkel <sysrqb@torproject.org>
2020-07-26 18:24:39 +0000

    TB 34403 [android]: Disable Normal mode by default.
    Originally, fenix#34403.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat.getColor
 import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -858,6 +859,8 @@ class HomeFragment : Fragment() {
             sessionControlInteractor.onPrivateModeButtonClicked(newMode)
             Homepage.privateModeIconTapped.record(NoExtras())
         }
+
+        binding.privateBrowsingButton.isGone = view.context.settings().shouldDisableNormalMode
 
         consumeFrom(requireComponents.core.store) {
             toolbarView.updateTabCounter(it)
