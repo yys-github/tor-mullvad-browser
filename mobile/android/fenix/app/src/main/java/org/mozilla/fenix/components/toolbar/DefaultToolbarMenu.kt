6c31581ace8bc37a12061ab35e6f9272e5056e08
Matthew Finkel <sysrqb@torproject.org>
2020-07-26 18:24:39 +0000

    TB 34403 [android]: Disable Normal mode by default.
    Originally, fenix#34403.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/toolbar/DefaultToolbarMenu.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/toolbar/DefaultToolbarMenu.kt
@@ -437,7 +437,7 @@ open class DefaultToolbarMenu(
                 newTabItem,
                 BrowserMenuDivider(),
                 bookmarksItem,
-                historyItem,
+                if (context.settings().shouldDisableNormalMode) null else historyItem,
                 downloadsItem,
                 passwordsItem,
                 extensionsItem,
