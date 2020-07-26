2db987eaf1ad03f83a2ae6d2c1ac4dbe7dd5a1d5
Matthew Finkel <sysrqb@torproject.org>
2020-07-26 18:24:39 +0000

    TB 34403: [android] Disable Normal mode by default.
    Originally, fenix#34403.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -548,7 +548,7 @@ class Settings(
 
     var openLinksInAPrivateTab by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_open_links_in_a_private_tab),
-        default = false,
+        default = true,
     )
 
     val shouldSecureModeBeOverridden
@@ -826,9 +826,14 @@ class Settings(
         default = 1f,
     )
 
+    val shouldDisableNormalMode by booleanPreference(
+        appContext.getPreferenceKey(R.string.pref_key_disable_normal_mode),
+        true
+    )
+
     val shouldShowHistorySuggestions by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_search_browsing_history),
-        default = true,
+        default = !shouldDisableNormalMode,
     )
 
     val shouldShowBookmarkSuggestions by booleanPreference(
@@ -1397,11 +1402,11 @@ class Settings(
             return touchExplorationIsEnabled || switchServiceIsEnabled
         }
 
-    var lastKnownMode: BrowsingMode = BrowsingMode.Normal
+    var lastKnownMode: BrowsingMode = BrowsingMode.Private
         get() {
             val lastKnownModeWasPrivate = preferences.getBoolean(
                 appContext.getPreferenceKey(R.string.pref_key_last_known_mode_private),
-                false,
+                shouldDisableNormalMode,
             )
 
             return if (lastKnownModeWasPrivate) {
