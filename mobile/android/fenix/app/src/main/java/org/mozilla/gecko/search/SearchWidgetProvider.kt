0168f686c8d100ba97fe6cb2cc1b6082a4da7f96
Matthew Finkel <sysrqb@torproject.org>
2020-07-31 16:49:03 +0000

    [android] Rename as Tor Browser
    Bug 40020: Change applicationId
    
    Bug 40020: Change app name
    
    Bug 40020: Change deeplink scheme
    
    Bug 40020: Change App icons
    
    Bug 40073: Use correct branding on About page
    
    Bug 40088: Use Tor Browser logo in migration screen

--- mobile/android/fenix/app/src/main/java/org/mozilla/gecko/search/SearchWidgetProvider.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/gecko/search/SearchWidgetProvider.kt
@@ -167,7 +167,7 @@ class SearchWidgetProvider : AppWidgetProvider() {
         // gradient color available for android:fillColor only on SDK 24+
         setImageViewResource(
             R.id.button_search_widget_new_tab_icon,
-            R.drawable.ic_launcher_foreground,
+            R.drawable.R.mipmap.ic_launcher,
         )
 
         val appName = context.getString(R.string.app_name)
