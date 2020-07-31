fc27ddcf6eeaac5b6c02d1179fbdc18806287d90
Matthew Finkel <sysrqb@torproject.org>
2020-07-31 16:49:03 +0000

    [android] Rename as Tor Browser
    Bug 40020: Change applicationId
    
    Bug 40020: Change app name
    
    Bug 40020: Change deeplink scheme
    
    Bug 40020: Change App icons
    
    Bug 40073: Use correct branding on About page
    
    Bug 40088: Use Tor Browser logo in migration screen

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/ThumbnailImage.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/ThumbnailImage.kt
@@ -199,7 +199,7 @@ private fun FallbackContent(
             )
         } else if (tabUrl == ABOUT_HOME_URL) {
             Image(
-                painter = painterResource(id = R.drawable.ic_firefox),
+                painter = painterResource(id = R.drawable.tor_browser_app_icon),
                 contentDescription = null,
                 modifier = Modifier
                     .size(FallbackIconSize)
