b6f1d2e84357456dddeac41d00596c81f7479b49
Alex Catarineu <acat@torproject.org>
2020-08-13 11:05:03 +0200

    TB 40073: Disable remote Public Suffix List fetching
    In https://bugzilla.mozilla.org/show_bug.cgi?id=1563246 Firefox implemented
    fetching the Public Suffix List via RemoteSettings and replacing the default
    one at runtime, which we do not want.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -76,8 +76,6 @@ ChromeUtils.defineESModuleGetters(lazy, {
   PluginManager: "resource:///actors/PluginParent.sys.mjs",
   PrivateBrowsingUtils: "resource://gre/modules/PrivateBrowsingUtils.sys.mjs",
   ProcessHangMonitor: "resource:///modules/ProcessHangMonitor.sys.mjs",
-  PublicSuffixList:
-    "resource://gre/modules/netwerk-dns/PublicSuffixList.sys.mjs",
   QuickSuggest: "resource:///modules/QuickSuggest.sys.mjs",
   RFPHelper: "resource://gre/modules/RFPHelper.sys.mjs",
   RemoteSecuritySettings:
@@ -3363,10 +3361,6 @@ BrowserGlue.prototype = {
         lazy.RemoteSettings.init();
         this._addBreachesSyncHandler();
       }.bind(this),
-
-      function PublicSuffixListInit() {
-        lazy.PublicSuffixList.init();
-      },
 
       function RemoteSecuritySettingsInit() {
         lazy.RemoteSecuritySettings.init();
