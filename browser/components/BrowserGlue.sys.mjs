9c3eecc85450afa26dd9abacb624fee4d7e12b47
Kathy Brade <brade@pearlcrescent.com>
2020-08-14 09:06:33 -0400

    BB 40002: Remove about:ion
    Firefox Ion (previously Firefox Pioneer) is an opt-in program in which people
    volunteer to participate in studies that collect detailed, sensitive data about
    how they use their browser.
    
    Bug 41662: Disable about:sync-logs
    
    Even though we disable sync by default with
    `identity.fxaccounts.enabled`, this about: page is still avilable.
    We could throw an exception on the constructor of the related
    component, but it would result only in an error in the console, without
    a visible "this address does not look right" error page.
    If we fix the issues with MOZ_SERVICES_SYNC, we can restore the
    component.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -2557,8 +2557,6 @@ BrowserGlue.prototype = {
 
     this._monitorWebcompatReporterPref();
     this._monitorHTTPSOnlyPref();
-    this._monitorIonPref();
-    this._monitorIonStudies();
     this._setupSearchDetection();
 
     this._monitorGPCPref();
