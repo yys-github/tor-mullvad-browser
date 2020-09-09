64910dfe18ada07478ebec4ed453c8924be015b2
Matthew Finkel <sysrqb@torproject.org>
2020-09-09 22:59:30 +0000

    [android] Modify UI/UX
    Bug 40015: Modify Home menu
    
    Bug 40016: Hide unwanted Settings
    
    Bug 40016: Modify Default toolbar menu
    
    Bug 40016: Add Donate settings button
    
    Bug 40016: Move Allow Screenshots under Advanced
    
    Bug 40016: Don't install WebCompat webext
    
    Bug 40016: Don't onboard Search Suggestions
    
    Bug 40094: Do not use MasterPasswordTipProvider in HomeFragment
    
    Bug 40095: Hide "Sign in to sync" in bookmarks
    
    Bug 40031: Hide Mozilla-specific items on About page
    
    Bug 40063: Do not sort search engines alphabetically
    
    Bug 40141: Hide EME site permission
    
    Bug 40166: Hide "Normal" tab (again) and Sync tab in TabTray
    
    Bug 40167: Hide "Save to Collection" in menu
    
    Bug 40172: Find the Quit button
    
    Bug 40186: Hide Credit Cards in Settings
    
    Bug 40198: Spoof English toggle now overlaps with locale list

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -52,6 +52,7 @@ import mozilla.components.support.utils.DefaultDateTimeProvider
 import mozilla.components.support.utils.ext.navigateToDefaultBrowserAppsSettings
 import mozilla.components.ui.widgets.withCenterAlignedButtons
 import mozilla.telemetry.glean.private.NoExtras
+import org.mozilla.fenix.BrowserDirection
 import org.mozilla.fenix.Config
 import org.mozilla.fenix.FeatureFlags
 import org.mozilla.fenix.GleanMetrics.Addons
@@ -88,6 +89,8 @@ import java.lang.ref.WeakReference
 import kotlin.system.exitProcess
 import mozilla.components.ui.icons.R as iconsR
 import org.mozilla.fenix.GleanMetrics.Settings as SettingsMetrics
+
+import android.view.WindowManager
 
 /**
  * Main settings screen.
@@ -561,6 +564,16 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
                 SettingsFragmentDirections.actionSettingsFragmentToAboutFragment()
             }
 
+            resources.getString(R.string.pref_key_donate) -> {
+                @Suppress("DEPRECATION")
+                (activity as HomeActivity).openToBrowserAndLoad(
+                    searchTermOrURL = SupportUtils.DONATE_URL,
+                    newTab = true,
+                    from = BrowserDirection.FromSettings
+                )
+                null
+            }
+
             // Only displayed when secret settings are enabled
             resources.getString(R.string.pref_key_debug_settings) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToSecretSettingsFragment()
@@ -591,6 +604,21 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
         val debuggingKey = getPreferenceKey(R.string.pref_key_remote_debugging)
         val preferenceLeakCanary = findPreference<Preference>(leakKey)
         val preferenceRemoteDebugging = findPreference<Preference>(debuggingKey)
+
+        // Copied from PrivateBrowsingFragment with some removals
+        requirePreference<SwitchPreferenceCompat>(R.string.pref_key_allow_screenshots_in_private_mode).apply {
+            onPreferenceChangeListener = object : SharedPreferenceUpdater() {
+                override fun onPreferenceChange(preference: Preference, newValue: Any?): Boolean {
+                    if (newValue == false) {
+                        activity?.window?.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                    } else {
+                        activity?.window?.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                    }
+                    return super.onPreferenceChange(preference, newValue)
+                }
+            }
+        }
+
 
         if (!Config.channel.isReleased) {
             preferenceLeakCanary?.setOnPreferenceChangeListener { _, newValue ->
