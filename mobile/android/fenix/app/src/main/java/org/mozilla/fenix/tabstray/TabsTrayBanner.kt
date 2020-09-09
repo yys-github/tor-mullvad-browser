6ad9c6fe8c828465520794f11855b17b7f953a65
Matthew Finkel <sysrqb@torproject.org>
2020-09-09 22:59:30 +0000

    [android] Modify UI/UX
    Bug 40015: Modify Home menu
    
    Bug 40016: Hide unwanted Settings
    
    Bug 40016: Modify Default toolbar menu
    
    Bug 40016: Add Donate settings button
    
    Bug 40016: Move Allow Screenshots under Advanced
    
    Bug 40016: Don't install WebCompat webext
    
    Bug 40016: Don't onboard Search Suggestions
    
    Bug 40094: Do not use MasterPasswordTipProvider in HomeFragment
    
    Bug 40095: Hide "Sign in to sync" in bookmarks
    
    Bug 40031: Hide Mozilla-specific items on About page
    
    Bug 40063: Do not sort search engines alphabetically
    
    Bug 40141: Hide EME site permission
    
    Bug 40166: Hide "Normal" tab (again) and Sync tab in TabTray
    
    Bug 40167: Hide "Save to Collection" in menu
    
    Bug 40172: Find the Quit button
    
    Bug 40186: Hide Credit Cards in Settings
    
    Bug 40198: Spoof English toggle now overlaps with locale list

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabstray/TabsTrayBanner.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tabstray/TabsTrayBanner.kt
@@ -257,6 +257,7 @@ private fun TabPageBanner(
                     unselectedContentColor = inactiveColor,
                 )
 
+/*
                 Tab(
                     selected = selectedPage == Page.SyncedTabs,
                     onClick = { onTabPageIndicatorClicked(Page.SyncedTabs) },
@@ -272,6 +273,7 @@ private fun TabPageBanner(
                     selectedContentColor = selectedColor,
                     unselectedContentColor = inactiveColor,
                 )
+*/
             }
 
             Spacer(modifier = Modifier.weight(1.0f))
