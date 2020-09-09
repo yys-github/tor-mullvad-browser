3cc18f5351ec4989206ac97a76354cad60e32ea5
Matthew Finkel <sysrqb@torproject.org>
2020-09-09 22:59:30 +0000

    [android] Modify UI/UX
    Bug 40015: Modify Home menu
    
    Bug 40016: Hide unwanted Settings
    
    Bug 40016: Modify Default toolbar menu
    
    Bug 40016: Add Donate settings button
    
    Bug 40016: Move Allow Screenshots under Advanced
    
    Bug 40016: Don't install WebCompat webext
    
    Bug 40016: Don't onboard Search Suggestions
    
    Bug 40094: Do not use MasterPasswordTipProvider in HomeFragment
    
    Bug 40095: Hide "Sign in to sync" in bookmarks
    
    Bug 40031: Hide Mozilla-specific items on About page
    
    Bug 40063: Do not sort search engines alphabetically
    
    Bug 40141: Hide EME site permission
    
    Bug 40166: Hide "Normal" tab (again) and Sync tab in TabTray
    
    Bug 40167: Hide "Save to Collection" in menu
    
    Bug 40172: Find the Quit button
    
    Bug 40186: Hide Credit Cards in Settings
    
    Bug 40198: Spoof English toggle now overlaps with locale list

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -27,6 +27,8 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.content.ContextCompat.getColor
 import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.children
+import androidx.core.view.doOnLayout
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -73,6 +75,7 @@ import mozilla.components.lib.state.ext.flow
 import mozilla.components.support.base.feature.ViewBoundFeatureWrapper
 import mozilla.telemetry.glean.private.NoExtras
 import org.mozilla.fenix.BrowserDirection
+import org.mozilla.fenix.BuildConfig
 import org.mozilla.fenix.GleanMetrics.HomeScreen
 import org.mozilla.fenix.GleanMetrics.Homepage
 import org.mozilla.fenix.HomeActivity
@@ -292,6 +295,16 @@ class HomeFragment : Fragment() {
             orientationChange = false,
             orientation = requireContext().resources.configuration.orientation,
         )
+
+        // Splits by full stops or commas and puts the parts in different lines.
+        // Ignoring separators at the end of the string, it is expected
+        // that there are at most two parts (e.g. "Explore. Privately.").
+        val localBinding = binding
+        binding.exploreprivately.text = localBinding
+            .exploreprivately
+            .text
+            ?.replace(" *([.,。।]) *".toRegex(), "$1\n")
+            ?.trim()
 
         lifecycleScope.launch(IO) {
             val settings = requireContext().settings()
@@ -1299,7 +1312,8 @@ class HomeFragment : Fragment() {
             else -> ColorStateList.valueOf(color)
         }
 
-        binding.wordmarkText.imageTintList = tintColor
+        // tor-browser#42590
+        // binding.wordmarkText.imageTintList = tintColor
         binding.privateBrowsingButton.buttonTintList = tintColor
     }
 
