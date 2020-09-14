904425c3440a162dae61902f9977efd7e70a96c7
Matthew Finkel <sysrqb@torproject.org>
2020-09-14 03:48:05 +0000

    TB 40026 [android]: Implement Security Level settings on Android.
    Originally, fenix#40026.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -10,6 +10,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
@@ -82,6 +83,7 @@ import org.mozilla.fenix.perf.ProfilerViewModelFactory
 import org.mozilla.fenix.settings.account.AccountUiView
 import org.mozilla.fenix.snackbar.FenixSnackbarDelegate
 import org.mozilla.fenix.snackbar.SnackbarBinding
+import org.mozilla.fenix.tor.TorSecurityLevel
 import org.mozilla.fenix.utils.Settings
 import java.lang.ref.WeakReference
 import kotlin.system.exitProcess
@@ -432,6 +434,10 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
                 SettingsFragmentDirections.actionSettingsFragmentToPrivateBrowsingFragment()
             }
 
+            resources.getString(R.string.pref_key_tor_security_level) -> {
+                SettingsFragmentDirections.actionSettingsFragmentToTorSecurityLevelFragment()
+            }
+
             resources.getString(R.string.pref_key_https_only_settings) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToHttpsOnlyFragment()
             }
@@ -673,6 +679,7 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
             FeatureFlags.customExtensionCollectionFeature,
         )
         setupGeckoLogsPreference(settings)
+        setupSecurityLevelPreference()
         setupHttpsOnlyPreferences(settings)
         setupIPProtectionPreferences(settings)
         setupNotificationPreference(
@@ -912,6 +919,19 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
             isVisible = settings.isIPProtectionAvailable
             showBetaBadge = FxNimbus.features.ipProtection.value().showBetaBadge
         }
+    }
+
+    @VisibleForTesting
+    internal fun setupSecurityLevelPreference() {
+        val securityLevelPreference =
+            requirePreference<Preference>(R.string.pref_key_tor_security_level)
+        securityLevelPreference.summary =
+            when (requireContext().settings().torSecurityLevel) {
+                TorSecurityLevel.STANDARD.level -> getString(R.string.tor_security_level_standard)
+                TorSecurityLevel.SAFER.level    -> getString(R.string.tor_security_level_safer)
+                TorSecurityLevel.SAFEST.level   -> getString(R.string.tor_security_level_safest)
+                else -> throw Exception("Unexpected TorSecurityLevel of ${requireContext().settings().torSecurityLevel}")
+            }
     }
 
     private fun updateProfilerUI(profilerStatus: Boolean) {
