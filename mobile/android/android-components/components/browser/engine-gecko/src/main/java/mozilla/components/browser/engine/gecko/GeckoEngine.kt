4e5fc485b0e2a9ab29f461cbd41e7119a5b13fb2
Matthew Finkel <sysrqb@torproject.org>
2020-09-14 03:48:05 +0000

    TB 40026 [android]: Implement Security Level settings on Android.
    Originally, fenix#40026.

--- mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngine.kt
+++ mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngine.kt
@@ -1332,6 +1332,12 @@ class GeckoEngine(
             get() = runtime.settings.globalPrivacyControl
             set(value) { runtime.settings.setGlobalPrivacyControl(value) }
 
+        override var torSecurityLevel: Int
+            get() = runtime.settings.torSecurityLevel
+            set(value) {
+                runtime.settings.torSecurityLevel = value
+            }
+
         override var spoofEnglish: Boolean
             get() = runtime.settings.spoofEnglish
             set(value) {
@@ -1366,6 +1372,7 @@ class GeckoEngine(
             this.cookieBannerHandlingGlobalRulesSubFrames = it.cookieBannerHandlingGlobalRulesSubFrames
             this.globalPrivacyControlEnabled = it.globalPrivacyControlEnabled
             this.emailTrackerBlockingPrivateBrowsing = it.emailTrackerBlockingPrivateBrowsing
+            this.torSecurityLevel = it.torSecurityLevel
             this.spoofEnglish = it.spoofEnglish
         }
     }
