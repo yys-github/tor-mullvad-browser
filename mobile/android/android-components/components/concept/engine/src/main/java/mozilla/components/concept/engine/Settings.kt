0f5166a91469128f7563d8d9ca801421e5429fe1
Matthew Finkel <sysrqb@torproject.org>
2020-09-14 03:48:05 +0000

    TB 40026 [android]: Implement Security Level settings on Android.
    Originally, fenix#40026.

--- mobile/android/android-components/components/concept/engine/src/main/java/mozilla/components/concept/engine/Settings.kt
+++ mobile/android/android-components/components/concept/engine/src/main/java/mozilla/components/concept/engine/Settings.kt
@@ -279,6 +279,11 @@ abstract class Settings {
      */
     open var userCharacteristicPingCurrentVersion: Int by UnsupportedSetting()
 
+    /**
+     * Setting to control the current security level
+     */
+    open var torSecurityLevel: Int by UnsupportedSetting()
+
     open var spoofEnglish: Boolean by UnsupportedSetting()
 }
 
@@ -335,6 +340,7 @@ data class DefaultSettings(
     override var queryParameterStrippingStripList: String = "",
     override var emailTrackerBlockingPrivateBrowsing: Boolean = false,
     override var userCharacteristicPingCurrentVersion: Int = 0,
+    override var torSecurityLevel: Int = 4,
     override var spoofEnglish: Boolean = false,
 ) : Settings()
 
