4fe9df0ecb0fc1b367f23a01351814d27623bd30
Matthew Finkel <sysrqb@torproject.org>
2020-09-14 03:48:05 +0000

    TB 40026 [android]: Implement Security Level settings on Android.
    Originally, fenix#40026.

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tor/TorSecurityLevel.kt
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.tor
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+enum class TorSecurityLevel(val level: Int) : Parcelable {
+    STANDARD(4), SAFER(2), SAFEST(1)
+}
