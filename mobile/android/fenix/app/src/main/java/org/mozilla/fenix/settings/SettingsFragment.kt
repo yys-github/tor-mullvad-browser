bac824aeeb7e82c6d716f0c06903aea4e8b443ee
Matthew Finkel <sysrqb@torproject.org>
2020-09-18 01:57:44 +0000

    TB 40041 [android]: Implement Tor Network Settings
    Originally, fenix#40041.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -34,8 +34,10 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import mozilla.components.browser.state.state.selectedOrDefaultSearchEngine
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.sync.AccountObserver
@@ -57,6 +59,7 @@ import org.mozilla.fenix.GleanMetrics.TrackingProtection
 import org.mozilla.fenix.GleanMetrics.Translations
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
+import org.mozilla.fenix.ReleaseChannel
 import org.mozilla.fenix.components.accounts.FenixFxAEntryPoint
 import org.mozilla.fenix.databinding.AmoCollectionOverrideDialogBinding
 import org.mozilla.fenix.ext.application
@@ -73,6 +76,7 @@ import org.mozilla.fenix.settings.account.AccountUiView
 import org.mozilla.fenix.snackbar.FenixSnackbarDelegate
 import org.mozilla.fenix.snackbar.SnackbarBinding
 import org.mozilla.fenix.tor.TorSecurityLevel
+import org.mozilla.fenix.tor.QuickstartViewModel
 import org.mozilla.fenix.utils.Settings
 import kotlin.system.exitProcess
 import org.mozilla.fenix.GleanMetrics.Settings as SettingsMetrics
@@ -85,6 +89,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
     private lateinit var addonFilePicker: AddonFilePicker
     private val profilerViewModel: ProfilerViewModel by activityViewModels()
     private val snackbarBinding = ViewBoundFeatureWrapper<SnackbarBinding>()
+
+    private val quickstartViewModel: QuickstartViewModel by activityViewModels()
 
     @VisibleForTesting
     internal val accountObserver = object : AccountObserver {
@@ -184,7 +190,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        runBlocking(context = Dispatchers.IO) {
+            setPreferencesFromResource(R.xml.preferences, rootKey)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -222,7 +230,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
         update(shouldUpdateAccountUIState = !creatingFragment)
 
         requireView().findViewById<RecyclerView>(R.id.recycler_view)
-            ?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+            .also {
+                it?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+                // Prevent disabled settings from having a collapsing animation on open
+                it?.disableHidingAnimation()
+            }
 
         args.preferenceToScrollTo?.let {
             scrollToPreference(it)
@@ -282,9 +294,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //            getString(R.string.preferences_credit_cards_2)
 //        }
 
-        val openLinksInAppsSettingsPreference =
-            requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
-        openLinksInAppsSettingsPreference.summary = context?.settings()?.getOpenLinksInAppsString()
+        // val openLinksInAppsSettingsPreference =
+        //     requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
+        // openLinksInAppsSettingsPreference.summary = context?.settings()?.getOpenLinksInAppsString()
 
         // Hide "Delete browsing data on quit" when in Private Browsing-only mode
         deleteBrowsingDataPreference.isVisible =
@@ -469,9 +481,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 SettingsFragmentDirections.actionSettingsFragmentToLinkSharingFragment()
             }
 
-            resources.getString(R.string.pref_key_open_links_in_apps) -> {
-                SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
-            }
+            // resources.getString(R.string.pref_key_open_links_in_apps) -> {
+            //     SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
+            // }
 
             resources.getString(R.string.pref_key_sync_debug) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToSyncDebugFragment()
@@ -609,6 +621,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setupHomepagePreference()
         setupTrackingProtectionPreference()
         setupDnsOverHttpsPreference(requireContext().settings())
+        setupConnectionPreferences()
     }
 
     /**
@@ -641,6 +654,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
             delay(SCROLL_INDICATOR_DELAY)
             scrollBarSize = originalSize
         }
+    }
+
+    private fun RecyclerView.disableHidingAnimation() {
+        this.setItemAnimator(null)
+        this.setLayoutAnimation(null)
     }
 
     @VisibleForTesting
@@ -743,6 +761,56 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 Engine.DohSettingsMode.OFF -> getString(R.string.preference_doh_off)
                 Engine.DohSettingsMode.INCREASED -> getString(R.string.preference_doh_increased_protection)
                 Engine.DohSettingsMode.MAX -> getString(R.string.preference_doh_max_protection)
+            }
+        }
+    }
+
+    internal fun setupConnectionPreferences() {
+        // will be needed for phase2
+        //val torController = requireContext().components.torController
+
+        requirePreference<Preference>(R.string.pref_key_tor_network_settings_bridge_config).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections
+                        .actionSettingsFragmentToTorBridgeConfigFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+
+        requirePreference<SwitchPreference>(R.string.pref_key_quick_start).apply {
+            isChecked = quickstartViewModel.quickstart().value == true
+            setOnPreferenceClickListener {
+                quickstartViewModel.quickstartSet(
+                    isChecked,
+                )
+                true
+            }
+        }
+
+        requirePreference<Preference>(R.string.pref_key_use_html_connection_ui).apply {
+            onPreferenceChangeListener = object : SharedPreferenceUpdater() {}
+            isVisible = Config.channel != ReleaseChannel.Release
+        }
+
+        requirePreference<Preference>(R.string.pref_key_tor_logs).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections.actionSettingsFragmentToTorLogsFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+        requirePreference<Preference>(R.string.pref_key_about_config_shortcut).apply {
+            isVisible = requireContext().settings().showSecretDebugMenuThisSession || Config.channel == ReleaseChannel.Debug
+            setOnPreferenceClickListener {
+                (requireActivity() as HomeActivity).openToBrowserAndLoad(
+                    searchTermOrURL = "about:config",
+                    from = BrowserDirection.FromSettings,
+                    newTab = true,
+                )
+                true
             }
         }
     }
