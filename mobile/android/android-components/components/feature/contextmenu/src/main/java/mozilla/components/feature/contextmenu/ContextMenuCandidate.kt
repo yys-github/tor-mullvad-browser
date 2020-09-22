01242e4eeb0807e56ae66949ddddca52184477a8
Alex Catarineu <acat@torproject.org>
2020-09-22 16:34:51 +0200

    TB 34378: [android] Port external helper app prompting
    Together with the corresponding fenix patch, this allows all `startActivity`
    that may open external apps to be replaced by `TorUtils.startActivityPrompt`.
    
    Originally, android-components#40007 and fenix#34378.

--- mobile/android/android-components/components/feature/contextmenu/src/main/java/mozilla/components/feature/contextmenu/ContextMenuCandidate.kt
+++ mobile/android/android-components/components/feature/contextmenu/src/main/java/mozilla/components/feature/contextmenu/ContextMenuCandidate.kt
@@ -29,6 +29,7 @@ import mozilla.components.support.ktx.kotlin.takeOrReplace
 import mozilla.components.ui.widgets.DefaultSnackbarDelegate
 import mozilla.components.ui.widgets.SnackbarDelegate
 import kotlin.String
+import mozilla.components.support.utils.TorUtils
 
 /**
  * A candidate for an item to be displayed in the context menu.
@@ -508,7 +509,8 @@ data class ContextMenuCandidate(
                 }
 
                 try {
-                    context.startActivity(
+                    TorUtils.startActivityPrompt(
+                        context,
                         intent.createChooserExcludingCurrentApp(
                             context,
                             context.getString(R.string.mozac_feature_contextmenu_share_link),
