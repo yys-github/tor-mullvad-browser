727bcb117d543f3f093b9f2337f6b3969fbeba13
Alex Catarineu <acat@torproject.org>
2020-09-22 16:34:51 +0200

    TB 34378: [android] Port external helper app prompting
    Together with the corresponding fenix patch, this allows all `startActivity`
    that may open external apps to be replaced by `TorUtils.startActivityPrompt`.
    
    Originally, android-components#40007 and fenix#34378.

--- mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/content/Context.kt
+++ mobile/android/android-components/components/support/ktx/src/main/java/mozilla/components/support/ktx/android/content/Context.kt
@@ -52,6 +52,7 @@ import mozilla.components.support.base.log.logger.Logger
 import mozilla.components.support.ktx.R
 import mozilla.components.support.ktx.android.content.res.resolveAttribute
 import mozilla.components.support.utils.ext.packageManagerCompatHelper
+import mozilla.components.support.utils.TorUtils
 import java.io.File
 
 /**
@@ -120,12 +121,11 @@ fun Context.share(text: String, subject: String = getString(R.string.mozac_suppo
             flags = FLAG_ACTIVITY_NEW_TASK
         }
 
-        startActivity(
-            intent.createChooserExcludingCurrentApp(
-                this,
-                getString(R.string.mozac_support_ktx_menu_share_with),
-            ),
-        )
+        val shareIntent = intent.createChooserExcludingCurrentApp(this, getString(R.string.mozac_support_ktx_menu_share_with)).apply {
+            flags = FLAG_ACTIVITY_NEW_TASK
+        }
+
+        TorUtils.startActivityPrompt(this, shareIntent)
         true
     } catch (e: ActivityNotFoundException) {
         Log.log(Log.Priority.WARN, message = "No activity to share to found", throwable = e, tag = "share")
@@ -314,7 +314,7 @@ fun Context.email(
             flags = FLAG_ACTIVITY_NEW_TASK
         }
 
-        startActivity(emailIntent)
+        TorUtils.startActivityPrompt(this, emailIntent)
         true
     } catch (e: ActivityNotFoundException) {
         Logger.warn("No activity found to handle email intent", throwable = e)
@@ -345,7 +345,7 @@ fun Context.call(
             flags = FLAG_ACTIVITY_NEW_TASK
         }
 
-        startActivity(callIntent)
+        TorUtils.startActivityPrompt(this, callIntent)
         true
     } catch (e: ActivityNotFoundException) {
         Logger.warn("No activity found to handle dial intent", throwable = e)
@@ -373,7 +373,7 @@ fun Context.addContact(
             addFlags(FLAG_ACTIVITY_NEW_TASK)
         }
 
-        startActivity(intent)
+        TorUtils.startActivityPrompt(this, intent)
         true
     } catch (e: ActivityNotFoundException) {
         Logger.warn("No activity found to handle dial intent", throwable = e)
