e69a3e6312892535976298c116bbb64e41441c75
Alex Catarineu <acat@torproject.org>
2020-10-01 18:31:11 +0200

    [android] Modify Addon support
    android-components#40011: Hide option for disallowing addons in private mode
    
    android-components#40016: Allow inheriting from AddonCollectionProvider
    
    This will allow implementing our own AddonsProvider in fenix.

--- mobile/android/android-components/components/feature/addons/src/main/java/mozilla/components/feature/addons/AddonManager.kt
+++ mobile/android/android-components/components/feature/addons/src/main/java/mozilla/components/feature/addons/AddonManager.kt
@@ -495,6 +495,7 @@ class AddonManager(
         val installedAddon = Addon.newFromWebExtension(ext, installedState)
 
         addonUpdater.registerForFutureUpdates(installedAddon.id)
+        setAddonAllowedInPrivateBrowsing(installedAddon, true)
         completePendingAddonAction(pendingAction)
         onSuccess(installedAddon)
     }
