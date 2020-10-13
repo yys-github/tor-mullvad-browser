42a7a16d8728b724c8770c879f35024a597849c4
Georg Koppen <gk@torproject.org>
2020-10-13 07:17:10 +0000

    [android] Modify build system
    Bug 40083: Make locale ordering in BuildConfig deterministic
    
    Bug 40042: Add option do overwrite timestamp in extension version
    
    Bug 40059: Use MOZ_BUILD_DATE for versionCode
    
    At the same time we adapt MOZ_BUILD_DATE to our needs where it is
    actually used and not in tor-browser-build. This gives us more
    flexibility. See: tor-browser-build#40084.
    
    Bug 40067: Fix reproducibility issue in classes2.dex
    
    We make sure our MOZ_BUILD_DATE gets used as a source for showing date
    related information on the Fenix about page.
    
    Bug 40071: Show only supported locales
    
    Bug 40064: Use Gecko Beta for Nightly and Debug variants
    
    Bug 40123: Allow building the instrumented tests apks for variants other than debug
    
    This allows to specify the variant of the instrumented tests via
    a `testBuildType` gradle argument. It also applies a workaround for
    a R8 issue from https://issuetracker.google.com/issues/140851070.
    
    Bug 40143: Use deterministic date in Test apk
    
    The build config was using Date() when generating the Test apk's
    versionName.

---
+++ mobile/android/fenix/tools/tba-fetch-deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "\033[1;33mThis file is deprecated.\033[0m"
+echo "Run ./mach bootstrap and/or define options in your mozconfig from now on."
+echo "See documentation at: https://gitlab.torproject.org/tpo/applications/wiki/-/wikis/Development-Information/Firefox-Android/Building"
+
+exit 1
