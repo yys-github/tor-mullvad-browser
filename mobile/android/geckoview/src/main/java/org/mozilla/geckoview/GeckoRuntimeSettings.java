c30e96259cdb9fc7fb4ca45a4af55c2a7d080938
Alex Catarineu <acat@torproject.org>
2020-10-16 10:45:17 +0200

    BB 30605: Honor privacy.spoof_english in Android
    This checks `privacy.spoof_english` whenever `setLocales` is
    called from Fenix side and sets `intl.accept_languages`
    accordingly.
    
    Bug 40198: Expose privacy.spoof_english pref in GeckoView

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
@@ -724,6 +724,17 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
 
       return this;
     }
+
+    /**
+     * Sets whether we should spoof locale to English for webpages.
+     *
+     * @param flag True if we should spoof locale to English for webpages, false otherwise.
+     * @return This Builder instance.
+     */
+    public @NonNull Builder spoofEnglish(final boolean flag) {
+      getSettings().mSpoofEnglish.set(flag ? 2 : 1);
+      return this;
+    }
   }
 
   private GeckoRuntime mRuntime;
@@ -855,6 +866,7 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
       new PrefWithoutDefault<Boolean>("browser.crashReports.requestedNeverShowAgain");
   /* package */ final PrefWithoutDefault<String> mCrliteChannel =
       new PrefWithoutDefault<String>("security.pki.crlite_channel");
+  /* package */ final Pref<Integer> mSpoofEnglish = new Pref<>("privacy.spoof_english", 0);
 
   /* package */ int mPreferredColorScheme = COLOR_SCHEME_SYSTEM;
 
@@ -2543,6 +2555,26 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
    */
   public boolean getAppZygoteProcessEnabled() {
     return mAppZygoteProcess;
+  }
+
+  /**
+   * Get whether we should spoof locale to English for webpages.
+   *
+   * @return Whether we should spoof locale to English for webpages.
+   */
+  public boolean getSpoofEnglish() {
+    return mSpoofEnglish.get() == 2;
+  }
+
+  /**
+   * Set whether we should spoof locale to English for webpages.
+   *
+   * @param flag A flag determining whether we should locale to English for webpages.
+   * @return This GeckoRuntimeSettings instance.
+   */
+  public @NonNull GeckoRuntimeSettings setSpoofEnglish(final boolean flag) {
+    mSpoofEnglish.commit(flag ? 2 : 1);
+    return this;
   }
 
   @Override // Parcelable
