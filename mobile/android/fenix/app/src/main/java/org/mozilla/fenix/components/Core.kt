e07b9f82ea745f4041674795fa86cdfe3eaa854a
Alex Catarineu <acat@torproject.org>
2020-10-18 23:09:12 +0200

    TB 40087 [android]: Implement a switch for spoof English.
    Originally, fenix#40087 and android-components#40019.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
@@ -157,6 +157,7 @@ class Core(
             cookieBannerHandlingGlobalRules = context.settings().shouldEnableCookieBannerGlobalRules,
             cookieBannerHandlingGlobalRulesSubFrames = context.settings().shouldEnableCookieBannerGlobalRulesSubFrame,
             emailTrackerBlockingPrivateBrowsing = false,
+            spoofEnglish = context.settings().spoofEnglish,
         )
 
         GeckoEngine(
