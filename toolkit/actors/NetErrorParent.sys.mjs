7708c68ac2fa963bb6477e7f2de1331867744b9f
Richard Pospesel <richard@torproject.org>
2021-04-28 23:09:34 -0500

    TB 27476: Implement about:torconnect captive portal within Tor Browser
    - implements new about:torconnect page as tor-launcher replacement
    - adds new torconnect component to browser
    - tor process management functionality remains implemented in tor-launcher through the TorProtocolService module
    - adds warning/error box to about:preferences#tor when not connected to tor
    
    Bug 40773: Update the about:torconnect frontend page to match additional UI flows.
    
    Bug 41608: Add a toolbar status button and a urlbar "Connect" button.

--- toolkit/actors/NetErrorParent.sys.mjs
+++ toolkit/actors/NetErrorParent.sys.mjs
@@ -16,6 +16,7 @@ const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   BrowserUtils: "resource://gre/modules/BrowserUtils.sys.mjs",
   HomePage: "resource:///modules/HomePage.sys.mjs",
+  TorConnect: "resource://gre/modules/TorConnect.sys.mjs",
 });
 
 class CaptivePortalObserver {
@@ -281,6 +282,9 @@ export class NetErrorParent extends EscapablePageParent {
         win.openPreferences("privacy-doh");
         break;
       }
+      case "ShouldShowTorConnect":
+        return lazy.TorConnect.shouldShowTorConnect;
     }
+    return undefined;
   }
 }
--- toolkit/modules/RemotePageAccessManager.sys.mjs
+++ toolkit/modules/RemotePageAccessManager.sys.mjs
@@ -73,6 +73,7 @@ export let RemotePageAccessManager = {
       RPMAddMessageListener: ["WWWReachable"],
       RPMTryPingSecureWWWLink: ["*"],
       RPMOpenSecureWWWLink: ["*"],
+      RPMSendQuery: ["ShouldShowTorConnect"],
     },
     "about:certificate": {
       RPMSendQuery: ["getCertificates"],
@@ -133,7 +134,7 @@ export let RemotePageAccessManager = {
       RPMIsSiteSpecificTRRError: ["*"],
       RPMSetTRRDisabledLoadFlags: ["*"],
       RPMShowOSXLocalNetworkPermissionWarning: ["*"],
-      RPMSendQuery: ["Browser:AddTRRExcludedDomain"],
+      RPMSendQuery: ["Browser:AddTRRExcludedDomain", "ShouldShowTorConnect"],
       RPMGetIntPref: ["network.trr.mode", "security.dialog_enable_delay"],
     },
     "about:newtab": {
@@ -273,6 +274,25 @@ export let RemotePageAccessManager = {
       RPMSendAsyncMessage: ["Load", "closeTab", "restoreTab", "restoreAll"],
       RPMAddMessageListener: ["*"],
       RPMRemoveMessageListener: ["*"],
+    },
+    "about:torconnect": {
+      RPMAddMessageListener: [
+        "torconnect:stage-change",
+        "torconnect:bootstrap-progress",
+        "torconnect:quickstart-change",
+        "torconnect:region-names-change",
+      ],
+      RPMSendAsyncMessage: [
+        "torconnect:open-tor-preferences",
+        "torconnect:begin-bootstrapping",
+        "torconnect:cancel-bootstrapping",
+        "torconnect:set-quickstart",
+        "torconnect:view-tor-logs",
+        "torconnect:restart",
+        "torconnect:start-again",
+        "torconnect:choose-region",
+      ],
+      RPMSendQuery: ["torconnect:get-init-args", "torconnect:get-regions"],
     },
     "about:welcome": {
       RPMSendAsyncMessage: ["ActivityStream:ContentToMain"],
