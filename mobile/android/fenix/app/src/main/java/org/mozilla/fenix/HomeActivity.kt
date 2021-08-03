4952befa0c1421dad2b3b3bb88a6fc92ed72bbec
Matthew Finkel <sysrqb@torproject.org>
2021-08-03 12:44:09 +0000

    TB 40185: [android] Use NimbusDisabled
    Originally, fenix#40185.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -119,7 +119,6 @@ import org.mozilla.fenix.ext.getIntentSource
 import org.mozilla.fenix.ext.getNavDirections
 import org.mozilla.fenix.ext.hasTopDestination
 import org.mozilla.fenix.ext.nav
-import org.mozilla.fenix.ext.recordEventInNimbus
 import org.mozilla.fenix.ext.setNavigationIcon
 import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.ext.systemGesturesInsets
@@ -347,7 +346,7 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity {
             }
         } else {
             lifecycleScope.launch(IO) {
-                showFullscreenMessageIfNeeded(applicationContext)
+                // showFullscreenMessageIfNeeded(applicationContext)
             }
 
             // Unless the activity is recreated, navigate to home first (without rendering it)
@@ -391,7 +390,7 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity {
                         )
                     }
                     // This will record an event in Nimbus' internal event store. Used for behavioral targeting
-                    recordEventInNimbus("app_opened")
+                    // recordEventInNimbus("app_opened")
 
                     if (safeIntent.action.equals(ACTION_OPEN_PRIVATE_TAB) && source == APP_ICON) {
                         AppIcon.newPrivateTabTapped.record(NoExtras())
@@ -1372,10 +1371,12 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity {
             keyDismissButtonText = null,
         )
 
+        /*
         researchSurfaceDialogFragment.onAccept = {
             processIntent(messaging.getIntentForMessage(nextMessage))
             components.appStore.dispatch(AppAction.MessagingAction.MessageClicked(nextMessage))
         }
+        */
 
         researchSurfaceDialogFragment.onDismiss = {
             components.appStore.dispatch(AppAction.MessagingAction.MessageDismissed(nextMessage))
@@ -1388,10 +1389,10 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity {
             )
         }
 
-        // Update message as displayed.
-        val currentBootUniqueIdentifier = BootUtils.getBootIdentifier(context)
-
-        messaging.onMessageDisplayed(nextMessage, currentBootUniqueIdentifier)
+//        // Update message as displayed.
+//        val currentBootUniqueIdentifier = BootUtils.getBootIdentifier(context)
+//
+//        messaging.onMessageDisplayed(nextMessage, currentBootUniqueIdentifier)
     }
 
     @VisibleForTesting
