5be05c3e27bf8fec23fe1f847265e00bbf2b4094
Richard Pospesel <richard@torproject.org>
2021-08-06 16:39:03 +0200

    Bug 40597: Implement TorSettings module
    - migrated in-page settings read/write implementation from about:preferences#tor
      to the TorSettings module
    - TorSettings initially loads settings from the tor daemon, and saves them to
      firefox prefs
    - TorSettings notifies observers when a setting has changed; currently only
      QuickStart notification is implemented for parity with previous preference
      notify logic in about:torconnect and about:preferences#tor
    - about:preferences#tor, and about:torconnect now read and write settings
      thorugh the TorSettings module
    - all tor settings live in the torbrowser.settings.* preference branch
    - removed unused pref modify permission for about:torconnect content page from
      AsyncPrefs.jsm
    
    Bug 40645: Migrate Moat APIs to Moat.jsm module

--- toolkit/components/tor-launcher/TorStartupService.sys.mjs
+++ toolkit/components/tor-launcher/TorStartupService.sys.mjs
@@ -3,8 +3,10 @@ const lazy = {};
 // We will use the modules only when the profile is loaded, so prefer lazy
 // loading
 ChromeUtils.defineESModuleGetters(lazy, {
+  TorConnect: "resource://gre/modules/TorConnect.sys.mjs",
   TorLauncherUtil: "resource://gre/modules/TorLauncherUtil.sys.mjs",
   TorProviderBuilder: "resource://gre/modules/TorProviderBuilder.sys.mjs",
+  TorSettings: "resource://gre/modules/TorSettings.sys.mjs",
 });
 
 /* Browser observer topis */
@@ -28,13 +30,17 @@ export class TorStartupService {
     }
   }
 
-  async #init() {
+  #init() {
     Services.obs.addObserver(this, BrowserTopics.QuitApplicationGranted);
+
+    lazy.TorSettings.init();
 
     // Theoretically, build() is expected to await the initialization of the
     // provider, and anything needing the Tor Provider should be able to just
     // await on TorProviderBuilder.build().
     lazy.TorProviderBuilder.init();
+
+    lazy.TorConnect.init();
 
     gInited = true;
   }
@@ -44,5 +50,6 @@ export class TorStartupService {
 
     lazy.TorProviderBuilder.uninit();
     lazy.TorLauncherUtil.cleanupTempDirectories();
+    lazy.TorSettings.uninit();
   }
 }
