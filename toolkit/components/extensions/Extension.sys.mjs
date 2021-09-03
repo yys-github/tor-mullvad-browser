459cf80953edc3ead99ae77c9d11bfaf82c72c7a
Matthew Finkel <sysrqb@torproject.org>
2021-09-03 14:58:28 +0000

    BB 41598: Prevent NoScript from being removed/disabled.
    Bug 40253: Explicitly allow NoScript in Private Browsing mode.

--- toolkit/components/extensions/Extension.sys.mjs
+++ toolkit/components/extensions/Extension.sys.mjs
@@ -946,6 +946,15 @@ export class ExtensionData {
   }
 
   /**
+   * Whether this is the NoScript extension.
+   *
+   * @type {boolean}
+   */
+  get isNoScript() {
+    return this.id === "{73a6fe31-595d-460b-a920-fcc0f8843232}";
+  }
+
+  /**
    * A factory function that allows the construction of ExtensionData, with
    * the isPrivileged flag computed asynchronously.
    *
@@ -4354,6 +4363,17 @@ export class Extension extends ExtensionData {
         });
         this.permissions.delete(PRIVATE_ALLOWED_PERMISSION);
       }
+    }
+
+    // tor-browser#40253: Explicitly allow NoScript in Private Browsing mode.
+    // tor-browser#45157: Grant "access local files" permission to NoScript.
+    if (this.isNoScript) {
+      lazy.ExtensionPermissions.add(this.id, {
+        permissions: [PRIVATE_ALLOWED_PERMISSION, "internal:fileSchemeAllowed"],
+        origins: [],
+      });
+      this.permissions.add(PRIVATE_ALLOWED_PERMISSION);
+      this.permissions.add("internal:fileSchemeAllowed");
     }
 
     // We only want to update the SVG_CONTEXT_PROPERTIES_PERMISSION during
