9cfa64fdabc0976d3abe9edfcc55e62625e41050
Pier Angelo Vendrame <pierov@torproject.org>
2022-02-21 15:39:11 +0100

    TB 40458: Implement .tor.onion aliases
    We have enabled HTTPS-Only mode, therefore we do not need
    HTTPS-Everywhere anymore.
    However, we want to keep supporting .tor.onion aliases (especially for
    securedrop).
    Therefore, in this patch we implemented the parsing of HTTPS-Everywhere
    rulesets, and the redirect of .tor.onion domains.
    Actually, Tor Browser believes they are actual domains. We change them
    on the fly on the SOCKS proxy requests to resolve the domain, and on
    the code that verifies HTTPS certificates.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -48,6 +48,7 @@ ChromeUtils.defineESModuleGetters(lazy, {
   MigrationUtils: "resource:///modules/MigrationUtils.sys.mjs",
   NewTabUtils: "resource://gre/modules/NewTabUtils.sys.mjs",
   NimbusFeatures: "resource://nimbus/ExperimentAPI.sys.mjs",
+  OnionAliasStore: "resource:///modules/OnionAliasStore.sys.mjs",
   OnboardingMessageProvider:
     "resource:///modules/asrouter/OnboardingMessageProvider.sys.mjs",
   OsEnvironment: "resource://gre/modules/OsEnvironment.sys.mjs",
@@ -86,6 +87,8 @@ ChromeUtils.defineESModuleGetters(lazy, {
     "resource://gre/modules/TelemetryReportingPolicy.sys.mjs",
   TRRRacer: "resource:///modules/TRRPerformance.sys.mjs",
   TabCrashHandler: "resource:///modules/ContentCrashHandlers.sys.mjs",
+  TorConnect: "resource://gre/modules/TorConnect.sys.mjs",
+  TorConnectTopics: "resource://gre/modules/TorConnect.sys.mjs",
   TorProviderBuilder: "resource://gre/modules/TorProviderBuilder.sys.mjs",
   TorSettingsNotification:
     "resource:///modules/TorSettingsNotification.sys.mjs",
@@ -942,6 +945,19 @@ let JSWINDOWACTORS = {
 
     messageManagerGroups: ["browsers"],
     enablePreference: "accessibility.blockautorefresh",
+  },
+
+  Rulesets: {
+    parent: {
+      esModuleURI: "resource:///modules/RulesetsParent.sys.mjs",
+    },
+    child: {
+      esModuleURI: "resource:///modules/RulesetsChild.sys.mjs",
+      events: {
+        DOMWindowCreated: {},
+      },
+    },
+    matches: ["about:rulesets*"],
   },
 
   ScreenshotsComponent: {
@@ -2336,6 +2352,7 @@ BrowserGlue.prototype = {
         // can perform at-shutdown tasks later in shutdown.
         Services.fog;
       },
+      () => lazy.OnionAliasStore.uninit(),
     ];
 
     for (let task of tasks) {
@@ -2953,6 +2970,30 @@ BrowserGlue.prototype = {
         name: "webProtocolHandlerService.asyncInit",
         task: () => {
           lazy.WebProtocolHandlerRegistrar.prototype.init(true);
+        },
+      },
+
+      {
+        task: () => {
+          if (!lazy.TorConnect.shouldShowTorConnect) {
+            // we will take this path when the user is using the legacy tor launcher or
+            // when Tor Browser didn't launch its own tor.
+            lazy.OnionAliasStore.init();
+          } else {
+            // this path is taken when using about:torconnect, we wait to init
+            // after we are bootstrapped and connected to tor
+            const topic = lazy.TorConnectTopics.BootstrapComplete;
+            let bootstrapObserver = {
+              observe(aSubject, aTopic) {
+                if (aTopic === topic) {
+                  lazy.OnionAliasStore.init();
+                  // we only need to init once, so remove ourselves as an obvserver
+                  Services.obs.removeObserver(this, topic);
+                }
+              },
+            };
+            Services.obs.addObserver(bootstrapObserver, topic);
+          }
         },
       },
 
