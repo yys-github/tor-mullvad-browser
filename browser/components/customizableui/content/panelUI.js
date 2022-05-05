67cc92315232afd36408e469d513a4936f1ea61e
Pier Angelo Vendrame <pierov@torproject.org>
2022-05-05 20:15:01 +0200

    TB 11698: Incorporate Tor Browser Manual pages into Tor Browser
    This patch associates the about:manual page to a translated page that
    must be injected to browser/omni.ja after the build.
    The content must be placed in chrome/browser/content/browser/manual/, so
    that is then available at chrome://browser/content/manual/.
    We preferred giving absolute freedom to the web team, rather than having
    to change the patch in case of changes on the documentation.

--- browser/components/customizableui/content/panelUI.js
+++ browser/components/customizableui/content/panelUI.js
@@ -823,6 +823,12 @@ const PanelUI = {
       case "appMenu_helpPolicySupport":
         openTrustedLinkIn(Services.policies.getSupportMenu().URL.href, "tab");
         break;
+      case "appMenu_torBrowserUserManual":
+        gBrowser.selectedTab = gBrowser.addTab("about:manual", {
+          triggeringPrincipal:
+            Services.scriptSecurityManager.getSystemPrincipal(),
+        });
+        break;
     }
   },
 
