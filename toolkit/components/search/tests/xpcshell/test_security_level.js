f99698f016f518459a706a7f9eebff0624d6c349
Pier Angelo Vendrame <pierov@torproject.org>
2022-07-08 16:19:41 +0200

    BB 40925: Implemented the Security Level component
    This component adds a new Security Level toolbar button which visually
    indicates the current global security level via icon (as defined by the
    extensions.torbutton.security_slider pref), a drop-down hanger with a
    short description of the current security level, and a new section in
    the about:preferences#privacy page where users can change their current
    security level. In addition, the hanger and the preferences page will
    show a visual warning when the user has modified prefs associated with
    the security level and provide a one-click 'Restore Defaults' button to
    get the user back on recommended settings.
    
    Bug 40125: Expose Security Level pref in GeckoView

---
+++ toolkit/components/search/tests/xpcshell/test_security_level.js
@@ -0,0 +1,26 @@
+/* Any copyright is dedicated to the Public Domain.
+ * http://creativecommons.org/publicdomain/zero/1.0/ */
+
+/**
+ * This tests that we use the HTML version of DuckDuckGo when in the safest
+ * security level.
+ */
+
+"use strict";
+
+const { SearchService } = ChromeUtils.importESModule(
+  "moz-src:///toolkit/components/search/SearchService.sys.mjs"
+);
+
+const expectedURLs = {
+  ddg: "https://html.duckduckgo.com/html?q=test",
+};
+
+add_task(async function test_securityLevel() {
+  await SearchService.init();
+  for (const [id, url] of Object.entries(expectedURLs)) {
+    const engine = SearchService.getEngineById(id);
+    const foundUrl = engine.getSubmission("test").uri.spec;
+    Assert.equal(foundUrl, url, `${engine.name} is in HTML mode.`);
+  }
+});
