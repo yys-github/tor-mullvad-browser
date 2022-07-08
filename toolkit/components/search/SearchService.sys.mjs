0f21b6f1332422b1d19606e671bb1a96f83f315f
Pier Angelo Vendrame <pierov@torproject.org>
2022-07-08 16:19:41 +0200

    BB 40925: Implemented the Security Level component
    This component adds a new Security Level toolbar button which visually
    indicates the current global security level via icon (as defined by the
    extensions.torbutton.security_slider pref), a drop-down hanger with a
    short description of the current security level, and a new section in
    the about:preferences#privacy page where users can change their current
    security level. In addition, the hanger and the preferences page will
    show a visual warning when the user has modified prefs associated with
    the security level and provide a one-click 'Restore Defaults' button to
    get the user back on recommended settings.
    
    Bug 40125: Expose Security Level pref in GeckoView

--- toolkit/components/search/SearchService.sys.mjs
+++ toolkit/components/search/SearchService.sys.mjs
@@ -24,6 +24,7 @@ ChromeUtils.defineESModuleGetters(lazy, {
     "moz-src:///toolkit/components/search/PolicySearchEngine.sys.mjs",
   Region: "resource://gre/modules/Region.sys.mjs",
   RemoteSettings: "resource://services-settings/remote-settings.sys.mjs",
+  SecurityLevelPrefs: "resource://gre/modules/SecurityLevel.sys.mjs",
   SearchEngine: "moz-src:///toolkit/components/search/SearchEngine.sys.mjs",
   SearchEngineSelector:
     "moz-src:///toolkit/components/search/SearchEngineSelector.sys.mjs",
@@ -71,6 +72,7 @@ ChromeUtils.defineLazyGetter(lazy, "defaultOverrideAllowlist", () => {
 
 const TOPIC_LOCALES_CHANGE = "intl:app-locales-changed";
 const QUIT_APPLICATION_TOPIC = "quit-application";
+const TOPIC_JSENABLED_CHANGED = "SecurityLevel:JavascriptEnabledChanged";
 
 // The update timer for OpenSearch engines checks in once a day.
 const OPENSEARCH_UPDATE_TIMER_TOPIC = "search-engine-update-timer";
@@ -2643,6 +2645,7 @@ export class SearchService {
       channel: lazy.SearchUtils.MODIFIED_APP_CHANNEL,
       experiment: this._experimentPrefValue,
       distroID: lazy.SearchUtils.distroID ?? "",
+      javascriptEnabled: lazy.SecurityLevelPrefs.javascriptEnabled,
     };
 
     for (let [key, value] of Object.entries(searchEngineSelectorProperties)) {
@@ -3536,6 +3539,7 @@ export class SearchService {
     Services.obs.addObserver(this, lazy.SearchUtils.TOPIC_ENGINE_MODIFIED);
     Services.obs.addObserver(this, QUIT_APPLICATION_TOPIC);
     Services.obs.addObserver(this, TOPIC_LOCALES_CHANGE);
+    Services.obs.addObserver(this, TOPIC_JSENABLED_CHANGED);
 
     this._settings.addObservers();
 
@@ -3598,6 +3602,7 @@ export class SearchService {
     Services.obs.removeObserver(this, QUIT_APPLICATION_TOPIC);
     Services.obs.removeObserver(this, TOPIC_LOCALES_CHANGE);
     Services.obs.removeObserver(this, lazy.Region.REGION_TOPIC);
+    Services.obs.removeObserver(this, TOPIC_JSENABLED_CHANGED);
   }
 
   QueryInterface = ChromeUtils.generateQI([
@@ -3675,6 +3680,13 @@ export class SearchService {
         lazy.logConsole.debug("Region updated:", lazy.Region.home);
         this._maybeReloadEngines(
           Ci.nsISearchService.CHANGE_REASON_REGION
+        ).catch(console.error);
+        break;
+
+      case TOPIC_JSENABLED_CHANGED:
+        lazy.logConsole.debug("JavaScript toggled");
+        this._maybeReloadEngines(
+          Ci.nsISearchService.CHANGE_REASON_CONFIG
         ).catch(console.error);
         break;
     }
