32a1596d7729964385516c99f1c1d844c426d9b7
Pier Angelo Vendrame <pierov@torproject.org>
2022-07-08 16:19:41 +0200

    BB 40925: Implemented the Security Level component
    This component adds a new Security Level toolbar button which visually
    indicates the current global security level via icon (as defined by the
    extensions.torbutton.security_slider pref), a drop-down hanger with a
    short description of the current security level, and a new section in
    the about:preferences#privacy page where users can change their current
    security level. In addition, the hanger and the preferences page will
    show a visual warning when the user has modified prefs associated with
    the security level and provide a one-click 'Restore Defaults' button to
    get the user back on recommended settings.
    
    Bug 40125: Expose Security Level pref in GeckoView

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -62,6 +62,13 @@ ChromeUtils.defineLazyGetter(lazy, "gParentalControlsService", () =>
     : null
 );
 
+// TODO: module import via ChromeUtils.defineModuleGetter
+XPCOMUtils.defineLazyScriptGetter(
+  this,
+  ["SecurityLevelPreferences"],
+  "chrome://browser/content/securitylevel/securityLevel.js"
+);
+
 XPCOMUtils.defineLazyServiceGetter(
   lazy,
   "TrackingDBService",
@@ -1957,6 +1964,16 @@ var gPrivacyPane = {
   _pane: null,
 
   /**
+   * Show the Security Level UI
+   */
+  _initSecurityLevel() {
+    SecurityLevelPreferences.init();
+    window.addEventListener("unload", () => SecurityLevelPreferences.uninit(), {
+      once: true,
+    });
+  },
+
+  /**
    * Whether the prompt to restart Firefox should appear when changing the autostart pref.
    */
   _shouldPromptForRestart: true,
@@ -2457,6 +2474,7 @@ var gPrivacyPane = {
     this._initTrackingProtectionExtensionControl();
     this._ensureTrackingProtectionExceptionListMigration();
     this._initProfilesInfo();
+    this._initSecurityLevel();
 
     Preferences.get("privacy.trackingprotection.enabled").on(
       "change",
