ef59954535c08a7b64fe56ba8c43932334096e04
Pier Angelo Vendrame <pierov@torproject.org>
2022-07-25 10:40:35 +0200

    TB 40926: Implemented the New Identity feature

--- browser/base/content/browser-sets.js
+++ browser/base/content/browser-sets.js
@@ -298,6 +298,9 @@ document.addEventListener(
           case "zoomWindow":
             zoomWindow();
             break;
+          case "cmd_newIdentity":
+            NewIdentityButton.onCommand(event);
+            break;
         }
       });
 
--- browser/components/customizableui/CustomizeMode.sys.mjs
+++ browser/components/customizableui/CustomizeMode.sys.mjs
@@ -252,6 +252,7 @@ export class CustomizeMode {
     "Browser:NewUserContextTab",
     "Tools:PrivateBrowsing",
     "zoomWindow",
+    "cmd_newIdentity",
   ]);
 
   /**
