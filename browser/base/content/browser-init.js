1b6584a26e81e8a210f602d7cc5e849edc03436b
Pier Angelo Vendrame <pierov@torproject.org>
2022-07-25 10:40:35 +0200

    BB 40926: Implemented the New Identity feature

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -271,6 +271,9 @@ var gBrowserInit = {
     // Init the SecurityLevelButton
     SecurityLevelButton.init();
 
+    // Init the NewIdentityButton
+    NewIdentityButton.init();
+
     // Certain kinds of automigration rely on this notification to complete
     // their tasks BEFORE the browser window is shown. SessionStore uses it to
     // restore tabs into windows AFTER important parts like gMultiProcessBrowser
@@ -1074,6 +1077,8 @@ var gBrowserInit = {
     DownloadsButton.uninit();
 
     SecurityLevelButton.uninit();
+
+    NewIdentityButton.uninit();
 
     if (gToolbarKeyNavEnabled) {
       ToolbarKeyboardNavigator.uninit();
