fdcdebeade5a6146416942fd5dd756944d2dc3bb
Pier Angelo Vendrame <pierov@torproject.org>
2022-07-25 10:40:35 +0200

    BB 40926: Implemented the New Identity feature

--- browser/base/content/browser-sets.js
+++ browser/base/content/browser-sets.js
@@ -269,6 +269,9 @@ document.addEventListener(
           case "zoomWindow":
             zoomWindow();
             break;
+          case "cmd_newIdentity":
+            NewIdentityButton.onCommand(event);
+            break;
         }
       });
 
