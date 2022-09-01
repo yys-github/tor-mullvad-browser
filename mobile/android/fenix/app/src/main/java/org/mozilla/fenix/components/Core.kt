1081899e3a2f2d8c36549a36f4b6a961cc4e35fa
hackademix <giorgio@maone.net>
2022-09-01 16:30:50 +0200

    [android] Modify add-on support
    Bug 41160: One-time ultimate switch Tor Browser Android to HTTPS-Only.
    Bug 41159: Remove HTTPS-Everywhere extension from Tor Browser Android.
    
    Bug 41094: Enable HTTPS-Only Mode by default in Tor Browser Android.
    
    Turn shouldUseHttpsOnly's default to true.
    
    Bug 40225: Bundled extensions don't get updated with Android Tor
               Browser updates.
    
    Bug 40030: Install NoScript addon on startup.
    
    Also 40070: Consider storing the list of recommended addons
    
    This implements our own AddonsProvider, which loads the list of
    available addons from assets instead of fetching it from an
    endpoint.
    
    Also, we hide the uninstall button for builtin addons.
    
    Bug 40058: Hide option for disallowing addon in private mode

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
@@ -195,6 +195,8 @@ class Core(
             if (Config.channel.isNightlyOrDebug || Config.channel.isBeta) {
                 WebCompatReporterFeature.install(it, "fenix")
             }
+
+            TorBrowserFeatures.install(context, it)
         }
     }
 
