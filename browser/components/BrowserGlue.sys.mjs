36eadc2a1dee4856af6cd64984b6419cd0010806
Pier Angelo Vendrame <pierov@torproject.org>
2022-10-10 15:13:04 +0200

    TB 40933: Add tor-launcher functionality
    Bug 41926: Reimplement the control port

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -96,6 +96,7 @@ ChromeUtils.defineESModuleGetters(lazy, {
   TabCrashHandler: "resource:///modules/ContentCrashHandlers.sys.mjs",
   TabUnloader: "resource:///modules/TabUnloader.sys.mjs",
   TelemetryUtils: "resource://gre/modules/TelemetryUtils.sys.mjs",
+  TorProviderBuilder: "resource://gre/modules/TorProviderBuilder.sys.mjs",
   UIState: "resource://services-sync/UIState.sys.mjs",
   UrlbarPrefs: "resource:///modules/UrlbarPrefs.sys.mjs",
   WebChannel: "resource://gre/modules/WebChannel.sys.mjs",
@@ -1998,6 +1999,8 @@ BrowserGlue.prototype = {
     lazy.PageActions.init();
 
     lazy.DoHController.init();
+
+    lazy.TorProviderBuilder.firstWindowLoaded();
 
     ClipboardPrivacy.startup();
 
