bb888c415fc33fa45bbf9483cb440b1aad4cc04e
hackademix <giorgio@maone.net>
2022-12-12 21:09:15 +0100

    TB 8324: Prevent DNS proxy bypasses caused by Drag&Drop
    Bug 41613: Skip Drang & Drop filtering for DNS-safe URLs

--- toolkit/components/places/PlacesUtils.sys.mjs
+++ toolkit/components/places/PlacesUtils.sys.mjs
@@ -12,6 +12,7 @@ const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   Bookmarks: "resource://gre/modules/Bookmarks.sys.mjs",
   History: "resource://gre/modules/History.sys.mjs",
+  OpaqueDrag: "moz-src:///toolkit/modules/DragDropFilter.sys.mjs",
   PlacesSyncUtils: "resource://gre/modules/PlacesSyncUtils.sys.mjs",
   Sqlite: "resource://gre/modules/Sqlite.sys.mjs",
 });
@@ -1230,6 +1231,9 @@ export var PlacesUtils = {
     /** @type {{uri: string, title: string, type: string}[]} */
     let validNodes = [];
     let invalidNodes = [];
+    if (type === "application/x-torbrowser-opaque") {
+      ({ value: blob, type } = lazy.OpaqueDrag.retrieve(blob));
+    }
     switch (type) {
       case this.TYPE_X_MOZ_PLACE:
       case this.TYPE_X_MOZ_PLACE_SEPARATOR:
