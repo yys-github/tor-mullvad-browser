f1144a43f712988041e54fece69fbcd2214b89a8
hackademix <giorgio@maone.net>
2022-12-12 21:09:15 +0100

    TB 8324: Prevent DNS proxy bypasses caused by Drag&Drop
    Bug 41613: Skip Drang & Drop filtering for DNS-safe URLs

--- browser/components/places/content/controller.js
+++ browser/components/places/content/controller.js
@@ -1277,6 +1277,7 @@ PlacesController.prototype = {
     [
       PlacesUtils.TYPE_X_MOZ_PLACE,
       PlacesUtils.TYPE_X_MOZ_URL,
+      "application/x-torbrowser-opaque",
       PlacesUtils.TYPE_PLAINTEXT,
     ].forEach(type => xferable.addDataFlavor(type));
 
