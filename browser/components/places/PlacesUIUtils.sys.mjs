dee74eb40bc37e82a14be74fa7b830be1e23fbd1
hackademix <giorgio@maone.net>
2022-12-12 21:09:15 +0100

    TB 8324: Prevent DNS proxy bypasses caused by Drag&Drop
    Bug 41613: Skip Drang & Drop filtering for DNS-safe URLs

--- browser/components/places/PlacesUIUtils.sys.mjs
+++ browser/components/places/PlacesUIUtils.sys.mjs
@@ -1774,7 +1774,11 @@ ChromeUtils.defineLazyGetter(PlacesUIUtils, "URI_FLAVORS", () => {
   ];
 });
 ChromeUtils.defineLazyGetter(PlacesUIUtils, "SUPPORTED_FLAVORS", () => {
-  return [...PlacesUIUtils.PLACES_FLAVORS, ...PlacesUIUtils.URI_FLAVORS];
+  return [
+    ...PlacesUIUtils.PLACES_FLAVORS,
+    ...PlacesUIUtils.URI_FLAVORS,
+    "application/x-torbrowser-opaque",
+  ];
 });
 
 ChromeUtils.defineLazyGetter(PlacesUIUtils, "promptLocalization", () => {
