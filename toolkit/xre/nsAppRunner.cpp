024ba15e0bb02a61de49126c97c7d3b88a0c00cc
Pier Angelo Vendrame <pierov@torproject.org>
2023-01-27 17:33:29 +0100

    BB 9173: Change the default Firefox profile directory to be relative.
    This commit makes Firefox look for the default profile directory in a
    directory relative to the binary path.
    The directory can be specified through the --with-relative-data-dir.
    This is relative to the same directory as the firefox main binary for
    Linux and Windows.
    
    On macOS, we remove Contents/MacOS from it.
    Or, in other words, the directory is relative to the application
    bundle.
    
    This behavior can be overriden at runtime, by placing a file called
    system-install adjacent to the firefox main binary (also on macOS).

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -2746,6 +2746,8 @@ static nsresult ProfileMissingDialog(nsINativeAppSupport* aNative) {
 #endif  // MOZ_WIDGET_ANDROID
 }
 
+// If aUnlocker is NULL, it is also OK for the following arguments to be NULL:
+//   aProfileDir, aProfileLocalDir, aResult.
 static ReturnAbortOnError ProfileLockedDialog(nsIFile* aProfileDir,
                                               nsIFile* aProfileLocalDir,
                                               nsIProfileUnlocker* aUnlocker,
@@ -2759,10 +2761,12 @@ static ReturnAbortOnError ProfileLockedDialog(nsIFile* aProfileDir,
   gStartupLock = nullptr;
 #endif
 
-  bool exists;
-  aProfileDir->Exists(&exists);
-  if (!exists) {
-    return ProfileMissingDialog(aNative);
+  if (aProfileDir) {
+    bool exists;
+    aProfileDir->Exists(&exists);
+    if (!exists) {
+      return ProfileMissingDialog(aNative);
+    }
   }
 
   ScopedXPCOMStartup xpcom;
@@ -2772,7 +2776,7 @@ static ReturnAbortOnError ProfileLockedDialog(nsIFile* aProfileDir,
 #if defined(MOZ_TELEMETRY_REPORTING)
   // We cannot check if telemetry has been disabled by the user, yet.
   // So, rely on the build time settings, instead.
-  mozilla::Telemetry::WriteFailedProfileLock(aProfileDir);
+  if (aProfileDir) mozilla::Telemetry::WriteFailedProfileLock(aProfileDir);
 #endif
 
   rv = xpcom.SetWindowCreator(aNative);
