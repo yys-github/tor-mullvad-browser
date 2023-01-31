c71a046c3e0c759f70e5cba9b2d33eea12a1d022
Pier Angelo Vendrame <pierov@torproject.org>
2023-01-31 11:59:21 +0100

    TB 13252: Customize profile management on macOS
    On macOS we allow both portable mode and system installation.
    However, in the latter case, we customize Firefox's directories to
    match the hierarchy we use for the portable mode.
    
    Also, display an informative error message if the TorBrowser-Data
    directory cannot be created due to an "access denied" or a
    "read only volume" error.

--- xpcom/io/nsAppFileLocationProvider.cpp
+++ xpcom/io/nsAppFileLocationProvider.cpp
@@ -306,6 +306,17 @@ nsresult nsAppFileLocationProvider::GetProductDirectory(nsIFile** aLocalFile,
     return rv;
   }
   localDir = localDirMac.forget();
+
+#  if defined(TOR_BROWSER)
+  rv = localDir->AppendNative("TorBrowser-Data"_ns);
+  NS_ENSURE_SUCCESS(rv, rv);
+  rv = localDir->AppendNative("Browser"_ns);
+  NS_ENSURE_SUCCESS(rv, rv);
+  if (aLocal) {
+    rv = localDir->AppendNative("Caches"_ns);
+    NS_ENSURE_SUCCESS(rv, rv);
+  }
+#  endif
 #elif defined(XP_WIN)
   nsCOMPtr<nsIProperties> directoryService =
       do_GetService(NS_DIRECTORY_SERVICE_CONTRACTID, &rv);
@@ -328,10 +339,12 @@ nsresult nsAppFileLocationProvider::GetProductDirectory(nsIFile** aLocalFile,
 #  error dont_know_how_to_get_product_dir_on_your_platform
 #endif
 
+#if !defined(TOR_BROWSER)
   rv = localDir->AppendRelativeNativePath(DEFAULT_PRODUCT_DIR);
   if (NS_FAILED(rv)) {
     return rv;
   }
+#endif
   rv = localDir->Exists(&exists);
 
   if (NS_SUCCEEDED(rv) && !exists) {
@@ -372,10 +385,13 @@ nsresult nsAppFileLocationProvider::GetDefaultUserProfileRoot(
 
 #if defined(MOZ_WIDGET_COCOA) || defined(XP_WIN)
   // These 3 platforms share this part of the path - do them as one
+#  ifndef TOR_BROWSER
+  // Legacy: we do not use "Profiles" on Tor Browser.
   rv = localDir->AppendRelativeNativePath("Profiles"_ns);
   if (NS_FAILED(rv)) {
     return rv;
   }
+#  endif
 
   bool exists;
   rv = localDir->Exists(&exists);
