9c5dd14bbea6c5a7efbc3c797142a591b06e65d4
Henry Wilkes <henry@torproject.org>
2023-02-03 16:57:17 +0000

    BB 31575: Disable Firefox Home (Activity Stream)
    Treat about:blank as the default home page and new tab page.
    
    Avoid loading AboutNewTab in BrowserGlue.sys.mjs in order
    to avoid several network requests that we do not need.
    
    Bug 41624: Disable about:pocket-* pages.
    
    Bug 40144: Redirect about:privatebrowsing to the user's home

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -67,7 +67,6 @@ ChromeUtils.defineESModuleGetters(this, {
   ResetPBMPanel: "resource:///modules/ResetPBMPanel.sys.mjs",
   SafeBrowsing: "resource://gre/modules/SafeBrowsing.sys.mjs",
   Sanitizer: "resource:///modules/Sanitizer.sys.mjs",
-  SaveToPocket: "chrome://pocket/content/SaveToPocket.sys.mjs",
   ScreenshotsUtils: "resource:///modules/ScreenshotsUtils.sys.mjs",
   SearchUIUtils: "resource:///modules/SearchUIUtils.sys.mjs",
   SessionStartup: "resource:///modules/sessionstore/SessionStartup.sys.mjs",
@@ -3390,8 +3389,6 @@ var XULBrowserWindow = {
     BrowserPageActions.onLocationChange();
 
     SafeBrowsingNotificationBox.onLocationChange(aLocationURI);
-
-    SaveToPocket.onLocationChange(window);
 
     UrlbarProviderSearchTips.onLocationChange(
       window,
