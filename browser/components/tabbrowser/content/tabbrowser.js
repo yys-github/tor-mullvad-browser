a7432e4d02ee071e75334f6392896ec965a9083a
Henry Wilkes <henry@torproject.org>
2023-02-03 16:57:17 +0000

    BB 31575: Disable Firefox Home (Activity Stream)
    Treat about:blank as the default home page and new tab page.
    
    BB 43886: Disable the newtab component/addon.
    
    Bug 41624: Disable about:pocket-* pages.
    
    Bug 40144: Redirect about:privatebrowsing to the user's home

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -108,8 +108,6 @@
         PictureInPicture: "resource://gre/modules/PictureInPicture.sys.mjs",
         // SmartTabGrouping.sys.mjs is missing. tor-browser#44045.
         // Unused in this context. See mozilla bug 1981785.
-        SponsorProtection:
-          "moz-src:///browser/components/newtab/SponsorProtection.sys.mjs",
         TabMetrics:
           "moz-src:///browser/components/tabbrowser/TabMetrics.sys.mjs",
         TabStateFlusher:
@@ -7618,10 +7616,6 @@
           debugStringArray.push("[A]");
         }
 
-        if (this.SponsorProtection.isProtectedBrowser(tab.linkedBrowser)) {
-          debugStringArray.push("[S]");
-        }
-
         if (debugStringArray.length) {
           labelArray.push(debugStringArray.join(" "));
         }
@@ -9175,10 +9169,6 @@
       { loadFlags, globalHistoryOptions }
     ) {
       if (globalHistoryOptions?.triggeringSponsoredURL) {
-        if (globalHistoryOptions.triggeringSource == "newtab") {
-          gBrowser.SponsorProtection.addProtectedBrowser(browser);
-        }
-
         try {
           // Browser may access URL after fixing it up, then store the URL into DB.
           // To match with it, fix the link up explicitly.
@@ -9199,8 +9189,6 @@
             globalHistoryOptions.triggeringSource
           );
         } catch (e) {}
-      } else {
-        gBrowser.SponsorProtection.removeProtectedBrowser(browser);
       }
 
       if (globalHistoryOptions?.triggeringSearchEngine) {
