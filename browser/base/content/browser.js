19c00a089d77386d051c47b5388e44c70cc1ae16
Henry Wilkes <henry@torproject.org>
2023-02-13 13:47:12 +0000

    MB 39: Add home page about:mullvad-browser

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -705,6 +705,7 @@ async function gLazyFindCommand(cmd, ...args) {
 }
 
 var gPageIcons = {
+  "about:mullvad-browser": "chrome://branding/content/icon32.png",
   "about:home": "chrome://branding/content/icon32.png",
   "about:newtab": "chrome://branding/content/icon32.png",
   "about:welcome": "chrome://branding/content/icon32.png",
@@ -712,6 +713,7 @@ var gPageIcons = {
 };
 
 var gInitialPages = [
+  "about:mullvad-browser",
   "about:blank",
   "about:home",
   "about:firefoxview",
