d92e290a30576526138066014af8614c89e6de1e
Henry Wilkes <henry@torproject.org>
2023-03-08 14:37:38 +0000

    TB 41600: Add a tor circuit display panel.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -298,6 +298,11 @@ XPCOMUtils.defineLazyScriptGetter(
   "gProfiles",
   "chrome://browser/content/browser-profiles.js"
 );
+XPCOMUtils.defineLazyScriptGetter(
+  this,
+  ["gTorCircuitPanel"],
+  "chrome://browser/content/torCircuitPanel.js"
+);
 
 // lazy service getters
 
