d1496060a8d3e1522e657b1c720bc37aa2c6191e
Henry Wilkes <henry@torproject.org>
2023-03-08 14:37:38 +0000

    TB 41600: Add a tor circuit display panel.

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -232,6 +232,8 @@ var gBrowserInit = {
     // Init the NewIdentityButton
     NewIdentityButton.init();
 
+    gTorCircuitPanel.init();
+
     // Certain kinds of automigration rely on this notification to complete
     // their tasks BEFORE the browser window is shown. SessionStore uses it to
     // restore tabs into windows AFTER important parts like gMultiProcessBrowser
@@ -1040,6 +1042,8 @@ var gBrowserInit = {
     SecurityLevelButton.uninit();
 
     NewIdentityButton.uninit();
+
+    gTorCircuitPanel.uninit();
 
     if (gToolbarKeyNavEnabled) {
       ToolbarKeyboardNavigator.uninit();
