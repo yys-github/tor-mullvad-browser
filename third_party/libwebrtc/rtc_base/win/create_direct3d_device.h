38d0b120b867f04b7952c3a2959279d243a9a423
Marco Simonelli <msimonelli@protonmail.com>
2023-03-10 11:50:33 +0000

    BB 41459: WebRTC fails to build under mingw (Part 2)
    - fixes required to build third_party/libwebrtc

--- third_party/libwebrtc/rtc_base/win/create_direct3d_device.h
+++ third_party/libwebrtc/rtc_base/win/create_direct3d_device.h
@@ -11,8 +11,9 @@
 #ifndef RTC_BASE_WIN_CREATE_DIRECT3D_DEVICE_H_
 #define RTC_BASE_WIN_CREATE_DIRECT3D_DEVICE_H_
 
-#include <windows.graphics.directX.direct3d11.h>
-#include <windows.graphics.directX.direct3d11.interop.h>
+#include <windows.graphics.directx.direct3d11.h>
+#include <windows.graphics.directx.direct3d11.interop.h>
+
 #include <winerror.h>
 #include <wrl/client.h>
 
