ce719e8a9c6b2d806c5d06b43167e2ac48c4bcf4
Marco Simonelli <msimonelli@protonmail.com>
2023-03-10 11:50:33 +0000

    BB 41459: WebRTC fails to build under mingw (Part 2)
    - fixes required to build third_party/libwebrtc

--- dom/media/webrtc/libwebrtc_overrides/modules/desktop_capture/desktop_capture_types.h
+++ dom/media/webrtc/libwebrtc_overrides/modules/desktop_capture/desktop_capture_types.h
@@ -5,9 +5,12 @@
 #ifndef DOM_MEDIA_WEBRTC_LIBWEBRTCOVERRIDES_MODULES_DESKTOP_CAPTURE_DESKTOP_CAPTURE_TYPES_H_
 #define DOM_MEDIA_WEBRTC_LIBWEBRTCOVERRIDES_MODULES_DESKTOP_CAPTURE_DESKTOP_CAPTURE_TYPES_H_
 
-#ifdef XP_WIN       // Moving this into the global namespace
-typedef int pid_t;  // matching what used to be in
-#endif              // video_capture_defines.h
+// pid_t
+#if !defined(XP_WIN) || defined(__MINGW32__)
+#  include <sys/types.h>
+#else
+typedef int pid_t;
+#endif
 
 #include "../../third_party/libwebrtc/modules/desktop_capture/desktop_capture_types.h"
 
--- third_party/libwebrtc/modules/desktop_capture/win/desktop_capture_utils.cc
+++ third_party/libwebrtc/modules/desktop_capture/win/desktop_capture_utils.cc
@@ -12,7 +12,9 @@
 
 #include <string>
 
-#include "rtc_base/strings/string_builder.h"
+#include <cstdio>
+#include <cstdlib>
+#include "stringapiset.h"
 
 namespace webrtc {
 namespace desktop_capture {
@@ -21,11 +23,16 @@ namespace utils {
 // Generates a human-readable string from a COM error.
 std::string ComErrorToString(const _com_error& error) {
   webrtc::StringBuilder string_builder;
-  // Use _bstr_t to simplify the wchar to char conversion for ErrorMessage().
-  _bstr_t error_message(error.ErrorMessage());
-  string_builder.AppendFormat("HRESULT: 0x%08X, Message: %s", error.Error(),
-                              static_cast<const char*>(error_message));
-  return string_builder.str();
+  string_builder.AppendFormat("HRESULT: 0x%08X, Message: ", error.Error());
+#ifdef _UNICODE
+  WideCharToMultiByte(CP_UTF8, 0, error.ErrorMessage(), -1,
+                      buffer + string_builder.size(),
+                      sizeof(buffer) - string_builder.size(), nullptr, nullptr);
+  buffer[sizeof(buffer) - 1] = 0;
+#else
+  string_builder << error.ErrorMessage();
+#endif
+  return buffer;
 }
 
 }  // namespace utils
