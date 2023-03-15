bb9ab1465541e3ba8e00ef081fb8aa8871491c3f
Pier Angelo Vendrame <pierov@torproject.org>
2023-03-15 14:56:56 +0100

    MB 21: Disable the password manager
    This commit disables the about:login page and removes the "Login and
    Password" section of about:preferences.
    
    We do not do anything to the real password manager of Firefox, that is
    in toolkit: it contains C++ parts that make it difficult to actually
    prevent it from being built..
    
    Finally, we modify the the function that opens about:login to report an
    error in the console so that we can quickly get a backtrace to the code
    that tries to use it.

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -75,6 +75,7 @@ static const RedirEntry kRedirMap[] = {
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_CAN_LOAD_IN_CHILD |
          nsIAboutModule::HIDE_FROM_ABOUTABOUT},
+#ifndef MULLVAD_BROWSER
     {"logins", "chrome://browser/content/aboutlogins/aboutLogins.html",
      nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
          nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
@@ -86,6 +87,7 @@ static const RedirEntry kRedirMap[] = {
          nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
          nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::IS_SECURE_CHROME_UI},
+#endif
 // Drop about:opentabs. tor-browser#44431.
 #ifndef BASE_BROWSER_VERSION
     {"opentabs", "chrome://browser/content/tabbrowser/opentabs.html",
