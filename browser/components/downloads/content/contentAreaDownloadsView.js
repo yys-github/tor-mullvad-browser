cdec8f330c0b4199c2bcb9f35ceff8c7f1e49b27
Dan Ballard <dan@mindstab.net>
2023-03-31 12:35:17 -0500

    TB 40701: Add security warning when downloading a file
    Shown in the downloads panel, about:downloads and places.xhtml.

--- browser/components/downloads/content/contentAreaDownloadsView.js
+++ browser/components/downloads/content/contentAreaDownloadsView.js
@@ -8,18 +8,52 @@ const { PrivateBrowsingUtils } = ChromeUtils.importESModule(
   "resource://gre/modules/PrivateBrowsingUtils.sys.mjs"
 );
 
+const { DownloadsTorWarning } = ChromeUtils.importESModule(
+  "resource:///modules/DownloadsTorWarning.sys.mjs"
+);
+
 var ContentAreaDownloadsView = {
   init() {
     let box = document.getElementById("downloadsListBox");
+
+    const torWarning = new DownloadsTorWarning(
+      document.getElementById("aboutDownloadsTorWarning"),
+      false,
+      () => {
+        // Try and focus the downloads list.
+        // NOTE: If #downloadsListBox is still hidden, this will do nothing.
+        // But in this case there are no other focusable targets within the
+        // view, so we just leave it up to the focus handler.
+        box.focus({ preventFocusRing: true });
+      }
+    );
+    torWarning.activate();
+    window.addEventListener("unload", () => {
+      torWarning.deactivate();
+    });
+
     let suppressionFlag = DownloadsCommon.SUPPRESS_CONTENT_AREA_DOWNLOADS_OPEN;
     box.addEventListener(
       "InitialDownloadsLoaded",
       () => {
         // Set focus to Downloads list once it is created
         // And prevent it from showing the focus ring around the richlistbox (Bug 1702694)
-        document
-          .getElementById("downloadsListBox")
-          .focus({ focusVisible: false });
+        // Prevent focusing the list whilst the tor browser warning is shown.
+        // Some screen readers (tested with Orca and NVDA) will not read out
+        // alerts if they are already present on page load. In that case, a
+        // screen reader user may not be aware of the warning before they
+        // interact with the downloads list, which we do not want.
+        // Some hacky workarounds were tested with Orca to get it to read back
+        // the alert before the focus is read, but this was inconsistent and the
+        // experience was bad.
+        // Without auto-focusing the downloads list, a screen reader should not
+        // skip beyond the alert's content.
+        if (torWarning.hidden) {
+          document
+            .getElementById("downloadsListBox")
+            .focus({ focusVisible: false });
+        }
+
         // Pause the indicator if the browser is active.
         if (document.visibilityState === "visible") {
           DownloadsCommon.getIndicatorData(window).attentionSuppressed |=
