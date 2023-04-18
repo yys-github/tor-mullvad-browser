2030069b30a025472720029dfc898220e2246cea
hackademix <giorgio@maone.net>
2023-04-18 22:50:11 +0200

    Bug 41728: Pin bridges.torproject.org domains to Let's Encrypt's root cert public key

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -284,6 +284,14 @@ static const StaticFingerprints kPinset_google = {
   kPinset_google_Data
 };
 
+static const char* const kPinset_tor_browser_Data[] = {
+  kISRG_Root_X1Fingerprint,
+};
+static const StaticFingerprints kPinset_tor_browser = {
+  sizeof(kPinset_tor_browser_Data) / sizeof(const char*),
+  kPinset_tor_browser_Data
+};
+
 static const char* const kPinset_facebook_Data[] = {
   kCOMODO_ECC_Certification_AuthorityFingerprint,
   kISRG_Root_X1Fingerprint,
@@ -340,6 +348,7 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
   { "aus5.mozilla.org", true, true, true, 7, &kPinset_mozilla_services },
   { "blogger.com", true, false, false, -1, &kPinset_google_root_pems },
   { "blogspot.com", true, false, false, -1, &kPinset_google_root_pems },
+  { "bridges.torproject.org", false, false, false, -1, &kPinset_tor_browser },
   { "bugs.chromium.org", true, false, false, -1, &kPinset_google_root_pems },
   { "build.chromium.org", true, false, false, -1, &kPinset_google_root_pems },
   { "business.facebook.com", true, false, false, -1, &kPinset_facebook },
