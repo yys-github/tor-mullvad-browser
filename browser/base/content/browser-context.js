76dd33a2cab88af182674be41ab9e84b518cbf71
cypherpunks1 <2478-cypherpunks1@gitlab.torproject.org>
2023-05-30 15:42:09 -0800

    BB 33955: When copying an image only copy the image contents to the clipboard

--- browser/base/content/browser-context.js
+++ browser/base/content/browser-context.js
@@ -134,7 +134,7 @@ document.addEventListener(
           gContextMenu.saveMedia();
           break;
         case "context-copyimage-contents":
-          goDoCommand("cmd_copyImage");
+          goDoCommand("cmd_copyImageContents");
           break;
         case "context-copyaudiourl":
         case "context-copyimage":
