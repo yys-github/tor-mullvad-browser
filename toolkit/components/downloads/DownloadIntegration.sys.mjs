de57afb92e34599a041c02b700363c93b71ac796
hackademix <giorgio@maone.net>
2023-07-05 17:05:40 +0200

    BB 41854: Allow overriding download spam protection.

--- toolkit/components/downloads/DownloadIntegration.sys.mjs
+++ toolkit/components/downloads/DownloadIntegration.sys.mjs
@@ -1466,10 +1466,7 @@ var DownloadObserver = {
       case "blocked-automatic-download":
         if (AppConstants.MOZ_BUILD_APP == "browser") {
           DownloadIntegration._initializeDownloadSpamProtection();
-          DownloadIntegration.downloadSpamProtection.update(
-            aData,
-            aSubject.topChromeWindow
-          );
+          DownloadIntegration.downloadSpamProtection.update(aData, aSubject);
         }
         break;
     }
