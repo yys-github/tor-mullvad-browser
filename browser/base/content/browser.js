b027afd6a057dd44f71986aa9649739a64ece944
Henry Wilkes <henry@torproject.org>
2023-08-02 12:18:08 +0100

    TB 7494: Create local home page for TBB.
    Bug 41333: Update about:tor to new design. Including:
    + make the favicon match the branding icon.
    + make the location bar show a search icon.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -740,6 +740,7 @@ async function gLazyFindCommand(cmd, ...args) {
 }
 
 var gPageIcons = {
+  "about:tor": "chrome://branding/content/icon32.png",
   "about:home": "chrome://branding/content/icon32.png",
   "about:newtab": "chrome://branding/content/icon32.png",
   "about:welcome": "chrome://branding/content/icon32.png",
@@ -747,6 +748,7 @@ var gPageIcons = {
 };
 
 var gInitialPages = [
+  "about:tor",
   "about:torconnect",
   "about:blank",
   "about:home",
