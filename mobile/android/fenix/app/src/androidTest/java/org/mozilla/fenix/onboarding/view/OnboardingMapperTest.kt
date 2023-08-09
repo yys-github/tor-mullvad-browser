808be1edf1ae0dad5409017be487bfb4608e2a9a
Dan Ballard <dan@mindstab.net>
2023-08-09 13:32:54 -0700

    TB 41972: [android] Disable Mozilla onboarding

--- mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/onboarding/view/OnboardingMapperTest.kt
+++ mobile/android/fenix/app/src/androidTest/java/org/mozilla/fenix/onboarding/view/OnboardingMapperTest.kt
@@ -429,7 +429,7 @@ class OnboardingMapperTest {
 
 private val defaultBrowserPageUiData = OnboardingPageUiData(
     type = OnboardingPageUiData.Type.DEFAULT_BROWSER,
-    imageRes = R.drawable.ic_onboarding_welcome,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = "default browser title",
     description = "default browser body",
     primaryButtonLabel = "default browser primary button text",
@@ -437,7 +437,7 @@ private val defaultBrowserPageUiData = OnboardingPageUiData(
 )
 private val addSearchWidgetPageUiData = OnboardingPageUiData(
     type = OnboardingPageUiData.Type.ADD_SEARCH_WIDGET,
-    imageRes = R.drawable.ic_onboarding_search_widget,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = "add search widget title",
     description = "add search widget body",
     primaryButtonLabel = "add search widget primary button text",
@@ -445,7 +445,7 @@ private val addSearchWidgetPageUiData = OnboardingPageUiData(
 )
 private val syncPageUiData = OnboardingPageUiData(
     type = OnboardingPageUiData.Type.SYNC_SIGN_IN,
-    imageRes = R.drawable.ic_onboarding_sync,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = "sync title",
     description = "sync body",
     primaryButtonLabel = "sync primary button text",
@@ -468,7 +468,7 @@ private val toolbarPageUiData = OnboardingPageUiData(
 )
 private val notificationPageUiData = OnboardingPageUiData(
     type = OnboardingPageUiData.Type.NOTIFICATION_PERMISSION,
-    imageRes = R.drawable.ic_notification_permission,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = "notification title",
     description = "notification body",
     primaryButtonLabel = "notification primary button text",
@@ -477,7 +477,7 @@ private val notificationPageUiData = OnboardingPageUiData(
 
 private val defaultBrowserCardData = OnboardingCardData(
     cardType = OnboardingCardType.DEFAULT_BROWSER,
-    imageRes = R.drawable.ic_onboarding_welcome,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = StringHolder(null, "default browser title"),
     body = StringHolder(null, "default browser body"),
     primaryButtonLabel = StringHolder(null, "default browser primary button text"),
@@ -489,7 +489,7 @@ private val defaultBrowserCardData = OnboardingCardData(
 
 private val defaultBrowserCardDataNoDisqualifiers = OnboardingCardData(
     cardType = OnboardingCardType.DEFAULT_BROWSER,
-    imageRes = R.drawable.ic_onboarding_welcome,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = StringHolder(null, "default browser title"),
     body = StringHolder(null, "default browser body"),
     primaryButtonLabel = StringHolder(null, "default browser primary button text"),
@@ -501,7 +501,7 @@ private val defaultBrowserCardDataNoDisqualifiers = OnboardingCardData(
 
 private val addSearchWidgetCardDataNoConditions = OnboardingCardData(
     cardType = OnboardingCardType.ADD_SEARCH_WIDGET,
-    imageRes = R.drawable.ic_onboarding_search_widget,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = StringHolder(null, "add search widget title"),
     body = StringHolder(null, "add search widget body"),
     primaryButtonLabel = StringHolder(null, "add search widget primary button text"),
@@ -513,7 +513,7 @@ private val addSearchWidgetCardDataNoConditions = OnboardingCardData(
 
 private val addSearchWidgetCardData = OnboardingCardData(
     cardType = OnboardingCardType.ADD_SEARCH_WIDGET,
-    imageRes = R.drawable.ic_onboarding_search_widget,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = StringHolder(null, "add search widget title"),
     body = StringHolder(null, "add search widget body"),
     primaryButtonLabel = StringHolder(null, "add search widget primary button text"),
@@ -523,7 +523,7 @@ private val addSearchWidgetCardData = OnboardingCardData(
 
 private val syncCardData = OnboardingCardData(
     cardType = OnboardingCardType.SYNC_SIGN_IN,
-    imageRes = R.drawable.ic_onboarding_sync,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = StringHolder(null, "sync title"),
     body = StringHolder(null, "sync body"),
     primaryButtonLabel = StringHolder(null, "sync primary button text"),
@@ -535,7 +535,7 @@ private val syncCardData = OnboardingCardData(
 
 private val notificationCardData = OnboardingCardData(
     cardType = OnboardingCardType.NOTIFICATION_PERMISSION,
-    imageRes = R.drawable.ic_notification_permission,
+    imageRes = R.drawable.mozac_lib_crash_notification,
     title = StringHolder(null, "notification title"),
     body = StringHolder(null, "notification body"),
     primaryButtonLabel = StringHolder(null, "notification primary button text"),
--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -2337,7 +2337,7 @@ class Settings(
      */
     var shouldShowMarketingOnboarding by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_should_show_marketing_onboarding),
-        default = true,
+        default = false,
     )
 
     var shouldUseMinimalBottomToolbarWhenEnteringText by booleanPreference(
