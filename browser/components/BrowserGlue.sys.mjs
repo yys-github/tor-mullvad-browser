797d6dae116988dd874d08837d4c32794d2fc290
Pier Angelo Vendrame <pierov@torproject.org>
2023-08-18 16:31:38 +0200

    BB 42027: Base Browser migration procedures.
    This commit implmenents the the Base Browser's version of _migrateUI.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -392,6 +392,7 @@ BrowserGlue.prototype = {
 
     // handle any UI migration
     this._migrateUI();
+    lazy.ProfileDataUpgrader.upgradeBB(this._isNewProfile);
 
     if (!Services.prefs.prefHasUserValue(PREF_PDFJS_ISDEFAULT_CACHE_STATE)) {
       lazy.PdfJs.checkIsDefault(this._isNewProfile);
