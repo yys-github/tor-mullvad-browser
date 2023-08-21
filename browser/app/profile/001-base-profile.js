40a5eac1065d5983ddf20dba4f0462d0d79615fc
hackademix <giorgio@maone.net>
2023-08-21 13:13:57 +0200

    BB 42019: Empty browser's clipboard on browser shutdown

--- browser/app/profile/001-base-profile.js
+++ browser/app/profile/001-base-profile.js
@@ -138,6 +138,9 @@ pref("browser.backup.scheduled.enabled", false);
 pref("browser.backup.archive.enabled", false);
 pref("browser.backup.restore.enabled", false);
 
+// Empty clipboard content from private windows on exit (tor-browser#42154)
+pref("browser.privatebrowsing.preserveClipboard", false);
+
 // tor-browser#42611: Do not include the URL of the image, when copying it.
 // Also, do not save clipboard in history/cloud.
 pref("clipboard.imageAsFile.enabled", false);
