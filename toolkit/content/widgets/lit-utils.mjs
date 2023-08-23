6cacf521ab7f630030b567e08be6d0ff08d835d4
Henry Wilkes <henry@torproject.org>
2023-08-23 17:33:46 +0100

    Customize moz-toggle for tor-browser.

--- toolkit/content/widgets/lit-utils.mjs
+++ toolkit/content/widgets/lit-utils.mjs
@@ -261,6 +261,9 @@ export class MozBaseInputElement extends MozLitElement {
     parentDisabled: { type: Boolean, state: true },
     ariaLabel: { type: String, mapped: true },
     ariaDescription: { type: String, mapped: true },
+    // label-align-before is a customisation for the moz-toggle in about:tor.
+    // See tor-browser#43727.
+    labelAlignBefore: { type: Boolean, attribute: "label-align-before" },
   };
   static inputLayout = "inline";
 
@@ -419,9 +422,10 @@ export class MozBaseInputElement extends MozLitElement {
           part="label"
           for="input"
           shownaccesskey=${ifDefined(this.accessKey)}
-          >${this.isInlineLayout
+          >${this.labelAlignBefore ? this.labelTemplate() : ""}${this
+            .isInlineLayout
             ? this.inputTemplate()
-            : ""}${this.labelTemplate()}</label
+            : ""}${this.labelAlignBefore ? "" : this.labelTemplate()}</label
         >${this.hasDescription ? "" : this.supportLinkTemplate()}
       </span>
       ${this.descriptionTemplate()}
