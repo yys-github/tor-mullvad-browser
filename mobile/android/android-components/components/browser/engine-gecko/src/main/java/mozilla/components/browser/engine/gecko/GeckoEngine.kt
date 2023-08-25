8283c27d14d730add7a4ceced552281e2b93909c
Dan Ballard <dan@mindstab.net>
2023-08-25 16:08:13 -0700

    TB 41878: [android] Add standalone Tor Bootstrap

--- mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngine.kt
+++ mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngine.kt
@@ -1671,6 +1671,8 @@ class GeckoEngine(
         installedExtension.registerTabHandler(webExtensionTabHandler, defaultSettings)
         onSuccess(installedExtension)
     }
+
+    fun getTorIntegrationController() = runtime.getTorIntegrationController()
 }
 
 internal fun ContentBlockingController.LogEntry.BlockingData.hasBlockedCookies(): Boolean {
