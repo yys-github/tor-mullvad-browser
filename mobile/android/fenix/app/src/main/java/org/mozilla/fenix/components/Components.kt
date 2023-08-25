d523a7a7127afc3994353d9425ea12889826b9ed
Dan Ballard <dan@mindstab.net>
2023-08-25 16:08:13 -0700

    TB 41878: [android] Add standalone Tor Bootstrap

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Components.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Components.kt
@@ -114,6 +114,7 @@ import org.mozilla.fenix.settings.pagesummaries.PageSummariesSettingsSearchProvi
 import org.mozilla.fenix.settings.settingssearch.DefaultFenixSettingsIndexer
 import org.mozilla.fenix.termsofuse.TermsOfUseManager
 import org.mozilla.fenix.termsofuse.store.DefaultTermsOfUsePromptRepository
+import org.mozilla.fenix.tor.TorControllerGV
 import org.mozilla.fenix.utils.Settings
 import org.mozilla.fenix.utils.getApplicationInstalledTime
 import org.mozilla.fenix.utils.isLargeScreenSize
@@ -580,6 +581,8 @@ class Components(private val context: Context) {
             context = context,
         )
     }
+
+    val torController by lazyMonitored { TorControllerGV(context) }
 }
 
 class NoopReviewManager(val value: Context) : ReviewManager {
