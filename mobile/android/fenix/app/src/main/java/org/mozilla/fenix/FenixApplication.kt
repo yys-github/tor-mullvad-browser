c82509c106453d556cb7f8db584783d4c4cbd929
Dan Ballard <dan@mindstab.net>
2023-08-25 16:08:13 -0700

    TB 41878: [android] Add standalone Tor Bootstrap

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
@@ -137,6 +137,8 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
         isDeviceRamAboveThreshold()
     }
 
+    var terminating = false
+
     open val components by lazy { Components(this) }
 
     var visibilityLifecycleCallback: VisibilityLifecycleCallback? = null
@@ -184,6 +186,21 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
         GlobalScope.launch(Dispatchers.IO) {
             PerfStartup.applicationOnCreate.accumulateSamples(listOf(durationMillis))
         }
+    }
+
+    fun isTerminating() = terminating
+
+    fun terminate() {
+        onTerminate()
+        System.exit(0)
+    }
+
+    override fun onTerminate() {
+        terminating = true
+
+        super.onTerminate()
+        components.torController.stop()
+        components.torController.stopTor()
     }
 
     @OptIn(DelicateCoroutinesApi::class) // GlobalScope usage
@@ -314,6 +331,8 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
         components.analytics.metricsStorage.tryRegisterAsUsageRecorder(this)
 
         downloadWallpapers()
+
+        components.torController.start()
     }
 
     @OptIn(DelicateCoroutinesApi::class) // GlobalScope usage
@@ -917,7 +936,7 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
             clipboardSuggestionsEnabled.set(settings.shouldShowClipboardSuggestions)
             searchShortcutsEnabled.set(settings.shouldShowSearchShortcuts)
             voiceSearchEnabled.set(settings.shouldShowVoiceSearch)
-            openLinksInAppEnabled.set(settings.openLinksInExternalApp)
+            //openLinksInAppEnabled.set(settings.openLinksInExternalApp)
             signedInSync.set(settings.signedInFxaAccount)
 
             val syncedItems = SyncEnginesStorage(applicationContext).getStatus().entries.filter {
