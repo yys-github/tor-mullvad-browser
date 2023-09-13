cd3a7011827764dbd2ccee5125e7820228009a22
Henry Wilkes <henry@torproject.org>
2023-09-13 14:09:59 +0100

    TB 42110: Add TorUIUtils module for common tor component methods.

--- browser/base/content/browser.js
+++ browser/base/content/browser.js
@@ -91,6 +91,7 @@ ChromeUtils.defineESModuleGetters(this, {
   TelemetryEnvironment: "resource://gre/modules/TelemetryEnvironment.sys.mjs",
   ToolbarContextMenu: "resource:///modules/ToolbarContextMenu.sys.mjs",
   ToolbarDropHandler: "resource:///modules/ToolbarDropHandler.sys.mjs",
+  TorUIUtils: "resource:///modules/TorUIUtils.sys.mjs",
   TranslationsParent: "resource://gre/actors/TranslationsParent.sys.mjs",
   UITour: "moz-src:///browser/components/uitour/UITour.sys.mjs",
   UpdateUtils: "resource://gre/modules/UpdateUtils.sys.mjs",
