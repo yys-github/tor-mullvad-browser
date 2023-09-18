87f9dcb95f87447a33437ecd7371a91f70da798b
Richard Pospesel <richard@torproject.org>
2023-09-18 20:24:32 +0000

    Bug 41822: Unconditionally disable default browser UX in about:preferences

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -1755,7 +1755,8 @@ var gMainPane = {
       let defaultBrowserBox = document.getElementById("defaultBrowserBox");
       let isInFlatpak = gGIOService?.isRunningUnderFlatpak;
       // Flatpak does not support setting nor detection of default browser
-      if (!shellSvc || isInFlatpak) {
+      // tor-browser#41822 disable making Tor Browser the default browser
+      if (shellSvc || isInFlatpak || true) {
         defaultBrowserBox.hidden = true;
         return;
       }
