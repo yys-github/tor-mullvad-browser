cae9161af1c9d867cfe2330de74f08c307243b0f
Cecylia Bocovich <cohosh@torproject.org>
2023-11-28 11:47:27 -0500

    Temporary commit: manually place generated wasm files
    These files are built reproducibly using tor-browser-build: https://gitlab.torproject.org/tpo/applications/tor-browser-build/-/merge_requests/715
    
    We're manually adding them here while working on the interface, but
    eventually these should be placed in the right location using
    tor-browser-build.

--- eslint-ignores.config.mjs
+++ eslint-ignores.config.mjs
@@ -312,4 +312,5 @@ export default [
   "mobile/android/app/000-tor-browser-android.js",
   "toolkit/content/pt_config.json",
   "toolkit/content/moat_contries_dev_build.json",
+  "toolkit/components/lox/lox_wasm.jsm",
 ];
