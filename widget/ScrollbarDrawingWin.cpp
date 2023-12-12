cd26ad91510f7f55969b67178fa900e6ac6ef3b0
Henry Wilkes <henry@torproject.org>
2023-12-12 11:50:52 +0000

    BB 42528: Don't leak system scrollbar size on windows.

--- widget/ScrollbarDrawingWin.cpp
+++ widget/ScrollbarDrawingWin.cpp
@@ -10,6 +10,7 @@
 #include "nsLayoutUtils.h"
 #include "Theme.h"
 #include "nsNativeTheme.h"
+#include "nsContentUtils.h"
 
 namespace mozilla::widget {
 
@@ -156,7 +157,10 @@ void ScrollbarDrawingWin::RecomputeScrollbarParams() {
   }
   ConfigureScrollbarSize(defaultSize);
 
-  if (StaticPrefs::widget_non_native_theme_win_scrollbar_use_system_size()) {
+  // Do not leak system size when using ResistFingerprinting.
+  if (!nsContentUtils::ShouldResistFingerprinting("No context available",
+                                                  RFPTarget::CSSResolution) &&
+      StaticPrefs::widget_non_native_theme_win_scrollbar_use_system_size()) {
     ConfigureScrollbarSize(LookAndFeel::GetInt(
         LookAndFeel::IntID::SystemScrollbarSize, defaultSize));
   }
