45d036ba5d576335e6ac803544de30711313fdea
clairehurst <clairehurst@torproject.org>
2023-12-11 17:42:52 -0700

    [android] Implement Android-native Connection Assist UI

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/browser/BaseBrowserFragment.kt
@@ -2077,6 +2077,9 @@ abstract class BaseBrowserFragment :
                 val context = requireContext()
                 resumeDownloadDialogState(selectedTab.id, context.components.core.store, context)
                 it.announceForAccessibility(selectedTab.toDisplayTitle())
+                if (getCurrentTab()?.content?.url == "about:torconnect") {
+                    browserToolbarView.view.visibility = View.GONE
+                }
             }
         } else {
             view?.let { view -> initializeUI(view) }
@@ -2111,6 +2114,23 @@ abstract class BaseBrowserFragment :
             true
         BiometricAuthenticationManager.biometricAuthenticationNeededInfo.authenticationStatus =
             AuthenticationStatus.NOT_AUTHENTICATED
+
+        handleBetaHtmlTorConnect()
+    }
+
+    private fun handleBetaHtmlTorConnect() {
+        val currentTab = getCurrentTab() ?: return
+        if (currentTab.content.url == "about:torconnect") {
+            if (!requireActivity().settings().useHtmlConnectionUi) {
+                requireContext().components.useCases.tabsUseCases.removeTab(currentTab.id)
+                (requireActivity() as HomeActivity).navigateToHome(findNavController())
+            } else {
+                // This just makes it not flash (be visible for a split second) before handleTabSelected() hides it again
+                browserToolbarView.view.visibility = View.GONE
+            }
+        } else if (currentTab.content.url == "about:tor") {
+            requireContext().components.useCases.tabsUseCases.removeTab(currentTab.id)
+        }
     }
 
     private fun evaluateMessagesForMicrosurvey(components: Components) =
