ba46d6a84bb2423e384692a5d3611c20fb5250c4
clairehurst <clairehurst@torproject.org>
2023-12-11 17:42:52 -0700

    [android] Implement Android-native Connection Assist UI

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/toolbar/BrowserToolbarMenuController.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/toolbar/BrowserToolbarMenuController.kt
@@ -160,6 +160,7 @@ class DefaultBrowserToolbarMenuController(
             }
             is ToolbarMenu.Item.Quit -> {
                 deleteAndQuit(activity, activity.lifecycleScope)
+                activity.shutDown()
             }
             is ToolbarMenu.Item.CustomizeReaderView -> {
                 readerModeController.showControls()
