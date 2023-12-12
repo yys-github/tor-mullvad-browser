bf252b61f029dc8dac7a9c8703b5e5b0805e9b3c
clairehurst <clairehurst@torproject.org>
2023-12-11 17:42:52 -0700

    [android] Implement Android-native Connection Assist UI

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/snackbar/Snackbar.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/compose/snackbar/Snackbar.kt
@@ -330,6 +330,7 @@ internal fun Snackbar(
                         onClick = snackbarState.action.onClick,
                         modifier = Modifier.testTag(SNACKBAR_BUTTON_TEST_TAG),
                         textColor = colors.actionTextColor,
+                        upperCaseText = false,
                     )
 
                     Spacer(modifier = Modifier.width(snackbarActionHorizontalSpacing))
