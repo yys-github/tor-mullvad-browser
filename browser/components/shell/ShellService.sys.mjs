3e9d0890555ce8e081f5925f3038594bb7c8a204
Pier Angelo Vendrame <pierov@torproject.org>
2024-02-14 17:08:34 +0100

    MB 80: Enable Mullvad Browser as a default browser

--- browser/components/shell/ShellService.sys.mjs
+++ browser/components/shell/ShellService.sys.mjs
@@ -308,7 +308,7 @@ let ShellServiceInternal = {
       ) {
         if (this._shouldSetDefaultPDFHandler()) {
           lazy.log.info("Setting Firefox as default PDF handler");
-          extraFileExtensions.push(".pdf", "FirefoxPDF");
+          extraFileExtensions.push(".pdf", "MullvadBrowserPDF");
         } else {
           lazy.log.info("Not setting Firefox as default PDF handler");
         }
@@ -346,7 +346,7 @@ let ShellServiceInternal = {
       try {
         this.defaultAgent.setDefaultExtensionHandlersUserChoice(aumi, [
           ".pdf",
-          "FirefoxPDF",
+          "MullvadBrowserPDF",
         ]);
       } catch (err) {
         telemetryResult = "ErrOther";
