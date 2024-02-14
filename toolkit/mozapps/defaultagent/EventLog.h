6685cd2adf0621e3b2c71ebae083fcae1a3fc9e4
Pier Angelo Vendrame <pierov@torproject.org>
2024-02-14 17:08:34 +0100

    MB 80: Enable Mullvad Browser as a default browser

--- toolkit/mozapps/defaultagent/EventLog.h
+++ toolkit/mozapps/defaultagent/EventLog.h
@@ -5,7 +5,7 @@
 #ifndef DEFAULT_BROWSER_AGENT_EVENT_LOG_H_
 #define DEFAULT_BROWSER_AGENT_EVENT_LOG_H_
 
-#include "mozilla/Types.h"
+#include <cwchar>
 
 MOZ_BEGIN_EXTERN_C
 
@@ -13,10 +13,21 @@ extern MOZ_EXPORT const wchar_t* gWinEventLogSourceName;
 
 MOZ_END_EXTERN_C
 
-#include "mozilla/WindowsEventLog.h"
-
-#define LOG_ERROR(hr) MOZ_WIN_EVENT_LOG_ERROR(gWinEventLogSourceName, hr)
-#define LOG_ERROR_MESSAGE(format, ...) \
-  MOZ_WIN_EVENT_LOG_ERROR_MESSAGE(gWinEventLogSourceName, format, __VA_ARGS__)
+#ifdef LOG_ERRORS_FILE
+extern FILE* gLogFile;
+#  define LOG_ERROR(hr)                                                 \
+    if (gLogFile) {                                                     \
+      fprintf(gLogFile, "Error in %s:%d: 0x%X\r\n", __FILE__, __LINE__, \
+              (unsigned int)hr);                                        \
+    }
+#  define LOG_ERROR_MESSAGE(format, ...)                     \
+    if (gLogFile) {                                          \
+      fwprintf(gLogFile, format __VA_OPT__(, ) __VA_ARGS__); \
+      fputs("\r\n", gLogFile);                               \
+    }
+#else
+#  define LOG_ERROR(hr)
+#  define LOG_ERROR_MESSAGE(format, ...)
+#endif
 
 #endif  // DEFAULT_BROWSER_AGENT_EVENT_LOG_H_
