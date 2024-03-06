4a5bf628022463de958e540e335e515af3702f66
hackademix <giorgio@maone.net>
2024-03-06 14:15:53 +0100

    BB 41918: Option to reuse last window size when letterboxing is enabled.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -1905,6 +1905,11 @@ BrowserGlue.prototype = {
         return false;
       }
 
+      // We don't want to mess up with RFP new window / letterboxing machinery.
+      if (Services.prefs.getBoolPref("privacy.resistFingerprinting", false)) {
+        return false;
+      }
+
       let width = getValue("width");
       let height = getValue("height");
 
