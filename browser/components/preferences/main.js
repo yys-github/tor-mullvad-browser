1d1957b4910d49c97eea6a4497cf30532fe15bb2
Pier Angelo Vendrame <pierov@torproject.org>
2024-05-07 17:50:20 +0200

    BB 41930: Remove the UI to customize accept_languages.

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -675,6 +675,7 @@ var gMainPane = {
     initSettingGroup("fonts");
     initSettingGroup("browserLanguage");
     initSettingGroup("websiteLanguage");
+    initSettingGroup("websiteSpoofEnglish");
     initSettingGroup("browsing");
     initSettingGroup("keyboardAndScrolling");
     initSettingGroup("motionAndLink");
@@ -693,7 +694,7 @@ var gMainPane = {
       gMainPane.showBrowserLanguagesSubDialog({ search: false });
     });
 
-    setEventListener("chooseLanguage", "command", gMainPane.showLanguages);
+    // setEventListener("chooseLanguage", "command", gMainPane.showLanguages);
 
     // Initilize Application section.
 
