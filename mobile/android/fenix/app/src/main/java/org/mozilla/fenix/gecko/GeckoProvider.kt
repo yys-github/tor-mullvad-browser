4ef9ca9a637caf925b0c8dc517e0645dbf8c84ab
Pier Angelo Vendrame <pierov@torproject.org>
2024-05-08 11:35:33 +0200

    BB 42562: Normalized the Accepted Languages on Android.
    The OS language might be outside the list of actually supported
    languages and it might leak the user's region.
    Therefore, we force the locale reported in Accept-Language to match one
    we support with translations, even when it means using a not exact
    region tag.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/gecko/GeckoProvider.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/gecko/GeckoProvider.kt
@@ -17,6 +17,7 @@ import mozilla.components.lib.crash.handler.CrashHandlerService
 import mozilla.components.lib.crash.store.CrashAction
 import mozilla.components.service.sync.autofill.GeckoCreditCardsAddressesStorageDelegate
 import mozilla.components.service.sync.logins.GeckoLoginStorageDelegate
+import org.mozilla.fenix.BuildConfig
 import org.mozilla.fenix.Config
 import org.mozilla.fenix.components.appstate.AppAction
 import org.mozilla.fenix.ext.components
@@ -133,6 +134,7 @@ object GeckoProvider {
             )
             .isolatedProcessEnabled(context.components.settings.isIsolatedProcessEnabled)
             .appZygoteProcessEnabled(context.components.settings.isAppZygoteEnabled)
+            .supportedLocales(BuildConfig.SUPPORTED_LOCALE_ARRAY.toList())
 
         if (FxNimbus.features.fission.value().shouldUseNimbus) {
             builder
