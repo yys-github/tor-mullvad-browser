5a22641bc00853d694080b9ad07dc3eb5fa3befa
Pier Angelo Vendrame <pierov@torproject.org>
2024-07-18 15:48:46 +0200

    TB 40562: Added Tor Browser preferences to 000-tor-browser.js
    Before reordering patches, we used to keep the Tor-related patches
    (torbutton and tor-launcher) at the beginning.
    After that issue, we decided to move them towards the end.
    
    In addition to that, we have decided to move Tor Browser-only
    preferences there, too, to make Base Browser-only fixups easier to
    apply.

--- .eslintrc-ignores.js
+++ .eslintrc-ignores.js
@@ -310,4 +310,6 @@ module.exports = [
   "netwerk/test/mochitests/test_import_json_module.mjs",
 
   "browser/app/profile/001-base-profile.js",
+  "browser/app/profile/000-tor-browser.js",
+  "mobile/android/app/000-tor-browser-android.js",
 ];
