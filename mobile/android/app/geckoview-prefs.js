b5f3ac9c635aa5ef2f3f81ca483478022ac12882
Pier Angelo Vendrame <pierov@torproject.org>
2024-07-18 15:48:46 +0200

    TB 40562: Added Tor Browser preferences to 000-tor-browser.js
    Before reordering patches, we used to keep the Tor-related patches
    (torbutton and tor-launcher) at the beginning.
    After that issue, we decided to move them towards the end.
    
    In addition to that, we have decided to move Tor Browser-only
    preferences there, too, to make Base Browser-only fixups easier to
    apply.

--- mobile/android/app/geckoview-prefs.js
+++ mobile/android/app/geckoview-prefs.js
@@ -419,3 +419,5 @@ pref("xpinstall.signatures.required", true);
 
 pref("xpinstall.whitelist.add", "https://addons.mozilla.org");
 pref("xpinstall.whitelist.fileRequest", false);
+
+#include 000-tor-browser-android.js
