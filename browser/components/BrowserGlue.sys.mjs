91750307cf8d625bc2d61c0dea4577b5e542e943
Morgan <morgan@torproject.org>
2024-08-22 02:22:56 +0000

    fixup! Bug 40166: Disable security.certerrors.mitm.auto_enable_enterprise_roots
    - Revert 612d54e8e97017891865db8003826d8688ad5a9b

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -1538,20 +1538,6 @@ BrowserGlue.prototype = {
     // Base Browser-specific version of _migrateUI.
     this._migrateUIBB();
 
-    // Clear possibly auto enabled enterprise_roots prefs (see bug 40166)
-    if (
-      !Services.prefs.getBoolPref(
-        "security.certerrors.mitm.auto_enable_enterprise_roots"
-      ) &&
-      Services.prefs.getBoolPref(
-        "security.enterprise_roots.auto-enabled",
-        false
-      )
-    ) {
-      Services.prefs.clearUserPref("security.enterprise_roots.enabled");
-      Services.prefs.clearUserPref("security.enterprise_roots.auto-enabled");
-    }
-
     if (!Services.prefs.prefHasUserValue(PREF_PDFJS_ISDEFAULT_CACHE_STATE)) {
       lazy.PdfJs.checkIsDefault(this._isNewProfile);
     }
