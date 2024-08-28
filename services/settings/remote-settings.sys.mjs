16b46a53ec187b4bbcd3dea8c42709198d6b04d5
cypherpunks1 <2478-cypherpunks1@gitlab.torproject.org>
2024-08-28 05:36:48 -0800

    BB 42730: Patch RemoteSettings to use only local dumps as a data source

--- services/settings/remote-settings.sys.mjs
+++ services/settings/remote-settings.sys.mjs
@@ -149,6 +149,7 @@ export async function jexlFilterCreator(environment, collectionName) {
 function remoteSettingsFunction() {
   const _clients = new Map();
   let _invalidatePolling = false;
+  let _initialized = false;
 
   // If not explicitly specified, use the default signer.
   const defaultOptions = {
@@ -215,6 +216,43 @@ function remoteSettingsFunction() {
     // completely unknown (ie. no database and no JSON dump).
     lazy.console.debug(`No known client for ${bucketName}/${collectionName}`);
     return null;
+  }
+
+  /**
+   * Internal helper that checks all registered remote settings clients for the presence
+   * of a newer local data dump. If a local dump is found and its last modified timestamp
+   * is more recent than the client's current data, imports the dump by triggering a sync.
+   * Notifies observers if any data was imported from a dump.
+   *
+   * @param {string} [trigger="manual"] - The reason or source for triggering the import (e.g., "manual", "startup").
+   * @returns {Promise<void>} Resolves when the import process is complete.
+   * @private
+   */
+  async function _maybeImportFromLocalDump(trigger = "manual") {
+    let importedFromDump = false;
+    for (const client of _clients.values()) {
+      const hasLocalDump = await lazy.Utils.hasLocalDump(
+        client.bucketName,
+        client.collectionName
+      );
+      if (hasLocalDump) {
+        const lastModified = await client.getLastModified();
+        const lastModifiedDump = await lazy.Utils.getLocalDumpLastModified(
+          client.bucketName,
+          client.collectionName
+        );
+        if (lastModified < lastModifiedDump) {
+          await client.maybeSync(lastModifiedDump, {
+            loadDump: true,
+            trigger,
+          });
+          importedFromDump = true;
+        }
+      }
+    }
+    if (importedFromDump) {
+      Services.obs.notifyObservers(null, "remote-settings:changes-poll-end");
+    }
   }
 
   /**
@@ -352,6 +390,16 @@ function remoteSettingsFunction() {
     full = false,
     // eslint-disable-next-line complexity
   } = {}) => {
+    if (AppConstants.BASE_BROWSER_VERSION) {
+      // Called multiple times on GeckoView due to bug 1730026
+      if (_initialized) {
+        return;
+      }
+      _initialized = true;
+      _maybeImportFromLocalDump(trigger);
+      return;
+    }
+
     if (lazy.Utils.shouldSkipRemoteActivity) {
       return;
     }
