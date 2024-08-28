1216ed24c95e490f5ce99976ac2b76a82123d5ce
cypherpunks1 <2478-cypherpunks1@gitlab.torproject.org>
2024-08-28 05:36:48 -0800

    BB 42730: Patch RemoteSettings to use only local dumps as a data source

--- mobile/shared/chrome/geckoview/geckoview.js
+++ mobile/shared/chrome/geckoview/geckoview.js
@@ -20,6 +20,7 @@ ChromeUtils.defineESModuleGetters(this, {
   InitializationTracker: "resource://gre/modules/GeckoViewTelemetry.sys.mjs",
   RemoteSecuritySettings:
     "resource://gre/modules/psm/RemoteSecuritySettings.sys.mjs",
+  RemoteSettings: "resource://services-settings/remote-settings.sys.mjs",
   SafeBrowsing: "resource://gre/modules/SafeBrowsing.sys.mjs",
   CaptchaDetectionPingUtils:
     "moz-src:///toolkit/components/captchadetection/CaptchaDetectionPingUtils.sys.mjs",
@@ -833,6 +834,10 @@ function startup() {
       // submits the ping if it has data and has been about 24 hours since the
       // last submission.
       CaptchaDetectionPingUtils.init();
+    });
+
+    InitLater(() => {
+      RemoteSettings.pollChanges({ trigger: "timer" });
     });
 
     // This should always go last, since the idle tasks (except for the ones with
