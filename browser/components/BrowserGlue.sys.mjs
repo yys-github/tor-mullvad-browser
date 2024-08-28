c3f5d8d0ac7b5955bfd6cb7a0308a2a370356083
cypherpunks1 <2478-cypherpunks1@gitlab.torproject.org>
2024-08-28 05:36:48 -0800

    BB 42730: Patch RemoteSettings to use only local dumps as a data source

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -1353,6 +1353,11 @@ BrowserGlue.prototype = {
         lazy.RemoteSecuritySettings.init();
       },
 
+      function RemoteSettingsPollChanges() {
+        // Support clients that use the "sync" event or "remote-settings:changes-poll-end".
+        lazy.RemoteSettings.pollChanges({ trigger: "timer" });
+      },
+
       function searchBackgroundChecks() {
         Services.search.runBackgroundChecks();
       },
