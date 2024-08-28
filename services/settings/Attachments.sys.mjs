071c925f30f275f7aeecfb1348f59e47d504a890
cypherpunks1 <2478-cypherpunks1@gitlab.torproject.org>
2024-08-28 05:36:48 -0800

    BB 42730: Patch RemoteSettings to use only local dumps as a data source

--- services/settings/Attachments.sys.mjs
+++ services/settings/Attachments.sys.mjs
@@ -355,6 +355,10 @@ export class Downloader {
       fallbackToDump = false;
     }
 
+    avoidDownload = true;
+    fallbackToCache = true;
+    fallbackToDump = true;
+
     const dumpInfo = new LazyRecordAndBuffer(() =>
       this._readAttachmentDump(attachmentId)
     );
@@ -576,6 +580,8 @@ export class Downloader {
       attachment: { location, hash, size },
     } = record;
 
+    return (await this.#fetchAttachment(record)).buffer;
+    // eslint-disable-next-line no-unreachable
     let baseURL;
     try {
       baseURL = await lazy.Utils.baseAttachmentsURL();
