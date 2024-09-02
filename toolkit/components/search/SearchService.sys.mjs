5613de84569bf2f8935ba77d33bfa4b9f9c4cfc3
Pier Angelo Vendrame <pierov@torproject.org>
2024-09-02 17:56:06 +0200

    TB 42891: Set the bundled search engine for Tor Browser.
    After upstream changes between Firefox 115 and 128, we had to
    completely rework the way in which we define our search engines.
    
    This commit replaces the old "Omnibox: Add DDG, Startpage, Disconnect,
    Youtube, Twitter; remove Amazon, eBay, bing".
    
    With that commit, we customized a list of addons IDs to ship as
    built-in search engines, but then upsteam moved to using only
    RemoteSettings.
    The configuration has many more fields, and it would be quite long to
    include it in the source code. Therefore, we use some local JSON files
    and load the settings from them.

--- toolkit/components/search/SearchService.sys.mjs
+++ toolkit/components/search/SearchService.sys.mjs
@@ -79,6 +79,8 @@ export const NON_SPLIT_ENGINE_IDS = [
   "engine-purpose",
   "engine-fr",
   "fixup_search",
+  "ddg-onion",
+  "startpage-onion",
 ];
 
 const TOPIC_LOCALES_CHANGE = "intl:app-locales-changed";
@@ -2519,23 +2521,9 @@ export class SearchService {
   // This is prefixed with _ rather than # because it is
   // called in test_remove_engine_notification_box.js
   async _fetchEngineSelectorEngines() {
-    let searchEngineSelectorProperties = {
-      locale: Services.locale.appLocaleAsBCP47,
-      region: lazy.Region.home || "unknown",
-      channel: lazy.SearchUtils.MODIFIED_APP_CHANNEL,
-      experiment:
-        lazy.NimbusFeatures.searchConfiguration.getVariable("experiment") ?? "",
-      distroID: lazy.SearchUtils.distroID ?? "",
-    };
-
-    for (let [key, value] of Object.entries(searchEngineSelectorProperties)) {
-      this._settings.setMetaDataAttribute(key, value);
-    }
-
-    let { engines, privateDefault } =
-      await this.#engineSelector.fetchEngineConfiguration(
-        searchEngineSelectorProperties
-      );
+    const engines = await (
+      await fetch("chrome://global/content/search/torBrowserSearchEngines.json")
+    ).json();
 
     for (let e of engines) {
       if (!e.webExtension) {
@@ -2566,7 +2554,7 @@ export class SearchService {
       e.webExtension.locale = locale;
     }
 
-    return { engines, privateDefault };
+    return { engines, privateDefault: undefined };
   }
 
   #setDefaultAndOrdersFromSelector(engines, privateDefault) {
