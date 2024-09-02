24a544f6f34c841cd9cf8fe8817b949f3cb64a36
Pier Angelo Vendrame <pierov@torproject.org>
2024-09-02 17:56:06 +0200

    TB 42891: Set the bundled search engine for Tor Browser.

--- toolkit/components/search/tests/xpcshell/test_security_level.js
+++ toolkit/components/search/tests/xpcshell/test_security_level.js
@@ -10,6 +10,8 @@
 
 const expectedURLs = {
   ddg: "https://html.duckduckgo.com/html?q=test",
+  "ddg-onion":
+    "https://duckduckgogg42xjoc72x3sjasowoarfbgcmvfimaftt6twagswzczad.onion/html?q=test",
 };
 
 add_task(async function test_securityLevel() {
