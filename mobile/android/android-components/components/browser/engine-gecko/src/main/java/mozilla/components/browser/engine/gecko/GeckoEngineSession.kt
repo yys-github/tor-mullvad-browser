663335ae22ed42757ef60d2a6410bd534fd8ffa0
clairehurst <clairehurst@torproject.org>
2024-09-03 17:33:03 -0600

    TB 42655 [android]: Implement "New circuit for this site" on Android

--- mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngineSession.kt
+++ mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/GeckoEngineSession.kt
@@ -1810,6 +1810,10 @@ class GeckoEngineSession(
         geckoSession.translationsSessionDelegate = GeckoTranslateSessionDelegate(this)
     }
 
+    fun newTorCircuit() {
+        geckoSession.newTorCircuit()
+    }
+
     companion object {
         internal const val PROGRESS_START = 25
         internal const val PROGRESS_STOP = 100
