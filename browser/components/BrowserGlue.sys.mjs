c8fec3692cc294d140abd99fb5fb67a2c7613136
Henry Wilkes <henry@torproject.org>
2024-09-03 18:49:17 +0100

    fixup! TB 7494: Create local home page for TBB.
    TB 44032: Implement the 2025 YEC banner.
    
    This adds the 2025 year end campaign banner. We want to keep this beyond
    the end of the campaign since next year's campaign is likely going to be
    similar.
    
    Assets specific to 2025 are in a separate commit.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -529,6 +529,7 @@ let JSWINDOWACTORS = {
         L10nMutationsFinished: {},
         SubmitSearchOnionize: { wantUntrusted: true },
         SurveyDismissed: { wantUntrusted: true },
+        UserDismissedYEC: { wantUntrusted: true },
       },
     },
 
