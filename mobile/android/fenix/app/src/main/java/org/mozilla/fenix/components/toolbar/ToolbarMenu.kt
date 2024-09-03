0e187049a2a19cc4ad6df05aa4ba40e2286676a4
clairehurst <clairehurst@torproject.org>
2024-09-03 17:33:03 -0600

    TB 42655 [android]: Implement "New circuit for this site" on Android

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/toolbar/ToolbarMenu.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/toolbar/ToolbarMenu.kt
@@ -68,6 +68,7 @@ interface ToolbarMenu {
         object Passwords : Item()
         object Downloads : Item()
         object NewTab : Item()
+        object NewTorCircuit : Item()
     }
 
     val menuBuilder: BrowserMenuBuilder
