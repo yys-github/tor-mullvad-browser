d370cae0e5139ecaf6e0adeedab8baa2fe37a227
Henry Wilkes <henry@torproject.org>
2024-09-16 16:38:10 +0100

    BB 42831: Remove the shopping components.

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -650,8 +650,6 @@ var gBrowserInit = {
 
     CaptivePortalWatcher.delayedStartup();
 
-    ShoppingSidebarManager.ensureInitialized();
-
     if (Services.prefs.getBoolPref("browser.profiles.enabled", false)) {
       SelectableProfileService?.init();
     }
