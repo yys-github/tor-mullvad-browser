c26a9026e25361967579b27d328f8fa902e210d8
Pier Angelo Vendrame <pierov@torproject.org>
2024-10-07 15:00:21 +0200

    BB 43196: Remove the vendor name from media notifications on Linux.
    Firefox shows "vendor remoteName" as a title of the "... is playing
    media" notification on Linux.
    However, for our browser the remote name is enough, and prepending the
    vendor to it creates a string users usually never see.

--- widget/gtk/MPRISServiceHandler.cpp
+++ widget/gtk/MPRISServiceHandler.cpp
@@ -481,8 +481,10 @@ void MPRISServiceHandler::InitIdentity() {
       do_GetService("@mozilla.org/xre/app-info;1", &rv);
   MOZ_ASSERT(NS_SUCCEEDED(rv));
 
+#ifndef BASE_BROWSER_VERSION
   rv = appInfo->GetVendor(mIdentity);
   MOZ_ASSERT(NS_SUCCEEDED(rv));
+#endif
 
   if (gAppData) {
     mDesktopEntry = gAppData->remotingName;
@@ -491,7 +493,9 @@ void MPRISServiceHandler::InitIdentity() {
     MOZ_ASSERT(NS_SUCCEEDED(rv));
   }
 
+#ifndef BASE_BROWSER_VERSION
   mIdentity.Append(' ');
+#endif
   mIdentity.Append(mDesktopEntry);
 
   LOGMPRIS("InitIdentity() MPRIS desktop ID %s", mDesktopEntry.get());
