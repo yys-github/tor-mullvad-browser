858a78282cb49af14b5366baa42d5f113dcd75a5
Dan Ballard <dan@mindstab.net>
2024-10-17 14:31:15 -0700

    TB 43006: Disable RFP for Font Visibility on Android

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -17308,6 +17308,12 @@ bool Document::RecomputeResistFingerprinting(bool aForceRefreshRTPCallerType) {
 }
 
 bool Document::ShouldResistFingerprinting(RFPTarget aTarget) const {
+#ifdef ANDROID
+  if (aTarget == RFPTarget::FontVisibilityBaseSystem ||
+      aTarget == RFPTarget::FontVisibilityLangPack) {
+    return false;
+  }
+#endif
   return mShouldResistFingerprinting &&
          nsRFPService::IsRFPEnabledFor(this->IsInPrivateBrowsing(), aTarget,
                                        mOverriddenFingerprintingSettings);
