35bb85a6ef589c900546f60bdf473b13a17520c2
Pier Angelo Vendrame <pierov@torproject.org>
2024-11-26 19:37:25 +0100

    BB 43322: Customize the font visibility lists.
    Customize the lists of fonts to assign base visibility to in
    base browser and derivatives.
    
    Also, rename the files with the upstream lists, to make sure we do not
    use them by mistake.

--- gfx/thebes/gfxDWriteFontList.cpp
+++ gfx/thebes/gfxDWriteFontList.cpp
@@ -37,6 +37,7 @@
 #define StandardFonts
 #include "StandardFonts-win10.inc"
 #undef StandardFonts
+#include "StandardFonts-win10-bb.inc"
 
 using namespace mozilla;
 using namespace mozilla::gfx;
@@ -938,7 +939,9 @@ size_t gfxDWriteFontEntry::ComputedSizeOfExcludingThis(
 
 gfxDWriteFontList::gfxDWriteFontList() : mForceGDIClassicMaxFontSize(0.0) {
   CheckFamilyList(kBaseFonts);
+#ifndef BASE_BROWSER_VERSION
   CheckFamilyList(kLangPackFonts);
+#endif
 }
 
 // bug 602792 - CJK systems default to large CJK fonts which cause excessive
@@ -1179,12 +1182,14 @@ FontVisibility gfxDWriteFontList::GetVisibilityForFamily(
   if (FamilyInList(aName, kBaseFonts)) {
     return FontVisibility::Base;
   }
+#ifndef BASE_BROWSER_VERSION
   if (FamilyInList(aName, kLangPackFonts)) {
     return FontVisibility::LangPack;
   }
   if (nsRFPService::FontIsAllowedByLocale(aName)) {
     return FontVisibility::LangPack;
   }
+#endif
   return FontVisibility::User;
 }
 
@@ -1193,8 +1198,10 @@ gfxDWriteFontList::GetFilteredPlatformFontLists() {
   nsTArray<std::pair<const char**, uint32_t>> fontLists;
 
   fontLists.AppendElement(std::make_pair(kBaseFonts, std::size(kBaseFonts)));
+#ifndef BASE_BROWSER_VERSION
   fontLists.AppendElement(
       std::make_pair(kLangPackFonts, std::size(kLangPackFonts)));
+#endif
 
   return fontLists;
 }
