0815f81e5cf8e1315fe74d00a62499a0ee149a72
Pier Angelo Vendrame <pierov@torproject.org>
2024-11-26 19:37:25 +0100

    BB 43322: Customize the font visibility lists.
    Customize the lists of fonts to assign base visibility to in
    base browser and derivatives.
    
    Also, rename the files with the upstream lists, to make sure we do not
    use them by mistake.

--- gfx/thebes/gfxFcPlatformFontList.cpp
+++ gfx/thebes/gfxFcPlatformFontList.cpp
@@ -27,9 +27,11 @@
 #include "nsString.h"
 #include "nsStringFwd.h"
 #include "nsXULAppAPI.h"
-#define StandardFonts
-#include "StandardFonts-linux.inc"
-#undef StandardFonts
+#ifndef BASE_BROWSER_VERSION
+#  define StandardFonts
+#  include "StandardFonts-linux.inc"
+#  undef StandardFonts
+#endif
 #include <cairo-ft.h>
 #include <dlfcn.h>
 #include <fontconfig/fcfreetype.h>
@@ -1489,12 +1491,14 @@ gfxFcPlatformFontList::gfxFcPlatformFontList()
       mFcSubstituteCache(64),
       mLastConfig(nullptr),
       mAlwaysUseFontconfigGenerics(true) {
+#ifndef BASE_BROWSER_VERSION
   CheckFamilyList(kBaseFonts_Ubuntu_22_04);
   CheckFamilyList(kLangFonts_Ubuntu_22_04);
   CheckFamilyList(kBaseFonts_Ubuntu_20_04);
   CheckFamilyList(kLangFonts_Ubuntu_20_04);
   CheckFamilyList(kBaseFonts_Fedora_39);
   CheckFamilyList(kBaseFonts_Fedora_38);
+#endif
   mLastConfig = FcConfigGetCurrent();
   if (XRE_IsParentProcess()) {
     // if the rescan interval is set, start the timer
@@ -1802,11 +1806,14 @@ using Device = nsIGfxInfo::FontVisibilityDeviceDetermination;
 static Device sFontVisibilityDevice = Device::Unassigned;
 
 void AssignFontVisibilityDevice() {
+  sFontVisibilityDevice = Device::Linux_Unknown;
+#ifndef BASE_BROWSER_VERSION
   if (sFontVisibilityDevice == Device::Unassigned) {
     nsCOMPtr<nsIGfxInfo> gfxInfo = components::GfxInfo::Service();
     NS_ENSURE_SUCCESS_VOID(
         gfxInfo->GetFontVisibilityDetermination(&sFontVisibilityDevice));
   }
+#endif
 }
 
 // Per family array of faces.
@@ -2122,6 +2129,7 @@ FontVisibility gfxFcPlatformFontList::GetVisibilityForFamily(
   AssignFontVisibilityDevice();
 
   switch (sFontVisibilityDevice) {
+#ifndef BASE_BROWSER_VERSION
     case Device::Linux_Ubuntu_any:
     case Device::Linux_Ubuntu_22:
       if (FamilyInList(aName, kBaseFonts_Ubuntu_22_04)) {
@@ -2161,7 +2169,7 @@ FontVisibility gfxFcPlatformFontList::GetVisibilityForFamily(
         return FontVisibility::Base;
       }
       return FontVisibility::User;
-
+#endif
     default:
       // We don't know how to categorize fonts on this system
       return FontVisibility::Unknown;
@@ -2174,6 +2182,7 @@ gfxFcPlatformFontList::GetFilteredPlatformFontLists() {
 
   nsTArray<std::pair<const char**, uint32_t>> fontLists;
 
+#ifndef BASE_BROWSER_VERSION
   switch (sFontVisibilityDevice) {
     case Device::Linux_Ubuntu_any:
     case Device::Linux_Ubuntu_22:
@@ -2207,6 +2216,7 @@ gfxFcPlatformFontList::GetFilteredPlatformFontLists() {
       // We don't know how to categorize fonts on this system
       break;
   }
+#endif
 
   return fontLists;
 }
