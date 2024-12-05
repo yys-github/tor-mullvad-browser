827ff648e1cdb40dc066afb2acdcf34019eb8583
clairehurst <clairehurst@torproject.org>
2024-12-05 15:46:06 -0700

    [android] Bug_43351: Add function to disable forcing ALL CAPS for the fenix snackbar action button text
    This is needed because the default snackbar will force all caps, which can cause problems in some languages. See tor-browser#43351 and bugzilla bug 1935387.
    
    TODO: Remove once the forced all caps is removed by mozilla.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -1136,6 +1136,7 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, TorIn
                 view = binding.root,
             )
                 .setText(getString(R.string.connection_assist_connect_to_tor_before_opening_links))
+                .setAllCapsForActionButton(false)
                 .setAction(getString(R.string.connection_assist_connect_to_tor_before_opening_links_confirmation)) {
                     torConnectionAssistViewModel.handleConnect(searchTermOrURL)
                     if (navHost.navController.previousBackStackEntry?.destination?.id == R.id.torConnectionAssistFragment) {
