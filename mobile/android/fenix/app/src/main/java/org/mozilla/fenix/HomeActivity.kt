0b867753cf2fdc5aa39346be9b4322f0841b9abe
clairehurst <clairehurst@torproject.org>
2024-12-10 14:26:23 -0700

    fixup! [android] Add standalone Tor Bootstrap
    Bug 43368: Add @Suppress for linting error "Overriding method should call super. onNewIntent"

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -4,6 +4,7 @@
 
 package org.mozilla.fenix
 
+import android.annotation.SuppressLint
 import android.app.assist.AssistContent
 import android.app.PendingIntent
 import android.content.Context
@@ -710,6 +711,7 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, TorIn
     /**
      * Handles intents received when the activity is open.
      */
+    @SuppressLint("MissingSuperCall") // super.onNewIntent is called in [onNewIntentInternal(intent)]
     final override fun onNewIntent(intent: Intent?) {
         if (intent?.action == ACTION_MAIN || components.torController.isConnected) {
             onNewIntentInternal(intent)
