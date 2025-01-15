27744479cae9d4becfe8f6589749d6258f1ef3cd
clairehurst <clairehurst@torproject.org>
2025-01-14 19:33:17 -0700

    fixup! TB 42247: Android helpers for the TorProvider
    Bug 43222: Fix tor logs timestamps

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/TorIntegrationAndroid.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/TorIntegrationAndroid.java
@@ -169,8 +169,9 @@ public class TorIntegrationAndroid implements BundleEventListener {
     } else if (EVENT_TOR_LOGS.equals(event)) {
       String msg = message.getString("message");
       String type = message.getString("logType");
+      String timestamp = message.getString("timestamp");
       for (TorLogListener listener : mLogListeners) {
-        listener.onLog(type, msg);
+        listener.onLog(type, msg, timestamp);
       }
     }
   }
@@ -636,7 +637,7 @@ public class TorIntegrationAndroid implements BundleEventListener {
   }
 
   public interface TorLogListener {
-    void onLog(String logType, String message);
+    void onLog(String logType, String message, String timestamp);
   }
 
   private @NonNull void reloadSettings() {
