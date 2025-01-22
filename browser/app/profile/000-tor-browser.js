41a14ffedadcf85dcebabb29662ce1db3735f680
Henry Wilkes <henry@torproject.org>
2025-01-22 17:00:19 +0000

    fixup! TB 40597: Implement TorSettings module
    TB 43254: Allow some DomainFrontedRequests to be cancelled.
    
    This is used by some of the Moat calls to stop abandoned requests
    running in the background:
    
    1. Cancelled AutoBootstraps.
    2. Cancelled CAPTCHA request/submissions.

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -143,6 +143,7 @@ pref("browser.tor_provider.cp_log_level", "Warn");
 pref("lox.log_level", "Warn");
 pref("torbrowser.bootstrap.log_level", "Info");
 pref("browser.torsettings.log_level", "Warn");
+pref("browser.torMoat.loglevel", "Warn");
 pref("browser.tordomainisolator.loglevel", "Warn");
 pref("browser.torcircuitpanel.loglevel", "Log");
 pref("browser.tor_android.log_level", "Info");
