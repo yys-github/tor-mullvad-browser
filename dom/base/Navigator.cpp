e2648efad58aff79141beaf6c248f20775a56886
Pier Angelo Vendrame <pierov@torproject.org>
2025-01-24 16:08:27 +0000

    Bug 1840385 - Do not spoof the OS in HTTP User Agent. r=tjr
    Differential Revision: https://phabricator.services.mozilla.com/D234835

--- dom/base/Navigator.cpp
+++ dom/base/Navigator.cpp
@@ -2112,7 +2112,7 @@ nsresult Navigator::GetUserAgent(nsPIDOMWindowInner* aWindow,
   // specific OS version, etc.
   if (shouldResistFingerprinting) {
     nsAutoCString spoofedUA;
-    nsRFPService::GetSpoofedUserAgent(spoofedUA, false);
+    nsRFPService::GetSpoofedUserAgent(spoofedUA);
     CopyASCIItoUTF16(spoofedUA, aUserAgent);
     return NS_OK;
   }
--- toolkit/components/resistfingerprinting/nsRFPService.h
+++ toolkit/components/resistfingerprinting/nsRFPService.h
@@ -58,14 +58,6 @@
 
 #define SPOOFED_POINTER_INTERFACE MouseEvent_Binding::MOZ_SOURCE_MOUSE
 
-// For the HTTP User-Agent header, we use a simpler set of spoofed values
-// that do not reveal the specific desktop platform.
-#if defined(MOZ_WIDGET_ANDROID)
-#  define SPOOFED_HTTP_UA_OS "Android 10; Mobile"
-#else
-#  define SPOOFED_HTTP_UA_OS "Windows NT 10.0"
-#endif
-
 struct JSContext;
 
 class nsIChannel;
@@ -268,7 +260,7 @@ class nsRFPService final : public nsIObserver, public nsIRFPService {
   // --------------------------------------------------------------------------
 
   // This method generates the spoofed value of User Agent.
-  static void GetSpoofedUserAgent(nsACString& userAgent, bool isForHTTPHeader);
+  static void GetSpoofedUserAgent(nsACString& userAgent);
 
   // --------------------------------------------------------------------------
 
