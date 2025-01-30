7f03e926306a4ec9c3ec1d8fa0257c97a71aa2f8
Beatriz Rizental <brizental@torproject.org>
2025-01-30 19:12:00 +0100

    TB 42669: [android] Use custom no-op app-services
    Fetch the custom built no-op application services library
    from tor-browser-build when building for Android.

--- mobile/android/android-components/components/browser/storage-sync/src/main/java/mozilla/components/browser/storage/sync/Types.kt
+++ mobile/android/android-components/components/browser/storage-sync/src/main/java/mozilla/components/browser/storage/sync/Types.kt
@@ -72,6 +72,7 @@ internal fun mozilla.appservices.places.uniffi.VisitType.into() = when (this) {
     mozilla.appservices.places.uniffi.VisitType.REDIRECT_TEMPORARY -> VisitType.REDIRECT_TEMPORARY
     mozilla.appservices.places.uniffi.VisitType.DOWNLOAD -> VisitType.DOWNLOAD
     mozilla.appservices.places.uniffi.VisitType.FRAMED_LINK -> VisitType.FRAMED_LINK
+    mozilla.appservices.places.uniffi.VisitType.__NOOP -> VisitType.LINK
 }
 
 internal fun mozilla.appservices.places.uniffi.HistoryVisitInfo.into(): VisitInfo {
@@ -133,6 +134,18 @@ internal fun BookmarkItem.asBookmarkNode(): BookmarkNode {
                 null,
             )
         }
+        is BookmarkItem.__NOOP -> {
+            BookmarkNode(
+                BookmarkNodeType.ITEM,
+                "",
+                "",
+                null,
+                null,
+                null,
+                0L,
+                null,
+            )
+        }
     }
 }
 
@@ -156,6 +169,7 @@ internal fun mozilla.appservices.places.uniffi.DocumentType.into(): DocumentType
     return when (this) {
         mozilla.appservices.places.uniffi.DocumentType.REGULAR -> DocumentType.Regular
         mozilla.appservices.places.uniffi.DocumentType.MEDIA -> DocumentType.Media
+        mozilla.appservices.places.uniffi.DocumentType.__NOOP -> DocumentType.Regular
     }
 }
 
