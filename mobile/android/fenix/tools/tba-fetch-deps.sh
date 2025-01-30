559c96637624362f7c1d09a7da0a119fba5d9716
Beatriz Rizental <brizental@torproject.org>
2025-01-30 19:12:00 +0100

    TB 42669: [android] Use custom no-op app-services
    Fetch the custom built no-op application services library
    from tor-browser-build when building for Android.

--- mobile/android/fenix/tools/tba-fetch-deps.sh
+++ mobile/android/fenix/tools/tba-fetch-deps.sh
@@ -28,7 +28,7 @@ if [[ $1 == "--tbb" ]]; then
   tor_expert_bundle_aar="$(ls -1td "$TOR_BROWSER_BUILD/out/tor-expert-bundle-aar/"tor-expert-bundle-aar-* | head -1)"
   cp "$tor_expert_bundle_aar"/* app/
 else
-  tor_expert_bundle_aar_dirname="$(curl -s $TBB_BUILD_06/tor-expert-bundle-aar/ | sed -nE 's/.*href=\"(tor-expert-bundle-aar-[0-9a-z\.\-]*).*/\1/p')"
+  tor_expert_bundle_aar_dirname="$(curl -s $TBB_BUILD_06/tor-expert-bundle-aar/ | sed -nE 's/.*href=\"(tor-expert-bundle-aar-[0-9a-z\.\-]*).*/\1/p' | head -n 1)"
   curl -o app/tor-expert-bundle.aar $TBB_BUILD_06/tor-expert-bundle-aar/$tor_expert_bundle_aar_dirname/tor-expert-bundle.aar
 fi
 
@@ -50,3 +50,75 @@ else
   curl -o "app/src/main/assets/extensions/{73a6fe31-595d-460b-a920-fcc0f8843232}.xpi" $TBB_BUILD_06/browser/$noscript_fname
 fi
 echo ""
+
+if [ -z "$GRADLE_MAVEN_REPOSITORIES" ]; then
+	GRADLE_MAVEN_REPOSITORIES="$HOME/.m2/repository"
+fi
+
+os="$(uname -s)"
+case "${os}" in
+    Linux*)     os=unknown-linux;;
+    Darwin*)    os=apple-darwin;;
+	# This is not quite correct, however the only option for the nimbus-fml
+	# build are these three... so if it's not Linux or Darwin it's very likely
+	# we are building from Windows. I apologize in advance to all the BSD users.
+    *)          os="pc-windows";;
+esac
+
+arch="$(uname -m)"
+case "${arch}" in
+	# Also no quite correct, but again these are the only options for nimbus-fml.
+	aarch64)    arch=aarch64;;
+	arm64)      arch=aarch64;;
+	*)          arch="x86_64";;
+esac
+
+if [ "$os" = "unsupported" ] || [ "$arch" = "unsupported" ]; then
+	echo "Android builds from $os-$arch are not supported."
+	exit 2
+fi
+
+echo "Fetching application-services..."
+
+if [[ $1 == "--tbb" ]]; then
+  app_services="$(ls -1t "$TOR_BROWSER_BUILD/out/application-services/"application-services*.tar.zst | head -1)"
+  tar -C /tmp -xf "$app_services"
+else
+  app_services_fname="$(curl -s $TBB_BUILD_06/application-services/ | sed -nE 's/.*href=\"(application-services-[0-9a-z\.\-]*).*/\1/p')"
+  app_services=/tmp/$app_services_fname
+  curl -o $app_services $TBB_BUILD_06/application-services/$app_services_fname
+  tar -C /tmp -xf "$app_services"
+	rm "$app_services"
+fi
+mkdir -p "$GRADLE_MAVEN_REPOSITORIES/org/mozilla"
+if [ -d /tmp/application-services ]; then
+	cp -r /tmp/application-services/maven/org/mozilla/* "$GRADLE_MAVEN_REPOSITORIES/org/mozilla"
+
+	# Over on tor-browser-build all build tools are built for x86_64-linux.
+	# If we are not building from that platform, we need to fetch the correct
+	# nimbus-fml binary.
+	#
+	# Even though we do modify nimbus-fml in tbb, all the changes are made to
+	# support reproducibility and are not necessary for development builds.
+	if [ "$os" != "unknown-linux" ] || [ "$arch" != "x86_64" ]; then
+		echo "Downloading nimbus-fml binary for $arch-$os"
+		app_services_version=$(echo "$app_services" | grep -oE 'application-services-[0-9]+\.[0-9]+(\.[0-9]{1,2})?' | grep -oE '[0-9]+\.[0-9]+(\.[0-9]{1,2})?')
+
+		curl -L -o /tmp/nimbus-fml.zip "https://archive.mozilla.org/pub/app-services/releases/$app_services_version/nimbus-fml.zip"
+		unzip -d /tmp/nimbus-fml /tmp/nimbus-fml.zip
+		nimbus_fml="$(find "/tmp/nimbus-fml/" -name 'nimbus-fml*' | grep "$arch-$os")"
+		echo "Using nimbus-fml binary: $nimbus_fml"
+		cp $nimbus_fml tools/
+
+		rm -rf /tmp/nimbus-fml
+		rm /tmp/nimbus-fml.zip
+	else
+		cp /tmp/application-services/nimbus-fml tools/
+	fi
+	chmod +x tools/nimbus-fml
+
+	rm -rf /tmp/application-services
+else
+	echo "Cannot find application-services artifacts!"
+	exit 2
+fi
