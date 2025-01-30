270b66a24635d29b3879601c0841ae62f53d51f3
Beatriz Rizental <brizental@torproject.org>
2025-01-30 19:12:00 +0100

    TB 42669: [android] Use custom no-op app-services
    Fetch the custom built no-op application services library
    from tor-browser-build when building for Android.

--- mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/Types.kt
+++ mobile/android/android-components/components/service/firefox-accounts/src/main/java/mozilla/components/service/fxa/Types.kt
@@ -128,7 +128,7 @@ internal fun RustDeviceType.into(): DeviceType {
         RustDeviceType.TABLET -> DeviceType.TABLET
         RustDeviceType.TV -> DeviceType.TV
         RustDeviceType.VR -> DeviceType.VR
-        RustDeviceType.UNKNOWN -> DeviceType.UNKNOWN
+        else-> DeviceType.UNKNOWN
     }
 }
 
@@ -165,7 +165,7 @@ fun DeviceCapability.into(): RustDeviceCapability {
 fun RustDeviceCapability.into(): DeviceCapability {
     return when (this) {
         RustDeviceCapability.SEND_TAB -> DeviceCapability.SEND_TAB
-        RustDeviceCapability.CLOSE_TABS -> DeviceCapability.CLOSE_TABS
+        else -> DeviceCapability.CLOSE_TABS
     }
 }
 
@@ -250,7 +250,7 @@ fun AccountEvent.into(): mozilla.components.concept.sync.AccountEvent {
                 deviceId = this.deviceId,
                 isLocalDevice = this.isLocalDevice,
             )
-        is AccountEvent.Unknown -> mozilla.components.concept.sync.AccountEvent.Unknown
+        else -> mozilla.components.concept.sync.AccountEvent.Unknown
     }
 }
 
@@ -258,6 +258,7 @@ fun IncomingDeviceCommand.into(): mozilla.components.concept.sync.DeviceCommandI
     return when (this) {
         is IncomingDeviceCommand.TabReceived -> this.into()
         is IncomingDeviceCommand.TabsClosed -> this.into()
+        is IncomingDeviceCommand.__NOOP -> this.into()
     }
 }
 
