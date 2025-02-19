316158ca314d2279ec3e7d638653aae86fa92665
Henry Wilkes <henry@torproject.org>
2025-02-19 19:27:08 +0000

    fixup! TB 7494: Create local home page for TBB.
    Bug 43489: Add a survey banner in about:tor.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -523,6 +523,7 @@ let JSWINDOWACTORS = {
         DOMContentLoaded: {},
         L10nMutationsFinished: {},
         SubmitSearchOnionize: { wantUntrusted: true },
+        SurveyDismissed: { wantUntrusted: true },
       },
     },
 
