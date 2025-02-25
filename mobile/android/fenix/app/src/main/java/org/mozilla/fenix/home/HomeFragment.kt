7c46e807463ced79862448702a8fe2426594b84f
clairehurst <clairehurst@torproject.org>
2025-02-25 16:29:25 -0700

    fixup! TB 41878: [android] Add standalone Tor Bootstrap
    tor-browser#43329 [Android] Remove remaining traces of the old Bootstrap

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -184,8 +184,6 @@ import org.mozilla.fenix.snackbar.SnackbarBinding
 import org.mozilla.fenix.tabstray.Page
 import org.mozilla.fenix.tabstray.TabsTrayAccessPoint
 import org.mozilla.fenix.theme.FirefoxTheme
-import org.mozilla.fenix.tor.TorBootstrapFragmentDirections
-import org.mozilla.fenix.tor.TorBootstrapStatus
 import org.mozilla.fenix.utils.Settings.Companion.TOP_SITES_PROVIDER_MAX_THRESHOLD
 import org.mozilla.fenix.utils.allowUndo
 import org.mozilla.fenix.wallpapers.Wallpaper
@@ -283,7 +281,6 @@ class HomeFragment : Fragment() {
     private val searchSelectorMenuBinding = ViewBoundFeatureWrapper<SearchSelectorMenuBinding>()
     private val homeScreenPopupManager = ViewBoundFeatureWrapper<HomeScreenPopupManager>()
 
-    private lateinit var torBootstrapStatus: TorBootstrapStatus
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // DO NOT ADD ANYTHING ABOVE THIS getProfilerTime CALL!
@@ -320,12 +317,6 @@ class HomeFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val activity = activity as HomeActivity
         val components = requireComponents
-
-        torBootstrapStatus = TorBootstrapStatus(
-            !BuildConfig.DISABLE_TOR,
-            components.torController,
-            ::dispatchModeChanges
-        )
 
         val currentWallpaperName = requireContext().settings().currentWallpaperName
         applyWallpaper(
@@ -1353,7 +1344,6 @@ class HomeFragment : Fragment() {
     override fun onStop() {
         dismissRecommendPrivateBrowsingShortcut()
         super.onStop()
-        torBootstrapStatus.unregisterTorListener()
     }
 
     override fun onStart() {
@@ -1404,15 +1394,6 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun dispatchModeChanges(isBootstrapping: Boolean) {
-        if (isBootstrapping) {
-            val directions =
-                TorBootstrapFragmentDirections
-                    .actionStartupTorbootstrap()
-            findNavController().navigate(directions)
-        }
-    }
-
     @VisibleForTesting
     internal fun removeCollectionWithUndo(tabCollection: TabCollection) {
         val snackbarMessage = getString(R.string.snackbar_collection_deleted)
@@ -1435,7 +1416,6 @@ class HomeFragment : Fragment() {
 
     override fun onResume() {
         super.onResume()
-        torBootstrapStatus.registerTorListener()
         if (browsingModeManager.mode == BrowsingMode.Private) {
             activity?.window?.setBackgroundDrawableResource(R.drawable.private_home_background_gradient)
         }
