ee52c680ba7383ba17f6df2af614b54402ebce5d
Henry Wilkes <henry@torproject.org>
2025-02-25 16:40:58 +0000

    fixup! TB 27476: Implement about:torconnect captive portal within Tor Browser
    TB 43463: Renamed TorConnect getCountryCodes to getFrequentRegions.

--- toolkit/modules/RemotePageAccessManager.sys.mjs
+++ toolkit/modules/RemotePageAccessManager.sys.mjs
@@ -283,7 +283,7 @@ export let RemotePageAccessManager = {
       ],
       RPMSendQuery: [
         "torconnect:get-init-args",
-        "torconnect:get-country-codes",
+        "torconnect:get-frequent-regions",
       ],
     },
     "about:welcome": {
