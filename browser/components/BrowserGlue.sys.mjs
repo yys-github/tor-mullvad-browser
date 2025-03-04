b066d16823e624623de050e7850cdc2d6139f7e4
Henry Wilkes <henry@torproject.org>
2025-03-04 15:15:36 +0000

    TB 43405: Show a prompt whenever we fail to apply Tor settings.

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -75,6 +75,8 @@ ChromeUtils.defineESModuleGetters(lazy, {
     "resource://gre/modules/TelemetryReportingPolicy.sys.mjs",
   TRRRacer: "resource:///modules/TRRPerformance.sys.mjs",
   TorProviderBuilder: "resource://gre/modules/TorProviderBuilder.sys.mjs",
+  TorSettingsNotification:
+    "resource:///modules/TorSettingsNotification.sys.mjs",
   WebChannel: "resource://gre/modules/WebChannel.sys.mjs",
   WebProtocolHandlerRegistrar:
     "resource:///modules/WebProtocolHandlerRegistrar.sys.mjs",
@@ -991,6 +993,8 @@ BrowserGlue.prototype = {
     lazy.DragDropFilter.init();
 
     lazy.TorProviderBuilder.firstWindowLoaded();
+
+    lazy.TorSettingsNotification.ready();
 
     ClipboardPrivacy.startup();
 
