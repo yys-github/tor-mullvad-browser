b3fec905be8732135fcdc4776210ca778ccf4daa
clairehurst <clairehurst@torproject.org>
2025-03-17 20:56:08 -0600

    fixup! TB 40041 [android]: Implement Tor Network Settings

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -819,6 +819,17 @@ class SettingsFragment : PreferenceFragmentCompat(), UserInteractionHandler {
                 true
             }
         }
+        requirePreference<Preference>(R.string.pref_key_about_config_shortcut).apply {
+            isVisible = requireContext().settings().showSecretDebugMenuThisSession || Config.channel == ReleaseChannel.Debug
+            setOnPreferenceClickListener {
+                (requireActivity() as HomeActivity).openToBrowserAndLoad(
+                    searchTermOrURL = "about:config",
+                    from = BrowserDirection.FromSettings,
+                    newTab = true,
+                )
+                true
+            }
+        }
     }
 
     @VisibleForTesting
