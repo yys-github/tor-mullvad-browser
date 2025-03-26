c3d75c318b47d2eda7c9588be8e61bb941dc1ad5
clairehurst <clairehurst@torproject.org>
2025-03-26 16:44:15 -0600

    fixup! [android] Implement Android-native Connection Assist UI
    Bug 43565: The quit button on Android doesn't actually exit

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeMenuView.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeMenuView.kt
@@ -206,6 +206,7 @@ class HomeMenuView(
                     activity = homeActivity,
                     coroutineScope = homeActivity.lifecycleScope,
                 )
+                homeActivity.shutDown()
             }
             HomeMenu.Item.ReconnectSync -> {
                 navController.nav(
