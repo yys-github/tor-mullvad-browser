4c8bd29879f1d1b06e3b3287877c92d1160dd657
hackademix <giorgio@maone.net>
2025-04-02 19:31:37 +0200

    BB 41919: Letterboxing, add temporarily visible web content-size indicator on window resizing.

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -9965,6 +9965,7 @@ var StatusPanel = {
     if (XULBrowserWindow.busyUI) {
       types.push("status");
     }
+    types.push("letterboxingStatus");
     types.push("defaultStatus");
     for (type of types) {
       if ((text = XULBrowserWindow[type])) {
