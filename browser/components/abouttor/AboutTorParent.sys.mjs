ac79324b627bb398d0b0a33496a78de7d9d6cabf
henry <henry@torproject.org>
2025-04-15 10:00:33 +0000

    fixup! TB 7494: Create local home page for TBB.
    TB 43647: Use the app locale directly from the parent actor for
    setting the survey banner's language.
    
    
    (cherry picked from commit bc19a47cb9a1fe45e7cd9ecbb7b4a25063aa3891)
    
    Co-authored-by: Henry Wilkes <henry@torproject.org>

--- browser/components/abouttor/AboutTorParent.sys.mjs
+++ browser/components/abouttor/AboutTorParent.sys.mjs
@@ -23,6 +23,7 @@ export class AboutTorParent extends JSWindowActorParent {
             surveyDismissVersionPref,
             0
           ),
+          appLocale: Services.locale.appLocaleAsBCP47,
         });
       case "AboutTor:SetSearchOnionize":
         Services.prefs.setBoolPref(onionizePref, message.data);
