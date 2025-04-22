88b8f52c805a62089aca101c5f94fdf4aa9c7048
clairehurst <clairehurst@torproject.org>
2025-04-21 18:54:17 -0600

    fixup! [android] Implement Android-native Connection Assist UI
    Bug 43645: Swiping away doesn't always disconnect from tor

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -821,6 +821,10 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, TorAn
 
         super.onDestroy()
 
+        if (isFinishing) {
+            exitProcess(0)
+        }
+
         // Diagnostic breadcrumb for "Display already aquired" crash:
         // https://github.com/mozilla-mobile/android-components/issues/7960
         breadcrumb(
