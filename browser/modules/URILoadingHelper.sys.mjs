Kevinisagirl21/tor-browser

TB 27476: Implement about:torconnect captive portal within Tor Browser
- implements new about:torconnect page as tor-launcher replacement
- adds new torconnect component to browser
- tor process management functionality remains implemented in tor-launcher through the TorProtocolService module
- adds warning/error box to about:preferences#tor when not connected to tor

Bug 40773: Update the about:torconnect frontend page to match additional UI flows.

Bug 41608: Add a toolbar status button and a urlbar "Connect" button.

browser/modules/URILoadingHelper.sys.mjs
@@ -11,6 +11,7 @@ const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   AboutNewTab: "resource:///modules/AboutNewTab.sys.mjs",
   BrowserWindowTracker: "resource:///modules/BrowserWindowTracker.sys.mjs",
+  TorConnect: "resource://gre/modules/TorConnect.sys.mjs",
 });
 
 ChromeUtils.defineLazyGetter(lazy, "ReferrerInfo", () =>
@@ -424,6 +425,28 @@ export const URILoadingHelper = {
   openLinkIn(window, url, where, params) {
     if (!where || !url) {
       return;
+    }
+
+    // make sure users are not faced with the scary red 'tor isn't working' screen
+    // if they navigate to about:tor before bootstrapped
+    //
+    // fixes tor-browser#40752
+    // new tabs also redirect to about:tor if browser.newtabpage.enabled is true
+    // otherwise they go to about:blank
+    if (lazy.TorConnect.shouldShowTorConnect) {
+      const homeURLs = [
+        "about:home",
+        "about:privatebrowsing",
+        "about:tor",
+        "about:welcome",
+      ];
+      if (
+        homeURLs.includes(url) ||
+        (url === "about:newtab" &&
+          Services.prefs.getBoolPref("browser.newtabpage.enabled", false))
+      ) {
+        url = lazy.TorConnect.getRedirectURL(url);
+      }
     }
 
     let {

yys-github/tor-mullvad-browser

browser/modules/URILoadingHelper.sys.mjs
@@ -11,6 +11,8 @@ const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   AboutNewTab: "resource:///modules/AboutNewTab.sys.mjs",
   BrowserWindowTracker: "resource:///modules/BrowserWindowTracker.sys.mjs",
+  TorConnect: "resource://gre/modules/TorConnect.sys.mjs",
+  TorConnectParent: "resource://gre/actors/TorConnectParent.sys.mjs",
 });
 
 ChromeUtils.defineLazyGetter(lazy, "ReferrerInfo", () =>
@@ -421,6 +423,28 @@ export const URILoadingHelper = {
   openLinkIn(window, url, where, params) {
     if (!where || !url) {
       return;
+    }
+
+    // make sure users are not faced with the scary red 'tor isn't working' screen
+    // if they navigate to about:tor before bootstrapped
+    //
+    // fixes tor-browser#40752
+    // new tabs also redirect to about:tor if browser.newtabpage.enabled is true
+    // otherwise they go to about:blank
+    if (lazy.TorConnect.shouldShowTorConnect) {
+      const homeURLs = [
+        "about:home",
+        "about:privatebrowsing",
+        "about:tor",
+        "about:welcome",
+      ];
+      if (
+        homeURLs.includes(url) ||
+        (url === "about:newtab" &&
+          Services.prefs.getBoolPref("browser.newtabpage.enabled", false))
+      ) {
+        url = lazy.TorConnectParent.getRedirectURL(url);
+      }
     }
 
     let {