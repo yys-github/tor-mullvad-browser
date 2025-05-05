660b1ec0542dd43e07d0721c6e1c30cbbc677dd5
Pier Angelo Vendrame <pierov@torproject.org>
2025-05-05 16:17:50 +0200

    fixup! BB 42831: Remove the shopping components.

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -615,15 +615,6 @@ var gBrowserInit = {
 
     CaptivePortalWatcher.delayedStartup();
 
-    if (
-      !Services.prefs.getBoolPref(
-        "browser.shopping.experience2023.integratedSidebar",
-        false
-      )
-    ) {
-      ShoppingSidebarManager.ensureInitialized();
-    }
-
     SessionStore.promiseAllWindowsRestored.then(() => {
       this._schedulePerWindowIdleTasks();
       document.documentElement.setAttribute("sessionrestored", "true");
