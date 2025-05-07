78e9c61b9d9332191d235fa83f30380b30e62eeb
Beatriz Rizental <brizental@torproject.org>
2025-05-07 14:37:21 +0200

    fixup! TB 42891: Set the bundled search engine for Tor Browser.

--- toolkit/components/search/AppProvidedSearchEngine.sys.mjs
+++ toolkit/components/search/AppProvidedSearchEngine.sys.mjs
@@ -238,7 +238,6 @@ class IconHandler {
    * Obtains the icon list from the remote settings collection.
    */
   async #buildIconMap() {
-    let iconList = [];
     try {
       this.#iconMap = new Map(
         await (
