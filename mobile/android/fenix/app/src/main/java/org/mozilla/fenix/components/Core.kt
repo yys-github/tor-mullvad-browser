a34445f59bd0afbdd33d6b352d62c2b9e2c1176e
Beatriz Rizental <brizental@torproject.org>
2025-05-13 16:17:33 +0200

    fixup! [android] Disable features and functionality

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Core.kt
@@ -324,7 +324,7 @@ class Core(
                 RecentlyClosedMiddleware(recentlyClosedTabsStorage, RECENTLY_CLOSED_MAX),
                 DownloadMiddleware(context, DownloadService::class.java),
                 ReaderViewMiddleware(),
-                TelemetryMiddleware(context, context.settings(), metrics, crashReporter),
+                // TelemetryMiddleware(context, context.settings(), metrics, crashReporter),
                 ThumbnailsMiddleware(thumbnailStorage),
                 UndoMiddleware(context.getUndoDelay()),
                 RegionMiddleware(context, locationService),
