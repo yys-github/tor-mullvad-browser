96448018099fdc03ea1aed44a00f07149815c010
clairehurst <clairehurst@torproject.org>
2025-07-15 21:07:55 -0600

    fixup! TB 41089: Add tor-browser build scripts + Makefile to tor-browser
    Bug 43984: Update android build scripts and docs for ESR 140

--- tools/geckoview/android-env-linux-template.sh
+++ tools/geckoview/android-env-linux-template.sh
@@ -1,6 +1,6 @@
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 export ANDROID_HOME=$HOME/.mozbuild/android-sdk-linux/ # or $HOME/Android/Sdk/ # Or .../android-toolchain/android-sdk-linux if you extract android-toolchain from tor-browser-build
-export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/r26c/ # for 128esr
-export GRADLE_HOME=/FULL/PATH/TO/tor-browser-build/out/gradle/gradle-8.8 # Or the version that we currently use
+export ANDROID_NDK_HOME=$HOME/.mozbuild/android-ndk-r28b/ # for 140esr
+export GRADLE_HOME=$HOME/.mozbuild/gradle-8.14.3 # not included by default, need to download from https://gradle.org/releases/ and put the extracted directory "gradle-8.14.3" into ~/.mozbuild/
 export LOCAL_DEV_BUILD=1
 export PATH=/FULL/PATH/TO/tor-browser-build/out/clang/clang-16.x.y-arm/bin/:$PATH # prepend our newly built and assembled clang to the path so it gets used to build geckoview
