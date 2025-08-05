9ba1b92d2224d137757aa09b171fde532dcbf2d8
Pier Angelo Vendrame <pierov@torproject.org>
2025-08-05 14:58:28 +0200

    fixup! Various Android workarounds
    Comment a couple of calls until we update A-S.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/account/AccountSettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/account/AccountSettingsFragment.kt
@@ -88,7 +88,8 @@ class AccountSettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        SyncTelemetry.processOpenSyncSettingsMenuTelemetry()
+        // FIXME: Update A-S
+        // SyncTelemetry.processOpenSyncSettingsMenuTelemetry()
         SyncAccount.opened.record(NoExtras())
     }
 
@@ -114,7 +115,8 @@ class AccountSettingsFragment : PreferenceFragmentCompat() {
                 }
             }
         }
-        SyncTelemetry.processSaveSyncSettingsTelemetry(enabledEngines, disabledEngines)
+        // FIXME: Update A-S
+        // SyncTelemetry.processSaveSyncSettingsTelemetry(enabledEngines, disabledEngines)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
