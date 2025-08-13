2896ec7458a1c62a734ed9ac5cfadb42249d43d3
Fatih Kilic <me@fklc.dev>
2025-08-13 15:05:34 +0000

    Bug 1977576 - Pass JSContext to VideoFrame::PrefEnabled in more places. r=manuel,chunmin,tjr
    Differential Revision: https://phabricator.services.mozilla.com/D257488

--- dom/media/webcodecs/VideoFrame.h
+++ dom/media/webcodecs/VideoFrame.h
@@ -104,7 +104,7 @@ class VideoFrame final : public nsISupports,
   JSObject* WrapObject(JSContext* aCx,
                        JS::Handle<JSObject*> aGivenProto) override;
 
-  static bool PrefEnabled(JSContext* aCx = nullptr, JSObject* aObj = nullptr);
+  static bool PrefEnabled(JSContext* aCx, JSObject* aObj = nullptr);
 
   static already_AddRefed<VideoFrame> Constructor(
       const GlobalObject& aGlobal, HTMLImageElement& aImageElement,
