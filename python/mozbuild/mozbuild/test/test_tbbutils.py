2ad123352b77a8cdbf14f224eda6285801a0c991
Beatriz Rizental <bea@torproject.org>
2025-08-19 15:58:40 +0200

    TB 43564: Modify ./mach bootstrap for Tor Browser

--- python/mozbuild/mozbuild/test/test_tbbutils.py
+++ python/mozbuild/mozbuild/test/test_tbbutils.py
@@ -4,7 +4,11 @@ from unittest.mock import MagicMock, patch
 
 import mozunit
 
-from mozbuild.tbbutils import get_artifact_index, get_artifact_path, list_files_http
+from mozbuild.tbbutils import (
+    get_artifact_index,
+    get_artifact_path,
+    list_files_http,
+)
 
 
 class TestGetArtifactName(unittest.TestCase):
@@ -36,9 +40,16 @@ class TestGetArtifactName(unittest.TestCase):
 
 class TestGetArtifactIndex(unittest.TestCase):
     def test_regular_artifact(self):
+        artifact = "tor"
         path = "https://tb-build-06.torproject.org/~tb-builder/tor-browser-build/out/tor/tor-b1f9824464dc-linux-x86_64-b0ffe2.tar.gz"
         expected = "tor-b1f9824464dc-linux-x86_64-b0ffe2.tar.gz"
-        self.assertEqual(get_artifact_index(path), expected)
+        self.assertEqual(get_artifact_index(path, artifact), expected)
+
+    def test_expert_bundle_artifact(self):
+        artifact = "tor-expert-bundle"
+        path = "https://tb-build-06.torproject.org/~tb-builder/tor-browser-build/out/tor-expert-bundle/tor-expert-bundle-linux-x86_64-tbb-nightly.2025.10.14-d9aa09/"
+        expected = "tor-expert-bundle-linux-x86_64-tbb-nightly.2025.10.14-d9aa09"
+        self.assertEqual(get_artifact_index(path, artifact), expected)
 
 
 class TestGetArtifactPath(unittest.TestCase):
@@ -145,6 +156,7 @@ class TestListFilesHttp(unittest.TestCase):
     def test_tor_expert_bundle_rewrites(self, mock_urlopen):
         html = """
             <a href="tor-expert-bundle">bundle</a>
+            <a href="tor-expert-bundle-aar">bundle</a>
         """
         mock_resp = MagicMock()
         mock_resp.status = 200
@@ -156,6 +168,7 @@ class TestListFilesHttp(unittest.TestCase):
             result,
             [
                 "tor-expert-bundle/tor-expert-bundle.tar.gz",
+                "tor-expert-bundle-aar/tor-expert-bundle.aar",
             ],
         )
 
