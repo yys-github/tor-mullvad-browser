893598234f5d3f03e1c5327ea3499b26786b364e
clairehurst <clairehurst@torproject.org>
2025-08-25 17:17:59 -0600

    fixup! TB 40041 [android]: Implement Tor Network Settings
    Better fix for #44036 Crash on opening "Search Settings" on android

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -12,6 +12,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.StrictMode
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -37,7 +38,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import mozilla.components.browser.state.state.selectedOrDefaultSearchEngine
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.sync.AccountObserver
@@ -191,7 +191,7 @@ class SettingsFragment : PreferenceFragmentCompat(), UserInteractionHandler {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        runBlocking(context = Dispatchers.IO) {
+        requireContext().components.strictMode.resetAfter(StrictMode.allowThreadDiskReads()) {
             setPreferencesFromResource(R.xml.preferences, rootKey)
         }
     }
