8fdd940fb265e19952d44a914e64ee42d52583ab
hackademix <giorgio@maone.net>
2025-08-26 11:42:59 +0200

    TB 44127: Do not show macOS Privacy hint on network error pages

--- toolkit/actors/NetErrorChild.sys.mjs
+++ toolkit/actors/NetErrorChild.sys.mjs
@@ -246,6 +246,9 @@ export class NetErrorChild extends RemotePageChild {
   }
 
   RPMShowOSXLocalNetworkPermissionWarning() {
+    // Short-circuit per tor-browser#44127
+    return false;
+    /*
     if (!lazy.AppInfo.isMac) {
       return false;
     }
@@ -257,5 +260,6 @@ export class NetErrorChild extends RemotePageChild {
     let version = parseInt(Services.sysinfo.getProperty("version"));
     // We only show this error on Sequoia or later
     return version >= 24;
+    */
   }
 }
