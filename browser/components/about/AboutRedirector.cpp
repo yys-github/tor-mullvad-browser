69c5e012a4fcb2e22ee3544ad2f3583a59d83586
Henry Wilkes <henry@torproject.org>
2025-08-26 17:22:12 +0100

    BB 43901: Modify about:license.
    We also drop about:rights.

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -96,9 +96,7 @@ static const RedirEntry kRedirMap[] = {
     {"referrals", "https://www.firefox.com/invite/",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
          nsIAboutModule::URI_MUST_LOAD_IN_CHILD},
-    {"rights", "https://www.mozilla.org/about/legal/terms/firefox/",
-     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
-         nsIAboutModule::URI_MUST_LOAD_IN_CHILD},
+// Drop about:rights. tor-browser#43901.
 #ifndef BASE_BROWSER_VERSION
     {"robots", "chrome://browser/content/aboutRobots.xhtml",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
