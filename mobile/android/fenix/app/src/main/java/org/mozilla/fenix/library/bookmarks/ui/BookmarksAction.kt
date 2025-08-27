3e091ad827b313c7bebfead44ee3f203bc82cced
clairehurst <clairehurst@torproject.org>
2025-08-27 14:29:06 -0600

    TB 44098: [android] Disable sync

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/library/bookmarks/ui/BookmarksAction.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/library/bookmarks/ui/BookmarksAction.kt
@@ -82,7 +82,6 @@ internal data object SearchClicked : BookmarksAction
 internal data object AddFolderClicked : BookmarksAction
 internal data object CloseClicked : BookmarksAction
 internal data object BackClicked : BookmarksAction
-internal data object SignIntoSyncClicked : BookmarksAction
 internal data class EditBookmarkClicked(val bookmark: BookmarkItem.Bookmark) : BookmarksAction
 internal data class ReceivedSyncSignInUpdate(val isSignedIn: Boolean) : BookmarksAction
 internal data object FirstSyncCompleted : BookmarksAction
