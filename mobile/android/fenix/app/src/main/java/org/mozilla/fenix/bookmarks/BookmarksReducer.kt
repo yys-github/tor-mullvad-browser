3d0b6e37d6d42f0ff97d84b045341c3772fab402
clairehurst <clairehurst@torproject.org>
2025-08-27 14:29:06 -0600

    TB 44098: [android] Disable sync
    tor-browser#44098 Bookmarks offer a way to go to sync in 15.0a1

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/bookmarks/BookmarksReducer.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/bookmarks/BookmarksReducer.kt
@@ -169,7 +169,6 @@ internal fun bookmarksReducer(state: BookmarksState, action: BookmarksAction) = 
     FirstSyncCompleted,
     ViewDisposed,
     SelectFolderAction.ViewAppeared,
-    SignIntoSyncClicked,
     is InitEdit,
     Init,
     PrivateBrowsingAuthorized,
