b1021919f5cedb4c3f0e244cb68bc933439d4636
clairehurst <clairehurst@torproject.org>
2025-09-04 15:20:20 -0600

    TB 44027 [android]: Update PBM lockscreen
    tor-browser#44027 Update PBM lockscreen

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/pbmlock/UnlockPrivateTabsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/pbmlock/UnlockPrivateTabsFragment.kt
@@ -70,8 +70,7 @@ class UnlockPrivateTabsFragment : Fragment(), UserInteractionHandler {
                 UnlockPrivateTabsScreen(
                     onUnlockClicked = { requestPrompt() },
                     onLeaveClicked = {
-                        PrivateBrowsingLocked.seeOtherTabsClicked.record()
-                        closeFragment()
+                        requireActivity().moveTaskToBack(true)
                     },
                     showNegativeButton = !isCustomPrivateTab,
                 )
@@ -88,13 +87,14 @@ class UnlockPrivateTabsFragment : Fragment(), UserInteractionHandler {
     }
 
     override fun onBackPressed(): Boolean {
-        closeFragment()
+        requireActivity().moveTaskToBack(true)
         return true
     }
 
     private fun requestPrompt() {
         DefaultBiometricUtils.bindBiometricsCredentialsPromptOrShowWarning(
-            titleRes = R.string.pbm_authentication_unlock_private_tabs,
+            titleRes = R.string.tor_authentication_unlock_private_tabs,
+            titleRes2 = R.string.app_name,
             view = requireView(),
             onShowPinVerification = { intent -> startForResult.launch(intent) },
             onAuthSuccess = ::onAuthSuccess,
