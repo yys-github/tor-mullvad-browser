dc42afa401cb4ee19d81dcd9f3314db2e0a1e6ca
Pier Angelo Vendrame <pierov@torproject.org>
2025-09-08 09:15:26 +0200

    fixup! TB 43006: Disable RFP for Font Visibility on Android
    TB 43943: Refactor fontvis exclusion on Android.
    
    Rather than updating Document, we can use
    nsRFPService::HandleExeptionalRFPTargets.

--- dom/base/Document.cpp
+++ dom/base/Document.cpp
@@ -17308,12 +17308,6 @@ bool Document::RecomputeResistFingerprinting(bool aForceRefreshRTPCallerType) {
 }
 
 bool Document::ShouldResistFingerprinting(RFPTarget aTarget) const {
-#ifdef ANDROID
-  if (aTarget == RFPTarget::FontVisibilityBaseSystem ||
-      aTarget == RFPTarget::FontVisibilityLangPack) {
-    return false;
-  }
-#endif
   return mShouldResistFingerprinting &&
          nsRFPService::IsRFPEnabledFor(this->IsInPrivateBrowsing(), aTarget,
                                        mOverriddenFingerprintingSettings);
