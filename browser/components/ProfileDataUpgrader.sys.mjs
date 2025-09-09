b73fd3fe6495b2510cb753943129b521c0f004dd
Henry Wilkes <henry@torproject.org>
2025-09-09 15:01:20 +0100

    fixup! TB 41435: Add a Tor Browser migration function
    TB 44180: Clear YEC 2024 preference.

--- browser/components/ProfileDataUpgrader.sys.mjs
+++ browser/components/ProfileDataUpgrader.sys.mjs
@@ -993,7 +993,9 @@ export let ProfileDataUpgrader = {
     //            (tor-browser#43567).
     // Version 8: Tor Browser 15.0a2: Remove legacy search addons
     //            (tor-browser#43111).
-    const TBB_MIGRATION_VERSION = 8;
+    // Version 9: Tor Browser 15.0a3: Remove YEC 2024 preference.
+    //            (tor-browser#44180)
+    const TBB_MIGRATION_VERSION = 9;
     const MIGRATION_PREF = "torbrowser.migration.version";
 
     // If we decide to force updating users to pass through any version
@@ -1092,6 +1094,9 @@ export let ProfileDataUpgrader = {
         "startpage-onion@search.mozilla.org",
         "wikipedia@search.mozilla.org",
       ]);
+    }
+    if (currentVersion < 9) {
+      Services.prefs.clearUserPref("torbrowser.homepage.yec2024.message");
     }
 
     Services.prefs.setIntPref(MIGRATION_PREF, TBB_MIGRATION_VERSION);
