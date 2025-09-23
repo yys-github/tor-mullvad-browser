81256076128349db5ef35f1a38880eac23b1ab00
Lee Salzman <lsalzman@mozilla.com>
2025-09-23 14:50:54 +0000

    Bug 1989945. r=ahale a=dmeehan
    Differential Revision: https://phabricator.services.mozilla.com/D265664

--- dom/canvas/DrawTargetWebgl.cpp
+++ dom/canvas/DrawTargetWebgl.cpp
@@ -1698,6 +1698,10 @@ void DrawTargetWebgl::PushDeviceSpaceClipRects(const IntRect* aRects,
 }
 
 void DrawTargetWebgl::PopClip() {
+  if (mClipStack.empty()) {
+    return;
+  }
+
   mClipChanged = true;
   mRefreshClipState = true;
   mSkia->PopClip();
