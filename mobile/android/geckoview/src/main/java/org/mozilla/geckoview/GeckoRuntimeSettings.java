88bad7b3e632744e27129876c14fd0518e69b04f
Pier Angelo Vendrame <pierov@torproject.org>
2025-09-24 15:30:56 +0200

    fixup! BB 42562: Normalized the Accepted Languages on Android.
    BB 43401: Replace Locale constructors with Locale.Builder.

--- mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
+++ mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntimeSettings.java
@@ -638,7 +638,8 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
       for (String tag : locales) {
         Locale locale = Locale.forLanguageTag(tag);
         getSettings().mSupportedLocales.put(locale, locale);
-        getSettings().mSupportedLocales.put(new Locale(locale.getLanguage()), locale);
+        Locale lang = new Locale.Builder().setLanguage(locale.getLanguage()).build();
+        getSettings().mSupportedLocales.put(lang, locale);
       }
       return this;
     }
@@ -1494,7 +1495,7 @@ public final class GeckoRuntimeSettings extends RuntimeSettings {
     if (mSupportedLocales.containsKey(exact)) {
       return exact;
     }
-    Locale fallback = new Locale(exact.getLanguage());
+    Locale fallback = new Locale.Builder().setLanguage(exact.getLanguage()).build();
     return mSupportedLocales.get(fallback);
   }
 
