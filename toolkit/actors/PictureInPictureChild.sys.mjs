60441c9f5a585e3307ff544d322762c747575ab5
Mike Conley <mconley@mozilla.com>
2025-09-25 16:09:05 +0000

    Bug 1880634 - Use chrome-only dispatch for the MozTogglePictureInPicture event. r=niklas
    Differential Revision: https://phabricator.services.mozilla.com/D202063

--- toolkit/actors/PictureInPictureChild.sys.mjs
+++ toolkit/actors/PictureInPictureChild.sys.mjs
@@ -191,7 +191,10 @@ export class PictureInPictureLauncherChild extends JSWindowActorChild {
           detail: { reason },
         }
       );
-      video.dispatchEvent(stopPipEvent);
+      this.contentWindow.windowUtils.dispatchEventToChromeOnly(
+        video,
+        stopPipEvent
+      );
       return;
     }
 
@@ -703,7 +706,7 @@ export class PictureInPictureToggleChild extends JSWindowActorChild {
           detail: { reason: "UrlBar", eventExtraKeys },
         }
       );
-      video.dispatchEvent(pipEvent);
+      this.contentWindow.windowUtils.dispatchEventToChromeOnly(video, pipEvent);
     }
   }
 
@@ -1092,7 +1095,7 @@ export class PictureInPictureToggleChild extends JSWindowActorChild {
         detail: { reason: "Toggle" },
       }
     );
-    video.dispatchEvent(pipEvent);
+    this.contentWindow.windowUtils.dispatchEventToChromeOnly(video, pipEvent);
 
     // Since we've initiated Picture-in-Picture, we can go ahead and
     // hide the toggle now.
