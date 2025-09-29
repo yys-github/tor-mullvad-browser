40581f92f92fbcc7c8f9b458edcb77efb0458e56
Lee Salzman <lsalzman@mozilla.com>
2025-09-29 15:04:23 +0000

    Bug 1990970. r=gfx-reviewers,ahale a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D266513
    
    Resolved conflicts in gfx/2d/HelpersSkia.h

--- gfx/2d/RecordedEventImpl.h
+++ gfx/2d/RecordedEventImpl.h
@@ -3456,9 +3456,14 @@ inline bool RecordedSourceSurfaceCreation::PlayEvent(
     return false;
   }
 
-  RefPtr<SourceSurface> src = Factory::CreateWrappingDataSourceSurface(
-      mData, mSize.width * BytesPerPixel(mFormat), mSize, mFormat,
-      [](void* aClosure) { delete[] static_cast<uint8_t*>(aClosure); }, mData);
+  CheckedInt32 stride = CheckedInt32(mSize.width) * BytesPerPixel(mFormat);
+  RefPtr<SourceSurface> src;
+  if (!mSize.IsEmpty() && stride.isValid() && stride.value() > 0) {
+    src = Factory::CreateWrappingDataSourceSurface(
+        mData, stride.value(), mSize, mFormat,
+        [](void* aClosure) { delete[] static_cast<uint8_t*>(aClosure); },
+        mData);
+  }
   if (src) {
     mDataOwned = false;
   }
@@ -3498,18 +3503,23 @@ RecordedSourceSurfaceCreation::RecordedSourceSurfaceCreation(S& aStream)
     return;
   }
 
-  size_t size = 0;
+  CheckedInt<size_t> size;
   if (mSize.width >= 0 && mSize.height >= 0) {
-    size = size_t(mSize.width) * size_t(mSize.height) * BytesPerPixel(mFormat);
-    mData = new (fallible) uint8_t[size];
+    CheckedInt32 stride = CheckedInt32(mSize.width) * BytesPerPixel(mFormat);
+    if (stride.isValid() && stride.value() >= 0) {
+      size = CheckedInt<size_t>(stride.value()) * size_t(mSize.height);
+      if (size.isValid()) {
+        mData = new (fallible) uint8_t[size.value()];
+      }
+    }
   }
   if (!mData) {
     gfxCriticalNote
         << "RecordedSourceSurfaceCreation failed to allocate data of size "
-        << size;
+        << (size.isValid() ? size.value() : 0);
     aStream.SetIsBad();
   } else {
-    aStream.read((char*)mData, size);
+    aStream.read((char*)mData, size.value());
   }
 }
 
