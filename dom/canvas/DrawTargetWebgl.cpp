0929bdcbee90c0da66512c25637c48e323b92e41
Lee Salzman <lsalzman@mozilla.com>
2025-09-29 15:04:23 +0000

    Bug 1990970. r=gfx-reviewers,ahale a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D266513
    
    Resolved conflicts in gfx/2d/HelpersSkia.h

--- dom/canvas/DrawTargetWebgl.cpp
+++ dom/canvas/DrawTargetWebgl.cpp
@@ -374,8 +374,14 @@ void SharedContextWebgl::ClearCachesIfNecessary() {
 // not exceed the available texture limits and that shader creation succeeded.
 bool DrawTargetWebgl::Init(const IntSize& size, const SurfaceFormat format,
                            const RefPtr<SharedContextWebgl>& aSharedContext) {
-  MOZ_ASSERT(format == SurfaceFormat::B8G8R8A8 ||
-             format == SurfaceFormat::B8G8R8X8);
+  switch (format) {
+    case SurfaceFormat::B8G8R8A8:
+    case SurfaceFormat::B8G8R8X8:
+      break;
+    default:
+      MOZ_ASSERT_UNREACHABLE("Unsupported format for DrawTargetWebgl.");
+      return false;
+  }
 
   mSize = size;
   mFormat = format;
