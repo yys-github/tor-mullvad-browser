6e7b36248783210d588df3927814a8e2b0050df4
clairehurst <clairehurst@torproject.org>
2025-09-30 17:59:39 -0600

    TB 44031 [android]: 2025 YEC
    # Conflicts:
    #	mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tor/TorHomePage.kt

---
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tor/TorCampaignViewModel.kt
@@ -0,0 +1,35 @@
+package org.mozilla.fenix.tor
+
+import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import kotlin.getValue
+
+class TorCampaignViewModel : ViewModel() {
+    val shouldInitiallyShowPromo: MutableState<Boolean> by lazy {
+        mutableStateOf(shouldInitiallyShowPromo())
+    }
+
+    fun shouldInitiallyShowPromo(): Boolean {
+//        return true // uncomment to test
+
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd-hh-zzz")
+        val startDate =
+            dateFormat.parse("2025-10-14-15-UTC") // from https://gitlab.torproject.org/tpo/web/team/-/issues/66
+        val endDate =
+            dateFormat.parse("2026-01-02-00-UTC") // from https://gitlab.torproject.org/tpo/web/team/-/issues/66#note_3257224
+        val currentDate = Date()
+
+        if (currentDate.before(startDate) || currentDate.after(endDate)) {
+            return false
+        }
+        Log.d("TorCampaignViewModel", "org.mozilla.fenix.BuildConfig.BUILD_TYPE = ${org.mozilla.fenix.BuildConfig.BUILD_TYPE}")
+        if (org.mozilla.fenix.BuildConfig.BUILD_TYPE == "release") {
+            return true
+        }
+        return false
+    }
+}
