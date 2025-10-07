a5f70d01cbc1a6483104fd41cbdee858f26853f5
clairehurst <clairehurst@torproject.org>
2025-10-07 01:20:42 -0600

    fixup! [android] Modify UI/UX

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -586,9 +586,7 @@ class HomeFragment : Fragment() {
             listenForMicrosurveyMessage(requireContext())
         }
 
-        binding.torHomepageView.setContent {
-            initComposeTorHomePageView()
-        }
+        initComposeTorHomePageView()
 
         disableAppBarDragging()
 
