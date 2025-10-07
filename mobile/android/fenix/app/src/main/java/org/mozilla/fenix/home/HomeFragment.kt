1620eeaa9e9c688c1f20502e389b1c4b44d9ecd8
clairehurst <clairehurst@torproject.org>
2025-10-07 01:26:08 -0600

    fixup! TB 44031 [android]: 2025 YEC

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/HomeFragment.kt
@@ -161,9 +161,9 @@ import org.mozilla.fenix.wallpapers.Wallpaper
 import org.mozilla.fenix.GleanMetrics.TabStrip as TabStripMetrics
 
 import org.mozilla.fenix.components.toolbar.ToolbarPosition
+import org.mozilla.fenix.tor.TorCampaignViewModel
 import org.mozilla.fenix.tor.TorHomePage
 import org.mozilla.fenix.tor.UrlQuickLoadViewModel
-import org.mozilla.fenix.tor.shouldInitiallyShowPromo
 import java.util.Locale
 
 @Suppress("TooManyFunctions", "LargeClass")
@@ -179,6 +179,7 @@ class HomeFragment : Fragment(), UserInteractionHandler {
     internal val binding get() = _binding!!
     private val snackbarBinding = ViewBoundFeatureWrapper<SnackbarBinding>()
 
+    private val torCampaignViewModel: TorCampaignViewModel by activityViewModels()
     private val homeViewModel: HomeScreenViewModel by activityViewModels()
     private val urlQuickLoadViewModel: UrlQuickLoadViewModel by activityViewModels()
 
@@ -976,7 +977,7 @@ class HomeFragment : Fragment(), UserInteractionHandler {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 TorHomePage(
-                    shouldInitiallyShowPromo(),
+                    torCampaignViewModel.shouldInitiallyShowPromo,
                     onClicked = {
                         val baseUrl =  "https://www.torproject.org/donate"
                         val locale = Locale.getDefault().getLanguage()
