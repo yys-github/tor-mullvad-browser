Kevinisagirl21/tor-browser

TB 40015: [android] Port padlock states for .onion services
Originally, android-components#40015.

mobile/android/android-components/components/feature/toolbar/src/main/java/mozilla/components/feature/toolbar/ToolbarPresenter.kt
@@ -18,6 +18,7 @@ import mozilla.components.concept.toolbar.Toolbar.Highlight
 import mozilla.components.concept.toolbar.Toolbar.SiteTrackingProtection
 import mozilla.components.feature.toolbar.internal.URLRenderer
 import mozilla.components.lib.state.ext.flowScoped
+import mozilla.components.support.ktx.kotlin.isOnionUrl
 
 /**
  * Presenter implementation for a toolbar implementation in order to update the toolbar whenever
@@ -69,7 +70,11 @@ class ToolbarPresenter(
             toolbar.displayProgress(tab.content.progress)
 
             toolbar.siteSecure = if (tab.content.securityInfo.secure) {
-                Toolbar.SiteSecurity.SECURE
+                if (tab.content.url.isOnionUrl()) {
+                    Toolbar.SiteSecurity.ONION
+                } else {
+                    Toolbar.SiteSecurity.SECURE
+                }
             } else {
                 Toolbar.SiteSecurity.INSECURE
             }

yys-github/tor-mullvad-browser

mobile/android/android-components/components/feature/toolbar/src/main/java/mozilla/components/feature/toolbar/ToolbarPresenter.kt
@@ -19,6 +19,7 @@ import mozilla.components.concept.toolbar.Toolbar.SiteTrackingProtection
 import mozilla.components.feature.toolbar.internal.URLRenderer
 import mozilla.components.lib.state.ext.flowScoped
 import mozilla.components.support.utils.ext.isContentUrl
+import mozilla.components.support.ktx.kotlin.isOnionUrl
 
 /**
  * Presenter implementation for a toolbar implementation in order to update the toolbar whenever
@@ -72,7 +73,11 @@ class ToolbarPresenter(
             toolbar.siteInfo = if (tab.content.url.isContentUrl()) {
                 Toolbar.SiteInfo.LOCAL_PDF
             } else if (tab.content.securityInfo.secure) {
-                Toolbar.SiteInfo.SECURE
+                if (tab.content.url.isOnionUrl()) {
+                    Toolbar.SiteInfo.ONION
+                } else {
+                    Toolbar.SiteInfo.SECURE
+                }
             } else {
                 Toolbar.SiteInfo.INSECURE
             }