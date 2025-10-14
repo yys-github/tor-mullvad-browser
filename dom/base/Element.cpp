3e1e3b80c43d4eb76febf78882db507f4521f81f
Pier Angelo Vendrame <pierov@torproject.org>
2025-10-14 18:14:23 +0200

    BB 43869: Hide pens with RFP.

--- dom/base/Element.cpp
+++ dom/base/Element.cpp
@@ -303,11 +303,6 @@ nsDOMAttributeMap* Element::Attributes() {
 }
 
 void Element::SetPointerCapture(int32_t aPointerId, ErrorResult& aError) {
-  if (OwnerDoc()->ShouldResistFingerprinting(RFPTarget::PointerId) &&
-      aPointerId != PointerEventHandler::GetSpoofedPointerIdForRFP()) {
-    aError.ThrowNotFoundError("Invalid pointer id");
-    return;
-  }
   const PointerInfo* pointerInfo =
       PointerEventHandler::GetPointerInfo(aPointerId);
   if (!pointerInfo) {
@@ -335,11 +330,6 @@ void Element::SetPointerCapture(int32_t aPointerId, ErrorResult& aError) {
 }
 
 void Element::ReleasePointerCapture(int32_t aPointerId, ErrorResult& aError) {
-  if (OwnerDoc()->ShouldResistFingerprinting(RFPTarget::PointerId) &&
-      aPointerId != PointerEventHandler::GetSpoofedPointerIdForRFP()) {
-    aError.ThrowNotFoundError("Invalid pointer id");
-    return;
-  }
   if (!PointerEventHandler::GetPointerInfo(aPointerId)) {
     aError.ThrowNotFoundError("Invalid pointer id");
     return;
