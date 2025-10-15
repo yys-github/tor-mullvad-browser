5a463ac62752732db6206dde93af32246e8041c8
hackademix <giorgio@maone.net>
2025-10-16 00:35:11 +0200

    fixup! [android] Modify add-on support
    BB 44133: Hide (again) the "Allow in private browsing" checkboxes from WebExtension management UI when in global PBM

--- mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/webextension/GeckoWebExtension.kt
+++ mobile/android/android-components/components/browser/engine-gecko/src/main/java/mozilla/components/browser/engine/gecko/webextension/GeckoWebExtension.kt
@@ -382,6 +382,7 @@ class GeckoWebExtension(
                 temporary = it.temporary,
                 detailUrl = it.amoListingUrl,
                 incognito = Incognito.fromString(it.incognito),
+                defaultPrivateBrowsingAllowed = it.allowedInPrivateBrowsing,
             )
         }
     }
