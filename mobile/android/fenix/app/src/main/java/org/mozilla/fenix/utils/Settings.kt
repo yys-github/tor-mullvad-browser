2ede0f216d8cbaf162161a8003f6b6ca9eded2ae
clairehurst <clairehurst@torproject.org>
2025-10-20 17:15:18 -0600

    fixup! [android] Disable features and functionality

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -883,7 +883,7 @@ class Settings(private val appContext: Context) : PreferencesHolder {
      */
     var showFirstTimeTranslation: Boolean by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_show_first_time_translation),
-        default = true,
+        default = false,
     )
 
     /**
@@ -891,7 +891,7 @@ class Settings(private val appContext: Context) : PreferencesHolder {
      */
     var offerTranslation: Boolean by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_translations_offer),
-        default = true,
+        default = false,
     )
 
     /**
