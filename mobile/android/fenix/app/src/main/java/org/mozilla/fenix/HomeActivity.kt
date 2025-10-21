62eaf300ff064dadad9c8c6c53f096dd2d6dd093
clairehurst <clairehurst@torproject.org>
2025-10-21 17:09:12 -0600

    TB 44196: [android] Persistent notification sometimes does not clear

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -1728,6 +1728,8 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, Crash
     }
 
     fun shutDown() : Nothing {
+        // If we don't manually stop the service, the persistent "close tabs" notification sometimes does not clear
+        applicationContext.stopService(Intent(applicationContext, PrivateNotificationService::class.java))
         finishAndRemoveTask()
         components.torController.shutdown()
         exitProcess(0)
