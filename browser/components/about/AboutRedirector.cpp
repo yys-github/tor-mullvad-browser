ca509ad00ad866857c7b306bed93d0b012e7604a
Pier Angelo Vendrame <pierov@torproject.org>
2025-10-29 10:42:38 +0100

    fixup! BB 31575: Disable Firefox Home (Activity Stream)
    BB 44302: Add URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS to some about pages.

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -36,6 +36,7 @@ struct RedirEntry {
 
 static const uint32_t BASE_BROWSER_HOME_PAGE_FLAGS =
     nsIAboutModule::URI_MUST_LOAD_IN_CHILD |
+    nsIAboutModule::URI_CAN_LOAD_IN_PRIVILEGEDABOUT_PROCESS |
     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
     nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::IS_SECURE_CHROME_UI;
 
