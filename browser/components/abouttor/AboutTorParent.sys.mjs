b3ca96998b7bcad1355209f9375d2de04378880b
Henry Wilkes <henry@torproject.org>
2025-10-30 18:12:46 +0000

    fixup! TB 7494: Create local home page for TBB.
    TB 44314: Delay initial data for preloaded about:tor pages.

--- browser/components/abouttor/AboutTorParent.sys.mjs
+++ browser/components/abouttor/AboutTorParent.sys.mjs
@@ -8,6 +8,8 @@ ChromeUtils.defineESModuleGetters(lazy, {
 });
 
 const initializedActors = new Set();
+const onionizePref = "torbrowser.homepage.search.onionize";
+const surveyDismissVersionPref = "torbrowser.homepage.survey.dismiss_version";
 
 /**
  * Actor parent class for the about:tor page.
@@ -21,35 +23,76 @@ export class AboutTorParent extends JSWindowActorParent {
    */
   static #dismissYEC = false;
 
+  /**
+   * Whether this instance has a preloaded browser.
+   *
+   * @type {boolean}
+   */
+  #preloaded = false;
+
+  /**
+   * Method to be called when the browser corresponding to this actor has its
+   * preloadedState attribute removed.
+   */
+  preloadedRemoved() {
+    if (!this.#preloaded) {
+      return;
+    }
+    this.#preloaded = false;
+    // Send in the initial data now that the page is actually going to be
+    // visible.
+    this.sendAsyncMessage(
+      "AboutTor:DelayedInitialData",
+      this.#getInitialData()
+    );
+  }
+
+  /**
+   * Get the initial data for the page when it is about to be shown.
+   *
+   * @returns {object} - The initial data.
+   */
+  #getInitialData() {
+    let appLocale = Services.locale.appLocaleAsBCP47;
+    if (appLocale === "ja-JP-macos") {
+      appLocale = "ja";
+    }
+
+    return {
+      torConnectEnabled: lazy.TorConnect.enabled,
+      messageData: lazy.AboutTorMessage.getNext(),
+      isStable: AppConstants.MOZ_UPDATE_CHANNEL === "release",
+      searchOnionize: Services.prefs.getBoolPref(onionizePref, false),
+      surveyDismissVersion: Services.prefs.getIntPref(
+        surveyDismissVersionPref,
+        0
+      ),
+      appLocale,
+      dismissYEC: AboutTorParent.#dismissYEC,
+    };
+  }
+
   didDestroy() {
     initializedActors.delete(this);
   }
 
   receiveMessage(message) {
-    const onionizePref = "torbrowser.homepage.search.onionize";
-    const surveyDismissVersionPref =
-      "torbrowser.homepage.survey.dismiss_version";
     switch (message.name) {
       case "AboutTor:GetInitialData": {
         // Track this actor to send future updates.
         initializedActors.add(this);
 
-        let appLocale = Services.locale.appLocaleAsBCP47;
-        if (appLocale === "ja-JP-macos") {
-          appLocale = "ja";
+        const browser = this.browsingContext.top.embedderElement;
+        if (browser?.getAttribute("preloadedState") === "preloaded") {
+          // Wait until the page is actually about to be shown before sending
+          // the initial data.
+          // Otherwise the preloaded page might receive data that has expired by
+          // the time the page is shown. And it will iterate
+          // AboutTorMessage.getNext too early. See tor-browser#44314.
+          this.#preloaded = true;
+          return Promise.resolve(null);
         }
-        return Promise.resolve({
-          torConnectEnabled: lazy.TorConnect.enabled,
-          messageData: lazy.AboutTorMessage.getNext(),
-          isStable: AppConstants.MOZ_UPDATE_CHANNEL === "release",
-          searchOnionize: Services.prefs.getBoolPref(onionizePref, false),
-          surveyDismissVersion: Services.prefs.getIntPref(
-            surveyDismissVersionPref,
-            0
-          ),
-          appLocale,
-          dismissYEC: AboutTorParent.#dismissYEC,
-        });
+        return Promise.resolve(this.#getInitialData());
       }
       case "AboutTor:SetSearchOnionize":
         Services.prefs.setBoolPref(onionizePref, message.data);
