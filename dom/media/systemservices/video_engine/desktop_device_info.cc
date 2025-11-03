Kevinisagirl21/tor-browser

Bug 41459: WebRTC fails to build under mingw (Part 6)
- fixes required to build dom/media/systemservices

dom/media/systemservices/video_engine/desktop_device_info.cc
@@ -67,7 +67,7 @@ void DesktopDisplayDevice::setUniqueIdName(const char* deviceUniqueIdUTF8) {
   SetStringMember(&deviceUniqueIdUTF8_, deviceUniqueIdUTF8);
 }
 
-void DesktopDisplayDevice::setPid(const int pid) { pid_ = pid; }
+void DesktopDisplayDevice::setPid(const pid_t pid) { pid_ = pid; }
 
 ScreenId DesktopDisplayDevice::getScreenId() { return screenId_; }
 

yys-github/tor-mullvad-browser

dom/media/systemservices/video_engine/desktop_device_info.cc
@@ -66,7 +66,7 @@ void DesktopDisplayDevice::setUniqueIdName(const char* aDeviceUniqueIdUTF8) {
   SetStringMember(&mDeviceUniqueIdUTF8, aDeviceUniqueIdUTF8);
 }
 
-void DesktopDisplayDevice::setPid(const int aPid) { mPid = aPid; }
+void DesktopDisplayDevice::setPid(const pid_t aPid) { mPid = aPid; }
 
 ScreenId DesktopDisplayDevice::getScreenId() { return mScreenId; }
 