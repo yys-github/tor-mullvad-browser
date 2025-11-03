c483e2978d7591ce0f5df6fca3f1fe18317ba6d4
Jon Coppeard <jcoppeard@mozilla.com>
2025-11-03 17:23:14 +0000

    Bug 1995637 - Make sure debugger object gets traced when tracing breakpoints r=iain
    Breakpoints currently trace |wrappedDebugger| but this doesn't keep |debugger|
    alive if we have nuked the CCWs. The debugger has a linked list of breakpoints that
    each breakpoint is part of so we need to make sure it lives at least as long as
    that.
    
    The patch traces the debugger's object even if our CCW to it has been nuked.
    
    Differential Revision: https://phabricator.services.mozilla.com/D271062

--- js/src/debugger/Debugger.cpp
+++ js/src/debugger/Debugger.cpp
@@ -454,6 +454,9 @@ Breakpoint::Breakpoint(Debugger* debugger, HandleObject wrappedDebugger,
 
 void Breakpoint::trace(JSTracer* trc) {
   TraceEdge(trc, &wrappedDebugger, "breakpoint owner");
+  // Trace the debugger object too in case |wrappedDebugger| got nuked.
+  TraceCrossCompartmentEdge(trc, wrappedDebugger, &debugger->object,
+                            "breakpoint debugger object");
   TraceEdge(trc, &handler, "breakpoint handler");
 }
 
