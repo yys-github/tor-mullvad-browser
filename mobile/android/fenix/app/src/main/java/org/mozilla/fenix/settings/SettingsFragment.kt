Kevinisagirl21/tor-browser

TB 40041 [android]: Implement Tor Network Settings
Originally, fenix#40041.

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -13,6 +13,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.annotation.VisibleForTesting
@@ -50,6 +51,7 @@ import org.mozilla.fenix.GleanMetrics.TrackingProtection
 import org.mozilla.fenix.GleanMetrics.Translations
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
+import org.mozilla.fenix.ReleaseChannel
 import org.mozilla.fenix.components.accounts.FenixFxAEntryPoint
 import org.mozilla.fenix.databinding.AmoCollectionOverrideDialogBinding
 import org.mozilla.fenix.ext.application
@@ -60,11 +62,16 @@ import org.mozilla.fenix.ext.openSetDefaultBrowserOption
 import org.mozilla.fenix.ext.requireComponents
 import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.ext.showToolbar
+import org.mozilla.fenix.gecko.GeckoProvider
 import org.mozilla.fenix.nimbus.FxNimbus
 import org.mozilla.fenix.perf.ProfilerViewModel
 import org.mozilla.fenix.settings.account.AccountUiView
+import org.mozilla.fenix.tor.QuickStartPreference
 import org.mozilla.fenix.tor.SecurityLevel
+import org.mozilla.fenix.tor.TorBridgeTransportConfig
+import org.mozilla.fenix.tor.TorEvents
 import org.mozilla.fenix.utils.Settings
+import org.mozilla.geckoview.BuildConfig
 import kotlin.system.exitProcess
 import org.mozilla.fenix.GleanMetrics.Settings as SettingsMetrics
 
@@ -167,6 +174,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        //setPreferencesFromResource(R.xml.tor_network_settings_preferences, rootKey)
+        //setupConnectionPreferences()
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
@@ -190,7 +199,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
         update(shouldUpdateAccountUIState = !creatingFragment)
 
         requireView().findViewById<RecyclerView>(R.id.recycler_view)
-            ?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+            .also {
+                it?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+                // Prevent disabled settings from having a collapsing animation on open
+                it?.disableHidingAnimation()
+            }
 
         args.preferenceToScrollTo?.let {
             scrollToPreference(it)
@@ -250,9 +263,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //            getString(R.string.preferences_credit_cards_2)
 //        }
 
-        val openLinksInAppsSettingsPreference =
-            requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
-        openLinksInAppsSettingsPreference.summary = context?.settings()?.getOpenLinksInAppsString()
+//        val openLinksInAppsSettingsPreference =
+//            requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
+//        openLinksInAppsSettingsPreference.summary = context?.settings()?.getOpenLinksInAppsString()
 
         // Hide "Delete browsing data on quit" when in Private Browsing-only mode
         deleteBrowsingDataPreference.isVisible =
@@ -429,9 +442,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 null
             }
 
-            resources.getString(R.string.pref_key_open_links_in_apps) -> {
-                SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
-            }
+//            resources.getString(R.string.pref_key_open_links_in_apps) -> {
+//                SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
+//            }
 
             resources.getString(R.string.pref_key_sync_debug) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToSyncDebugFragment()
@@ -560,6 +573,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setupSearchPreference()
         setupHomepagePreference()
         setupTrackingProtectionPreference()
+        setupConnectionPreferences()
     }
 
     /**
@@ -594,6 +608,10 @@ class SettingsFragment : PreferenceFragmentCompat() {
         }
     }
 
+    private fun RecyclerView.disableHidingAnimation() {
+        this.setItemAnimator(null)
+        this.setLayoutAnimation(null)
+    }
     private fun updateFxAAllowDomesticChinaServerMenu() {
         val settings = requireContext().settings()
         val preferenceAllowDomesticChinaServer =
@@ -728,6 +746,43 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 context.settings().useStrictTrackingProtection -> getString(R.string.tracking_protection_strict)
                 context.settings().useCustomTrackingProtection -> getString(R.string.tracking_protection_custom)
                 else -> null
+            }
+        }
+    }
+
+    internal fun setupConnectionPreferences() {
+        // will be needed for phase2
+        //val torController = requireContext().components.torController
+
+        requirePreference<Preference>(R.string.pref_key_tor_network_settings_bridge_config).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections
+                        .actionSettingsFragmentToTorBridgeConfigFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+
+        requirePreference<QuickStartPreference>(R.string.pref_key_quick_start).apply {
+            setOnPreferenceClickListener {
+                context.components.torController.quickstart = !context.components.torController.quickstart
+                updateSwitch()
+                true
+            }
+        }
+
+        requirePreference<Preference>(R.string.pref_key_use_html_connection_ui).apply {
+            onPreferenceChangeListener = object : SharedPreferenceUpdater() {}
+            isVisible = Config.channel != ReleaseChannel.Release
+        }
+
+        requirePreference<Preference>(R.string.pref_key_tor_logs).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections.actionSettingsFragmentToTorLogsFragment()
+                requireView().findNavController().navigate(directions)
+                true
             }
         }
     }

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tor/TorLogsComposeFragment.kt
@@ -1,0 +1,140 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.tor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import mozilla.components.ui.colors.PhotonColors
+import org.mozilla.fenix.R
+
+class TorLogsComposeFragment : Fragment() {
+    private val viewModel: TorLogsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Scaffold(
+                    floatingActionButton = { CopyLogsButton() },
+                    content = { TorLogs(paddingValues = it) },
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun TorLogs(paddingValues: PaddingValues) {
+        val torLogsState = remember { mutableStateOf<List<TorLog>>(emptyList()) }
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val scrollState = rememberScrollState()
+
+        DisposableEffect(viewModel.torLogs(), lifecycleOwner) {
+            val observer = Observer<List<TorLog>> { logs ->
+                torLogsState.value = logs
+            }
+            viewModel.torLogs().observe(lifecycleOwner, observer)
+            onDispose {
+                viewModel.torLogs().removeObserver(observer)
+            }
+        }
+
+        val torLogs = torLogsState.value
+
+        LaunchedEffect(torLogs) {
+            scrollState.animateScrollTo(scrollState.maxValue)
+        }
+
+        SelectionContainer {
+            Column(
+                // Column instead of LazyColumn so that you can select all the logs, and not just one "screen" at a time
+                // The logs won't be too big so loading them all instead of just whats visible shouldn't be a big deal
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .padding(paddingValues)
+                    .background(PhotonColors.Ink50), // Standard background color
+            ) {
+                for (log in torLogs) {
+                    LogRow(log = log)
+                }
+            }
+        }
+    }
+
+    @Composable
+    @Stable
+    private fun LogRow(log: TorLog, modifier: Modifier = Modifier) {
+        Column(
+            modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 16.dp,
+                ),
+        ) {
+            DisableSelection {
+                Text(
+                    text = log.timestamp.toString(),
+                    color = PhotonColors.LightGrey40,
+                    modifier = modifier
+                        .padding(bottom = 4.dp),
+                )
+            }
+            Text(
+                text = log.text,
+                color = PhotonColors.LightGrey05,
+            )
+        }
+    }
+
+    @Composable
+    private fun CopyLogsButton() {
+        FloatingActionButton(
+            onClick = { viewModel.copyAllLogsToClipboard() },
+            content = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_copy),
+                    contentDescription = getString(R.string.share_copy_link_to_clipboard),
+                )
+            },
+            backgroundColor = PhotonColors.Violet50, // Same color as connect button
+            contentColor = PhotonColors.LightGrey05,
+        )
+    }
+}

yys-github/tor-mullvad-browser

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -9,6 +9,7 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.StrictMode
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,6 +31,7 @@ import androidx.preference.SwitchPreference
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import mozilla.components.browser.state.state.selectedOrDefaultSearchEngine
@@ -53,6 +55,7 @@ import org.mozilla.fenix.GleanMetrics.TrackingProtection
 import org.mozilla.fenix.GleanMetrics.Translations
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
+import org.mozilla.fenix.ReleaseChannel
 import org.mozilla.fenix.components.Components
 import org.mozilla.fenix.components.accounts.FenixFxAEntryPoint
 import org.mozilla.fenix.databinding.AmoCollectionOverrideDialogBinding
@@ -72,6 +75,7 @@ import org.mozilla.fenix.settings.account.AccountUiView
 import org.mozilla.fenix.snackbar.FenixSnackbarDelegate
 import org.mozilla.fenix.snackbar.SnackbarBinding
 import org.mozilla.fenix.tor.TorSecurityLevel
+import org.mozilla.fenix.tor.QuickstartViewModel
 import org.mozilla.fenix.utils.Settings
 import kotlin.system.exitProcess
 import org.mozilla.fenix.GleanMetrics.Settings as SettingsMetrics
@@ -87,6 +91,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
         ProfilerViewModelFactory(requireActivity().application)
     }
     private val snackbarBinding = ViewBoundFeatureWrapper<SnackbarBinding>()
+
+    private val quickstartViewModel: QuickstartViewModel by activityViewModels()
 
     @VisibleForTesting
     internal val accountObserver = object : AccountObserver {
@@ -180,7 +186,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        requireContext().components.strictMode.resetAfter(StrictMode.allowThreadDiskReads()) {
+            setPreferencesFromResource(R.xml.preferences, rootKey)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -238,7 +246,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
         )
 
         requireView().findViewById<RecyclerView>(R.id.recycler_view)
-            ?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+            .also {
+                it?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+                // Prevent disabled settings from having a collapsing animation on open
+                it?.disableHidingAnimation()
+            }
 
         args.preferenceToScrollTo?.let {
             scrollToPreference(it)
@@ -303,9 +315,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //            getString(R.string.preferences_credit_cards_2)
 //        }
 
-        val openLinksInAppsSettingsPreference =
-            requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
-        openLinksInAppsSettingsPreference.summary = settings.getOpenLinksInAppsString()
+        // val openLinksInAppsSettingsPreference =
+        //     requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
+        // openLinksInAppsSettingsPreference.summary = settings.getOpenLinksInAppsString()
 
         setupPreferences(settings)
 
@@ -486,9 +498,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 SettingsFragmentDirections.actionSettingsFragmentToLinkSharingFragment()
             }
 
-            resources.getString(R.string.pref_key_open_links_in_apps) -> {
-                SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
-            }
+            // resources.getString(R.string.pref_key_open_links_in_apps) -> {
+            //     SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
+            // }
 
             resources.getString(R.string.pref_key_downloads) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToOpenDownloadsSettingsFragment()
@@ -614,6 +626,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setupHomepagePreference(settings)
         setupTrackingProtectionPreference(settings)
         setupDnsOverHttpsPreference(settings)
+        setupConnectionPreferences()
     }
 
     /**
@@ -646,6 +659,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
             delay(SCROLL_INDICATOR_DELAY)
             scrollBarSize = originalSize
         }
+    }
+
+    private fun RecyclerView.disableHidingAnimation() {
+        this.setItemAnimator(null)
+        this.setLayoutAnimation(null)
     }
 
     @VisibleForTesting
@@ -750,6 +768,56 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 Engine.DohSettingsMode.OFF -> getString(R.string.preference_doh_off)
                 Engine.DohSettingsMode.INCREASED -> getString(R.string.preference_doh_increased_protection)
                 Engine.DohSettingsMode.MAX -> getString(R.string.preference_doh_max_protection)
+            }
+        }
+    }
+
+    internal fun setupConnectionPreferences() {
+        // will be needed for phase2
+        //val torController = requireContext().components.torController
+
+        requirePreference<Preference>(R.string.pref_key_tor_network_settings_bridge_config).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections
+                        .actionSettingsFragmentToTorBridgeConfigFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+
+        requirePreference<SwitchPreference>(R.string.pref_key_quick_start).apply {
+            isChecked = quickstartViewModel.quickstart().value == true
+            setOnPreferenceClickListener {
+                quickstartViewModel.quickstartSet(
+                    isChecked,
+                )
+                true
+            }
+        }
+
+        requirePreference<Preference>(R.string.pref_key_use_html_connection_ui).apply {
+            onPreferenceChangeListener = object : SharedPreferenceUpdater() {}
+            isVisible = Config.channel != ReleaseChannel.Release
+        }
+
+        requirePreference<Preference>(R.string.pref_key_tor_logs).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections.actionSettingsFragmentToTorLogsFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+        requirePreference<Preference>(R.string.pref_key_about_config_shortcut).apply {
+            isVisible = requireContext().settings().showSecretDebugMenuThisSession || Config.channel == ReleaseChannel.Debug
+            setOnPreferenceClickListener {
+                (requireActivity() as HomeActivity).openToBrowserAndLoad(
+                    searchTermOrURL = "about:config",
+                    from = BrowserDirection.FromSettings,
+                    newTab = true,
+                )
+                true
             }
         }
     }

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/tor/TorLogsComposeFragment.kt
@@ -1,0 +1,138 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.tor
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.selection.DisableSelection
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import mozilla.components.ui.colors.PhotonColors
+import org.mozilla.fenix.R
+
+class TorLogsComposeFragment : Fragment() {
+    private val viewModel: TorLogsViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Scaffold(
+                    floatingActionButton = { CopyLogsButton() },
+                    content = { TorLogs(paddingValues = it) },
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun TorLogs(paddingValues: PaddingValues) {
+        val torLogsState = remember { mutableStateOf<List<TorLog>>(emptyList()) }
+        val lifecycleOwner = LocalLifecycleOwner.current
+        val scrollState = rememberScrollState()
+
+        DisposableEffect(viewModel.torLogs(), lifecycleOwner) {
+            val observer = Observer<List<TorLog>> { logs ->
+                torLogsState.value = logs
+            }
+            viewModel.torLogs().observe(lifecycleOwner, observer)
+            onDispose {
+                viewModel.torLogs().removeObserver(observer)
+            }
+        }
+
+        val torLogs = torLogsState.value
+
+        LaunchedEffect(torLogs) {
+            scrollState.animateScrollTo(scrollState.maxValue)
+        }
+
+        SelectionContainer {
+            Column(
+                // Column instead of LazyColumn so that you can select all the logs, and not just one "screen" at a time
+                // The logs won't be too big so loading them all instead of just whats visible shouldn't be a big deal
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .padding(paddingValues)
+                    .background(PhotonColors.Ink50), // Standard background color
+            ) {
+                for (log in torLogs) {
+                    LogRow(log = log)
+                }
+            }
+        }
+    }
+
+    @Composable
+    @Stable
+    private fun LogRow(log: TorLog, modifier: Modifier = Modifier) {
+        Column(
+            modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 16.dp,
+                ),
+        ) {
+            Text(
+                text = log.timestamp,
+                color = PhotonColors.LightGrey40,
+                modifier = modifier
+                    .padding(bottom = 4.dp),
+            )
+            Text(
+                text = "[${log.type}] " + log.text,
+                color = PhotonColors.LightGrey05,
+            )
+        }
+    }
+
+    @Composable
+    private fun CopyLogsButton() {
+        FloatingActionButton(
+            onClick = { viewModel.copyAllLogsToClipboard() },
+            content = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_copy),
+                    contentDescription = getString(R.string.share_copy_link_to_clipboard),
+                )
+            },
+            containerColor = PhotonColors.Violet50, // Same color as connect button
+            contentColor = PhotonColors.LightGrey05,
+        )
+    }
+}