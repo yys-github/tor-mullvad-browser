1ff02123b75c053aafd6e71f4c042eee56047dea
Tom Ritter <tom@mozilla.com>
2025-11-20 14:43:06 +0000

    Bug 1999126: Hide the value of the pdfjs pref from being disabled when RFP is enabled r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D272930

--- dom/base/nsPluginArray.cpp
+++ dom/base/nsPluginArray.cpp
@@ -97,7 +97,11 @@ void nsPluginArray::GetSupportedNames(nsTArray<nsString>& aRetval) {
   }
 }
 
-bool nsPluginArray::ForceNoPlugins() { return StaticPrefs::pdfjs_disabled(); }
+bool nsPluginArray::ForceNoPlugins() {
+  return StaticPrefs::pdfjs_disabled() &&
+         !nsContentUtils::ShouldResistFingerprinting(
+             mWindow ? mWindow->GetDocShell() : nullptr, RFPTarget::PdfjsSpoof);
+}
 
 NS_IMPL_CYCLE_COLLECTING_ADDREF(nsPluginArray)
 NS_IMPL_CYCLE_COLLECTING_RELEASE(nsPluginArray)
