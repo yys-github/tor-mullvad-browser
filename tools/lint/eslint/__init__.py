a9d4babca205b547a0da1fb2f06a7a0eec90fb0d
Alex Hochheiden <ahochheiden@mozilla.com>
2025-12-10 21:43:35 +0000

    Bug 1704891 - Move checking for/setting up `node` executables to `nodeutil.py` r=Standard8,firefox-build-system-reviewers,perftest-reviewers,mozperftest-reviewers,sparky,glandium
    Differential Revision: https://phabricator.services.mozilla.com/D264253

--- tools/lint/eslint/__init__.py
+++ tools/lint/eslint/__init__.py
@@ -11,7 +11,7 @@ import subprocess
 import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "eslint"))
-from mozbuild.nodeutil import find_node_executable
+from mozbuild.nodeutil import check_node_executables_valid, find_node_executable
 from mozlint import result
 
 from eslint import prettier_utils, setup_helper
@@ -36,7 +36,7 @@ and try again.
 def setup(root, **lintargs):
     setup_helper.set_project_root(root)
 
-    if not setup_helper.check_node_executables_valid():
+    if not check_node_executables_valid():
         return 1
 
     return setup_helper.eslint_maybe_setup()
--- tools/lint/stylelint/__init__.py
+++ tools/lint/stylelint/__init__.py
@@ -13,7 +13,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "eslint"))
 from eslint import prettier_utils, setup_helper
-from mozbuild.nodeutil import find_node_executable
+from mozbuild.nodeutil import check_node_executables_valid, find_node_executable
 from mozlint import result
 
 STYLELINT_ERROR_MESSAGE = """
@@ -38,7 +38,7 @@ FILE_EXT_REGEX = re.compile(r"\.[a-z0-9_]{2,10}$", re.IGNORECASE)
 def setup(root, **lintargs):
     setup_helper.set_project_root(root)
 
-    if not setup_helper.check_node_executables_valid():
+    if not check_node_executables_valid():
         return 1
 
     return setup_helper.eslint_maybe_setup()
