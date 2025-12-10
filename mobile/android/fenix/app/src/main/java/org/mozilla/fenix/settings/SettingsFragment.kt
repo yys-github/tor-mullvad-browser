Kevinisagirl21/tor-browser

TB 40041 [android]: Implement Tor Network Settings
Originally, fenix#40041.

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -13,6 +13,7 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import androidx.annotation.VisibleForTesting
@@ -50,6 +51,7 @@ import org.mozilla.fenix.GleanMetrics.TrackingProtection
 import org.mozilla.fenix.GleanMetrics.Translations
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
+import org.mozilla.fenix.ReleaseChannel
 import org.mozilla.fenix.components.accounts.FenixFxAEntryPoint
 import org.mozilla.fenix.databinding.AmoCollectionOverrideDialogBinding
 import org.mozilla.fenix.ext.application
@@ -60,11 +62,16 @@ import org.mozilla.fenix.ext.openSetDefaultBrowserOption
 import org.mozilla.fenix.ext.requireComponents
 import org.mozilla.fenix.ext.settings
 import org.mozilla.fenix.ext.showToolbar
+import org.mozilla.fenix.gecko.GeckoProvider
 import org.mozilla.fenix.nimbus.FxNimbus
 import org.mozilla.fenix.perf.ProfilerViewModel
 import org.mozilla.fenix.settings.account.AccountUiView
+import org.mozilla.fenix.tor.QuickStartPreference
 import org.mozilla.fenix.tor.SecurityLevel
+import org.mozilla.fenix.tor.TorBridgeTransportConfig
+import org.mozilla.fenix.tor.TorEvents
 import org.mozilla.fenix.utils.Settings
+import org.mozilla.geckoview.BuildConfig
 import kotlin.system.exitProcess
 import org.mozilla.fenix.GleanMetrics.Settings as SettingsMetrics
 
@@ -167,6 +174,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        //setPreferencesFromResource(R.xml.tor_network_settings_preferences, rootKey)
+        //setupConnectionPreferences()
         setPreferencesFromResource(R.xml.preferences, rootKey)
     }
 
@@ -190,7 +199,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
         update(shouldUpdateAccountUIState = !creatingFragment)
 
         requireView().findViewById<RecyclerView>(R.id.recycler_view)
-            ?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+            .also {
+                it?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+                // Prevent disabled settings from having a collapsing animation on open
+                it?.disableHidingAnimation()
+            }
 
         args.preferenceToScrollTo?.let {
             scrollToPreference(it)
@@ -250,9 +263,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //            getString(R.string.preferences_credit_cards_2)
 //        }
 
-        val openLinksInAppsSettingsPreference =
-            requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
-        openLinksInAppsSettingsPreference.summary = context?.settings()?.getOpenLinksInAppsString()
+//        val openLinksInAppsSettingsPreference =
+//            requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
+//        openLinksInAppsSettingsPreference.summary = context?.settings()?.getOpenLinksInAppsString()
 
         // Hide "Delete browsing data on quit" when in Private Browsing-only mode
         deleteBrowsingDataPreference.isVisible =
@@ -429,9 +442,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 null
             }
 
-            resources.getString(R.string.pref_key_open_links_in_apps) -> {
-                SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
-            }
+//            resources.getString(R.string.pref_key_open_links_in_apps) -> {
+//                SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
+//            }
 
             resources.getString(R.string.pref_key_sync_debug) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToSyncDebugFragment()
@@ -560,6 +573,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setupSearchPreference()
         setupHomepagePreference()
         setupTrackingProtectionPreference()
+        setupConnectionPreferences()
     }
 
     /**
@@ -594,6 +608,10 @@ class SettingsFragment : PreferenceFragmentCompat() {
         }
     }
 
+    private fun RecyclerView.disableHidingAnimation() {
+        this.setItemAnimator(null)
+        this.setLayoutAnimation(null)
+    }
     private fun updateFxAAllowDomesticChinaServerMenu() {
         val settings = requireContext().settings()
         val preferenceAllowDomesticChinaServer =
@@ -728,6 +746,43 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 context.settings().useStrictTrackingProtection -> getString(R.string.tracking_protection_strict)
                 context.settings().useCustomTrackingProtection -> getString(R.string.tracking_protection_custom)
                 else -> null
+            }
+        }
+    }
+
+    internal fun setupConnectionPreferences() {
+        // will be needed for phase2
+        //val torController = requireContext().components.torController
+
+        requirePreference<Preference>(R.string.pref_key_tor_network_settings_bridge_config).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections
+                        .actionSettingsFragmentToTorBridgeConfigFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+
+        requirePreference<QuickStartPreference>(R.string.pref_key_quick_start).apply {
+            setOnPreferenceClickListener {
+                context.components.torController.quickstart = !context.components.torController.quickstart
+                updateSwitch()
+                true
+            }
+        }
+
+        requirePreference<Preference>(R.string.pref_key_use_html_connection_ui).apply {
+            onPreferenceChangeListener = object : SharedPreferenceUpdater() {}
+            isVisible = Config.channel != ReleaseChannel.Release
+        }
+
+        requirePreference<Preference>(R.string.pref_key_tor_logs).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections.actionSettingsFragmentToTorLogsFragment()
+                requireView().findNavController().navigate(directions)
+                true
             }
         }
     }

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/TorBridgeConfigFragment.kt
@@ -1,0 +1,155 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings
+
+import android.os.Bundle
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import org.mozilla.fenix.Config
+import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.components
+import org.mozilla.fenix.ext.settings
+import org.mozilla.fenix.ext.showToolbar
+import org.mozilla.fenix.tor.TorBridgeTransportConfig
+import org.mozilla.fenix.utils.view.addToRadioGroup
+import org.mozilla.fenix.utils.view.GroupableRadioButton
+import org.mozilla.fenix.utils.view.uncheckAll
+
+/**
+ * Displays the toggle for enabling bridges, options for built-in pluggable transports, and an additional
+ * preference for configuring a user-provided bridge.
+ */
+@Suppress("SpreadOperator")
+class TorBridgeConfigFragment : PreferenceFragmentCompat() {
+    private val builtinBridgeRadioGroups = mutableListOf<GroupableRadioButton>()
+    private var previousTransportConfig: TorBridgeTransportConfig? = null
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.tor_bridge_config_preferences, rootKey)
+
+        // Initialize radio button group for built-in bridge transport types
+        val radioObfs4 = bindBridgeTransportRadio(TorBridgeTransportConfig.BUILTIN_OBFS4)
+        val radioMeekAzure = bindBridgeTransportRadio(TorBridgeTransportConfig.BUILTIN_MEEK_AZURE)
+        val radioSnowflake = bindBridgeTransportRadio(TorBridgeTransportConfig.BUILTIN_SNOWFLAKE)
+
+        builtinBridgeRadioGroups.addAll(mutableListOf(radioObfs4, radioMeekAzure, radioSnowflake))
+
+        // `*` is Kotlin's "spread" operator, for expanding an Array as a vararg.
+        addToRadioGroup(*builtinBridgeRadioGroups.toTypedArray())
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        showToolbar(getString(R.string.preferences_tor_network_settings_bridge_config))
+
+        val bridgesEnabled = requireContext().components.torController.bridgesEnabled
+
+        val prefBridgeConfig =
+            requirePreference<SwitchPreference>(R.string.pref_key_tor_network_settings_bridge_config_toggle)
+        prefBridgeConfig.apply {
+            isChecked = bridgesEnabled
+            setOnPreferenceChangeListener<Boolean> { preference, enabled ->
+                preference.context.components.torController.bridgesEnabled = enabled
+                updateCurrentConfiguredBridgePref(preference)
+                preference.context.components.torController.restartTor()
+                true
+            }
+        }
+
+        val userProvidedBridges = requirePreference<EditTextPreference>(
+            R.string.pref_key_tor_network_settings_bridge_config_user_provided_bridge
+        )
+        userProvidedBridges.apply {
+            setOnPreferenceChangeListener<String> { preference, userProvidedBridge ->
+                builtinBridgeRadioGroups.uncheckAll()
+
+                preference.context.components.torController.bridgeTransport = TorBridgeTransportConfig.USER_PROVIDED
+                preference.context.components.torController.userProvidedBridges = userProvidedBridge
+                updateCurrentConfiguredBridgePref(preference)
+                preference.context.components.torController.restartTor()
+                true
+            }
+            val userProvidedBridge: String? = context.components.torController.userProvidedBridges
+            if (userProvidedBridge != null) {
+                setText(userProvidedBridge)
+            }
+        }
+
+        val currentBridgeType = prefBridgeConfig.context.components.torController.bridgeTransport
+        // Cache the current configured transport type
+        previousTransportConfig = currentBridgeType
+        builtinBridgeRadioGroups.uncheckAll()
+        if (currentBridgeType != TorBridgeTransportConfig.USER_PROVIDED) {
+            val bridgeRadioButton = requirePreference<RadioButtonPreference>(currentBridgeType.preferenceKey)
+            bridgeRadioButton.setCheckedWithoutClickListener(true)
+        }
+
+        updateCurrentConfiguredBridgePref(prefBridgeConfig)
+    }
+
+    private fun bindBridgeTransportRadio(
+        bridge: TorBridgeTransportConfig
+    ): RadioButtonPreference {
+        val radio = requirePreference<RadioButtonPreference>(bridge.preferenceKey)
+
+        radio.apply {
+            setOnPreferenceChangeListener<Boolean> { preference, isChecked ->
+                if (isChecked && (previousTransportConfig!! != bridge)) {
+                    preference.context.components.torController.bridgeTransport = bridge
+                    previousTransportConfig = bridge
+                    updateCurrentConfiguredBridgePref(preference)
+                    preference.context.components.torController.restartTor()
+                }
+                true
+            }
+        }
+
+        return radio
+    }
+
+    private fun setCurrentBridgeLabel(currentBridgePref: Preference?, bridge: String) {
+        currentBridgePref?.apply {
+            title = getString(
+                R
+                .string
+                .preferences_tor_network_settings_bridge_config_current_bridge,
+                bridge
+            )
+        }
+    }
+
+    private fun updateCurrentConfiguredBridgePref(preference: Preference) {
+        val currentBridge: Preference? =
+            findPreference(
+                getString(
+                    R.string.pref_key_tor_network_settings_bridge_config_current_bridge
+                )
+            )
+
+        val enabled = requireContext().components.torController.bridgesEnabled
+
+        if (enabled) {
+            val configuredBridge = preference.context.components.torController.bridgeTransport
+            var bridges = when (configuredBridge) {
+                TorBridgeTransportConfig.USER_PROVIDED ->
+                    preference.context.components.torController.userProvidedBridges
+                else -> configuredBridge.transportName
+            }
+
+            if (bridges == null) {
+                bridges = "not known"
+            }
+            setCurrentBridgeLabel(currentBridge, bridges)
+        } else {
+            setCurrentBridgeLabel(
+                currentBridge,
+                getString(R.string.tor_network_settings_bridge_not_configured)
+            )
+        }
+    }
+}

yys-github/tor-mullvad-browser

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -9,6 +9,7 @@ import android.content.DialogInterface
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.StrictMode
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -30,6 +31,7 @@ import androidx.preference.SwitchPreference
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import mozilla.components.browser.state.state.selectedOrDefaultSearchEngine
@@ -53,6 +55,7 @@ import org.mozilla.fenix.GleanMetrics.TrackingProtection
 import org.mozilla.fenix.GleanMetrics.Translations
 import org.mozilla.fenix.HomeActivity
 import org.mozilla.fenix.R
+import org.mozilla.fenix.ReleaseChannel
 import org.mozilla.fenix.components.Components
 import org.mozilla.fenix.components.accounts.FenixFxAEntryPoint
 import org.mozilla.fenix.databinding.AmoCollectionOverrideDialogBinding
@@ -72,6 +75,7 @@ import org.mozilla.fenix.settings.account.AccountUiView
 import org.mozilla.fenix.snackbar.FenixSnackbarDelegate
 import org.mozilla.fenix.snackbar.SnackbarBinding
 import org.mozilla.fenix.tor.TorSecurityLevel
+import org.mozilla.fenix.tor.QuickstartViewModel
 import org.mozilla.fenix.utils.Settings
 import kotlin.system.exitProcess
 import org.mozilla.fenix.GleanMetrics.Settings as SettingsMetrics
@@ -87,6 +91,8 @@ class SettingsFragment : PreferenceFragmentCompat() {
         ProfilerViewModelFactory(requireActivity().application)
     }
     private val snackbarBinding = ViewBoundFeatureWrapper<SnackbarBinding>()
+
+    private val quickstartViewModel: QuickstartViewModel by activityViewModels()
 
     @VisibleForTesting
     internal val accountObserver = object : AccountObserver {
@@ -180,7 +186,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        requireContext().components.strictMode.resetAfter(StrictMode.allowThreadDiskReads()) {
+            setPreferencesFromResource(R.xml.preferences, rootKey)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -244,7 +252,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
         )
 
         requireView().findViewById<RecyclerView>(R.id.recycler_view)
-            ?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+            .also {
+                it?.hideInitialScrollBar(viewLifecycleOwner.lifecycleScope)
+                // Prevent disabled settings from having a collapsing animation on open
+                it?.disableHidingAnimation()
+            }
 
         args.preferenceToScrollTo?.let {
             scrollToPreference(it)
@@ -309,9 +321,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
 //            getString(R.string.preferences_credit_cards_2)
 //        }
 
-        val openLinksInAppsSettingsPreference =
-            requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
-        openLinksInAppsSettingsPreference.summary = settings.getOpenLinksInAppsString()
+        // val openLinksInAppsSettingsPreference =
+        //     requirePreference<Preference>(R.string.pref_key_open_links_in_apps)
+        // openLinksInAppsSettingsPreference.summary = settings.getOpenLinksInAppsString()
 
         setupPreferences(settings)
 
@@ -492,9 +504,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 SettingsFragmentDirections.actionSettingsFragmentToLinkSharingFragment()
             }
 
-            resources.getString(R.string.pref_key_open_links_in_apps) -> {
-                SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
-            }
+            // resources.getString(R.string.pref_key_open_links_in_apps) -> {
+            //     SettingsFragmentDirections.actionSettingsFragmentToOpenLinksInAppsFragment()
+            // }
 
             resources.getString(R.string.pref_key_downloads) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToOpenDownloadsSettingsFragment()
@@ -620,6 +632,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setupHomepagePreference(settings)
         setupTrackingProtectionPreference(settings)
         setupDnsOverHttpsPreference(settings)
+        setupConnectionPreferences()
     }
 
     /**
@@ -652,6 +665,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
             delay(SCROLL_INDICATOR_DELAY)
             scrollBarSize = originalSize
         }
+    }
+
+    private fun RecyclerView.disableHidingAnimation() {
+        this.setItemAnimator(null)
+        this.setLayoutAnimation(null)
     }
 
     @VisibleForTesting
@@ -756,6 +774,56 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 Engine.DohSettingsMode.OFF -> getString(R.string.preference_doh_off)
                 Engine.DohSettingsMode.INCREASED -> getString(R.string.preference_doh_increased_protection)
                 Engine.DohSettingsMode.MAX -> getString(R.string.preference_doh_max_protection)
+            }
+        }
+    }
+
+    internal fun setupConnectionPreferences() {
+        // will be needed for phase2
+        //val torController = requireContext().components.torController
+
+        requirePreference<Preference>(R.string.pref_key_tor_network_settings_bridge_config).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections
+                        .actionSettingsFragmentToTorBridgeConfigFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+
+        requirePreference<SwitchPreference>(R.string.pref_key_quick_start).apply {
+            isChecked = quickstartViewModel.quickstart().value == true
+            setOnPreferenceClickListener {
+                quickstartViewModel.quickstartSet(
+                    isChecked,
+                )
+                true
+            }
+        }
+
+        requirePreference<Preference>(R.string.pref_key_use_html_connection_ui).apply {
+            onPreferenceChangeListener = object : SharedPreferenceUpdater() {}
+            isVisible = Config.channel != ReleaseChannel.Release
+        }
+
+        requirePreference<Preference>(R.string.pref_key_tor_logs).apply {
+            setOnPreferenceClickListener {
+                val directions =
+                    SettingsFragmentDirections.actionSettingsFragmentToTorLogsFragment()
+                requireView().findNavController().navigate(directions)
+                true
+            }
+        }
+        requirePreference<Preference>(R.string.pref_key_about_config_shortcut).apply {
+            isVisible = requireContext().settings().showSecretDebugMenuThisSession || Config.channel == ReleaseChannel.Debug
+            setOnPreferenceClickListener {
+                (requireActivity() as HomeActivity).openToBrowserAndLoad(
+                    searchTermOrURL = "about:config",
+                    from = BrowserDirection.FromSettings,
+                    newTab = true,
+                )
+                true
             }
         }
     }

mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/TorBridgeConfigFragment.kt
@@ -1,0 +1,152 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+package org.mozilla.fenix.settings
+
+import android.os.Bundle
+import androidx.preference.EditTextPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
+import org.mozilla.fenix.Config
+import org.mozilla.fenix.R
+import org.mozilla.fenix.ext.components
+import org.mozilla.fenix.ext.settings
+import org.mozilla.fenix.ext.showToolbar
+import org.mozilla.fenix.tor.TorBridgeTransportConfig
+import org.mozilla.fenix.utils.view.addToRadioGroup
+import org.mozilla.fenix.utils.view.GroupableRadioButton
+import org.mozilla.fenix.utils.view.uncheckAll
+
+/**
+ * Displays the toggle for enabling bridges, options for built-in pluggable transports, and an additional
+ * preference for configuring a user-provided bridge.
+ */
+@Suppress("SpreadOperator")
+class TorBridgeConfigFragment : PreferenceFragmentCompat() {
+    private val builtinBridgeRadioGroups = mutableListOf<GroupableRadioButton>()
+    private var previousTransportConfig: TorBridgeTransportConfig? = null
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.tor_bridge_config_preferences, rootKey)
+
+        // Initialize radio button group for built-in bridge transport types
+        val radioObfs4 = bindBridgeTransportRadio(TorBridgeTransportConfig.BUILTIN_OBFS4)
+        val radioMeek = bindBridgeTransportRadio(TorBridgeTransportConfig.BUILTIN_MEEK)
+        val radioSnowflake = bindBridgeTransportRadio(TorBridgeTransportConfig.BUILTIN_SNOWFLAKE)
+
+        builtinBridgeRadioGroups.addAll(mutableListOf(radioObfs4, radioMeek, radioSnowflake))
+
+        // `*` is Kotlin's "spread" operator, for expanding an Array as a vararg.
+        addToRadioGroup(*builtinBridgeRadioGroups.toTypedArray())
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        showToolbar(getString(R.string.preferences_tor_network_settings_bridge_config))
+
+        val bridgesEnabled = requireContext().components.torController.bridgesEnabled
+
+        val prefBridgeConfig =
+            requirePreference<SwitchPreference>(R.string.pref_key_tor_network_settings_bridge_config_toggle)
+        prefBridgeConfig.apply {
+            isChecked = bridgesEnabled
+            setOnPreferenceChangeListener<Boolean> { preference, enabled ->
+                preference.context.components.torController.bridgesEnabled = enabled
+                updateCurrentConfiguredBridgePref(preference)
+                true
+            }
+        }
+
+        val userProvidedBridges = requirePreference<EditTextPreference>(
+            R.string.pref_key_tor_network_settings_bridge_config_user_provided_bridge
+        )
+        userProvidedBridges.apply {
+            setOnPreferenceChangeListener<String> { preference, userProvidedBridge ->
+                builtinBridgeRadioGroups.uncheckAll()
+
+                preference.context.components.torController.bridgeTransport = TorBridgeTransportConfig.USER_PROVIDED
+                preference.context.components.torController.userProvidedBridges = userProvidedBridge
+                updateCurrentConfiguredBridgePref(preference)
+                true
+            }
+            val userProvidedBridge: String? = context.components.torController.userProvidedBridges
+            if (userProvidedBridge != null) {
+                setText(userProvidedBridge)
+            }
+        }
+
+        val currentBridgeType = prefBridgeConfig.context.components.torController.bridgeTransport
+        // Cache the current configured transport type
+        previousTransportConfig = currentBridgeType
+        builtinBridgeRadioGroups.uncheckAll()
+        if (currentBridgeType != TorBridgeTransportConfig.USER_PROVIDED) {
+            val bridgeRadioButton = requirePreference<RadioButtonPreference>(currentBridgeType.preferenceKey)
+            bridgeRadioButton.setCheckedWithoutClickListener(true)
+        }
+
+        updateCurrentConfiguredBridgePref(prefBridgeConfig)
+    }
+
+    private fun bindBridgeTransportRadio(
+        bridge: TorBridgeTransportConfig
+    ): RadioButtonPreference {
+        val radio = requirePreference<RadioButtonPreference>(bridge.preferenceKey)
+
+        radio.apply {
+            setOnPreferenceChangeListener<Boolean> { preference, isChecked ->
+                if (isChecked && (previousTransportConfig!! != bridge)) {
+                    preference.context.components.torController.bridgeTransport = bridge
+                    previousTransportConfig = bridge
+                    updateCurrentConfiguredBridgePref(preference)
+                }
+                true
+            }
+        }
+
+        return radio
+    }
+
+    private fun setCurrentBridgeLabel(currentBridgePref: Preference?, bridge: String) {
+        currentBridgePref?.apply {
+            title = getString(
+                R
+                .string
+                .preferences_tor_network_settings_bridge_config_current_bridge,
+                bridge
+            )
+        }
+    }
+
+    private fun updateCurrentConfiguredBridgePref(preference: Preference) {
+        val currentBridge: Preference? =
+            findPreference(
+                getString(
+                    R.string.pref_key_tor_network_settings_bridge_config_current_bridge
+                )
+            )
+
+        val enabled = requireContext().components.torController.bridgesEnabled
+
+        if (enabled) {
+            val configuredBridge = preference.context.components.torController.bridgeTransport
+            var bridges = when (configuredBridge) {
+                TorBridgeTransportConfig.USER_PROVIDED ->
+                    preference.context.components.torController.userProvidedBridges
+                else -> configuredBridge.transportName
+            }
+
+            if (bridges == null) {
+                bridges = "not known"
+            }
+            setCurrentBridgeLabel(currentBridge, bridges)
+        } else {
+            setCurrentBridgeLabel(
+                currentBridge,
+                getString(R.string.tor_network_settings_bridge_not_configured)
+            )
+        }
+    }
+}