baab94df3467a9cf9f132a3047e8c1ddfc4cdc15
Henry Wilkes <henry@torproject.org>
2026-01-19 15:02:14 +0000

    dropme! TB 43901: Modify about:license for Tor Browser.
    MB 489: Revert the TB patch entirely by reverting
    f53f82ba5eaa6b4a5b011608e81c7b3887873c3b and
    66d6a1e876bd80274edb7b0225dc81dfbb39bc30.

--- browser/components/about/AboutRedirector.cpp
+++ browser/components/about/AboutRedirector.cpp
@@ -111,7 +111,9 @@ static const RedirEntry kRedirMap[] = {
     {"profiling",
      "chrome://devtools/content/performance-new/aboutprofiling/index.html",
      nsIAboutModule::ALLOW_SCRIPT | nsIAboutModule::IS_SECURE_CHROME_UI},
-// Drop about:rights. tor-browser#43901.
+    {"rights", "https://www.mozilla.org/about/legal/terms/firefox/",
+     nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
+         nsIAboutModule::URI_MUST_LOAD_IN_CHILD},
 #ifndef BASE_BROWSER_VERSION
     {"robots", "chrome://browser/content/aboutRobots.xhtml",
      nsIAboutModule::URI_SAFE_FOR_UNTRUSTED_CONTENT |
