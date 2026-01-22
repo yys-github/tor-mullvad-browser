28e175a53746a954e1d0822bd9b67d7a1580fe07
hackademix <giorgio@maone.net>
2026-01-22 23:06:51 +0100

    fixup! [android] Modify add-on support
    TB 44533: Install bundled NoScript extension at least once per build install

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -2491,14 +2491,9 @@ class Settings(
         default = false,
     )
 
-    var noscriptInstalled by booleanPreference(
-        appContext.getPreferenceKey(R.string.pref_key_noscript_installed),
-        default = false
-    )
-
-    var noscriptUpdated by intPreference(
-        appContext.getPreferenceKey(R.string.pref_key_noscript_updated),
-        default = 0
+    var extensionsVersion by stringPreference(
+        appContext.getPreferenceKey(R.string.pref_key_extensions_version),
+        default = ""
     )
 
     var httpsEverywhereRemoved by booleanPreference(
