e121af1cec8c7ad6c316bea9d500e699312871a7
Benjamin VanderSloot <bvandersloot@mozilla.com>
2026-01-22 17:03:41 +0000

    Bug 2010501 - DNS drop down under privacy and security is broken after 147 - r=valentin,tgiles
    Differential Revision: https://phabricator.services.mozilla.com/D279885

--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -3947,21 +3947,6 @@ var gPrivacyPane = {
 
     setSyncFromPrefListener("savePasswords", () => this.readSavePasswords());
 
-    if (AlertsServiceDND) {
-      let notificationsDoNotDisturbBox = document.getElementById(
-        "notificationsDoNotDisturbBox"
-      );
-      notificationsDoNotDisturbBox.removeAttribute("hidden");
-      let checkbox = document.getElementById("notificationsDoNotDisturb");
-      document.l10n.setAttributes(checkbox, "permissions-notification-pause");
-      if (AlertsServiceDND.manualDoNotDisturb) {
-        let notificationsDoNotDisturb = document.getElementById(
-          "notificationsDoNotDisturb"
-        );
-        notificationsDoNotDisturb.toggleAttribute("checked", true);
-      }
-    }
-
     this.initSiteDataControls();
 
     this.initCookieBannerHandling();
