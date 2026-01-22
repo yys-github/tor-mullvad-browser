6604aa1d4be22546f431ede9a2617bb417895a54
clairehurst <clairehurst@torproject.org>
2026-01-21 18:08:45 -0700

    TB 44581 [android]: Cleanup new android compose tab-strip feature

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/MoreSettingsSubmenu.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/MoreSettingsSubmenu.kt
@@ -57,39 +57,6 @@ internal fun MoreSettingsSubmenu(
     Column(
         verticalArrangement = Arrangement.spacedBy(2.dp),
     ) {
-        TranslationSection(
-            translationInfo = translationInfo,
-            isReaderViewActive = isReaderViewActive,
-        )
-        SummarizationMenuItem(
-            summarizationMenuState = summarizationMenuState,
-            onSummarizePageMenuExposed = onSummarizePageMenuExposed,
-            onSummarizePageClick = onSummarizePageClick,
-        )
-        WebCompatReporterMenuItem(
-            isWebCompatReporterSupported = isWebCompatReporterSupported,
-            isWebCompatEnabled = isWebCompatEnabled,
-            onWebCompatReporterClick = onWebCompatReporterClick,
-        )
-        ShortcutsSection(
-            showShortcuts = showShortcuts,
-            isPinned = isPinned,
-            onShortcutsMenuClick = onShortcutsMenuClick,
-        )
-        AddToHomeScreenMenuItem(
-            isAddToHomeScreenSupported = isAddToHomeScreenSupported,
-            isInstallable = isInstallable,
-            onAddToHomeScreenMenuClick = onAddToHomeScreenMenuClick,
-        )
-        SaveToCollectionMenuItem(
-            onSaveToCollectionMenuClick = onSaveToCollectionMenuClick,
-        )
-        OpenInAppMenuItem(
-            hasExternalApp = hasExternalApp,
-            externalAppName = externalAppName,
-            isOpenInAppMenuHighlighted = isOpenInAppMenuHighlighted,
-            onOpenInAppMenuClick = onOpenInAppMenuClick,
-        )
         SaveAsPdfMenuItem(
             onSaveAsPDFMenuClick = onSaveAsPDFMenuClick,
         )
