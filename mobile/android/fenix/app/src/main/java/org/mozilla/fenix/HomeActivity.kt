38c5573e9be1d441663d832851b3339ca12e49b1
clairehurst <clairehurst@torproject.org>
2026-01-21 18:08:45 -0700

    TB 44581: [android] Cleanup new android compose tab-strip feature

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -622,7 +622,6 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, Crash
             ),
             downloadSnackbar,
             privateBrowsingLockFeature,
-            externalAppLinkStatusBinding,
             summarizeToolbarHighlightBinding,
             components.core.summarizationSettings,
             translationsAIControllableFeatureRegistrar,
