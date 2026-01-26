b2b1dc65c94354e43b6856aefcd39415df515dd5
Henry Wilkes <henry@torproject.org>
2026-01-26 13:11:25 +0000

    fixup! TB 44711: Hide unwanted setting controls in Tor Browser.
    TB 41822: Hide the default browser settings.
    
    As per https://gitlab.torproject.org/tpo/applications/tor-browser/-/work_items/44869#note_3416757.

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -504,6 +504,10 @@ const DefaultBrowserHelper = {
    * @type {boolean}
    */
   get canCheck() {
+    if (AppConstants.BASE_BROWSER_VERSION) {
+      // Disabled for Tor Browser. tor-browser#44343 and tor-browser#41822.
+      return false;
+    }
     return (
       this.shellSvc &&
       /**
