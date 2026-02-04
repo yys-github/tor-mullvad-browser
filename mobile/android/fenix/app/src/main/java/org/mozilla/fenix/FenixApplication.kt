9dc8507fb32e04eb8283f47f8ec958319d5eab81
hackademix <giorgio@maone.net>
2026-02-04 08:20:12 +0100

    TB 44591: Force extensions initialization on startup.

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/FenixApplication.kt
@@ -31,6 +31,7 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import mozilla.appservices.autofill.AutofillApiException
+import mozilla.components.browser.engine.gecko.GeckoEngine
 import mozilla.components.browser.state.action.SystemAction
 import mozilla.components.browser.state.selector.selectedTab
 import mozilla.components.browser.state.store.BrowserStore
@@ -702,6 +703,20 @@ open class FenixApplication : LocaleAwareApplication(), Provider {
                     components.useCases.tabsUseCases.selectTab(sessionId)
                 },
                 onExtensionsLoaded = { extensions ->
+
+                    // temp fix for tb#44591 "fails to initialize WebExtensions"
+                    // disable and enable each extension to properly initialize them
+                    extensions.forEach { extension ->
+                        if (extension.isEnabled()) {
+                          (components.core.engine as GeckoEngine).disableWebExtension(
+                              extension,
+                              onSuccess = {
+                                  (components.core.engine as GeckoEngine).enableWebExtension(extension)
+                              },
+                          )
+                        }
+                    }
+
                     // Delay until bootstrap is finished so that it will actually update tor-browser#44303
                     components.torController.registerRunOnceBootstrapped(object : RunOnceBootstrapped {
                         override fun onBootstrapped() {
