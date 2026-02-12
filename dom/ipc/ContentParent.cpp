660b0a6abd16c26f47cd2f34da8e4f8c2b792cd8
Gela <gmalekpour@mozilla.com>
2026-02-12 16:15:51 +0000

    Bug 2008365 - Update Content Parent  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D282863
    
    Differential Revision: https://phabricator.services.mozilla.com/D282933

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -3501,7 +3501,9 @@ mozilla::ipc::IPCResult ContentParent::RecvGetIconForExtension(
     return IPC_OK();
   }
 
-  bits->AppendElements(aIconSize * aIconSize * 4);
+  // Explicitly zero-initialize the output to ensure we do not send
+  // uninitialized heap memory over IPC to the child process to avoid heap data leakage.
+  bits->InsertElementsAt(0, aIconSize * aIconSize * 4, 0);
 
   AndroidBridge::Bridge()->GetIconForExtension(aFileExt, aIconSize,
                                                bits->Elements());
