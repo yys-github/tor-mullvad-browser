fbf2f2bf75df16ce31399d2a6ae69fc0f41c2a4e
Henry Wilkes <henry@torproject.org>
2026-03-02 10:22:12 +0000

    BB 44711: Hide unwanted setting controls in Base Browser.

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -246,6 +246,7 @@ const CONFIG_PANES = Object.freeze({
     visible: () => srdSectionPrefs.all,
   },
   ai: {
+    skip: true, // Skip AI pane. tor-browser#44709.
     l10nId: "preferences-ai-controls-header2",
     iconSrc: "chrome://global/skin/icons/highlights.svg",
     groupIds: ["aiControlsDescription", "aiFeatures", "aiStatesDescription"],
@@ -280,6 +281,7 @@ const CONFIG_PANES = Object.freeze({
     replaces: "privacy",
   },
   etp: {
+    skip: true, // Skip enhanced tracking protection. tor-browser#33848.
     parent: "privacy",
     l10nId: "preferences-etp-header",
     groupIds: ["etpBanner", "etpAdvanced"],
@@ -328,6 +330,7 @@ const CONFIG_PANES = Object.freeze({
     visible: () => srdSectionEnabled("languages"),
   },
   manageAddresses: {
+    skip: true,
     parent: "passwordsAutofill",
     l10nId: "autofill-addresses-manage-addresses-title",
     groupIds: ["manageAddresses"],
@@ -343,6 +346,7 @@ const CONFIG_PANES = Object.freeze({
     supportPage: "smart-window-memories",
   },
   managePayments: {
+    skip: true,
     parent: "passwordsAutofill",
     l10nId: "autofill-payment-methods-manage-payments-title",
     groupIds: ["managePayments"],
@@ -443,6 +447,7 @@ const CONFIG_PANES = Object.freeze({
     visible: () => srdSectionEnabled("tabsBrowsing"),
   },
   translations: {
+    skip: true, // Skip translations. tor-browser#44710.
     parent: srdSectionEnabled("languages") ? "languages" : "general",
     l10nId: "settings-translations-subpage-header",
     groupIds: [
@@ -542,10 +547,22 @@ function init_all() {
   }
   register_module("paneSearchResults", gSearchResultsPane);
   for (let [id, config] of Object.entries(CONFIG_PANES)) {
-    if (!redesignEnabled && config.replaces) {
+    // Skip over configs we do not want, including all its children.
+    // See tor-browser#44711.
+    let skip = false;
+    let parentConfig = config;
+    while (parentConfig) {
+      skip = parentConfig.skip;
+      if (skip) {
+        break;
+      }
+      parentConfig = parentConfig.parent
+        ? CONFIG_PANES[parentConfig.parent]
+        : undefined;
+    }
+    if ((!redesignEnabled && config.replaces) || skip) {
       continue;
     }
-
     SettingPaneManager.registerPane(id, config);
   }
 
