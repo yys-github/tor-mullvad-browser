a36d1ed225b13cfda4d59cea906613f7d88abcbe
Henry Wilkes <henry@torproject.org>
2026-03-02 10:22:12 +0000

    BB 44711: Hide unwanted setting controls in Base Browser.

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -240,6 +240,7 @@ const CONFIG_PANES = Object.freeze({
     groupIds: ["dnsOverHttpsAdvanced"],
   },
   managePayments: {
+    skip: true,
     parent: "privacy",
     l10nId: "autofill-payment-methods-manage-payments-title",
     groupIds: ["managePayments"],
@@ -251,6 +252,7 @@ const CONFIG_PANES = Object.freeze({
     groupIds: ["profilePane"],
   },
   etp: {
+    skip: true, // Skip enhanced tracking protection. tor-browser#33848.
     parent: "privacy",
     l10nId: "preferences-etp-header",
     groupIds: ["etpBanner", "etpAdvanced"],
@@ -261,12 +263,14 @@ const CONFIG_PANES = Object.freeze({
     groupIds: ["etpCustomize", "etpReset"],
   },
   manageAddresses: {
+    skip: true,
     parent: "privacy",
     l10nId: "autofill-addresses-manage-addresses-title",
     groupIds: ["manageAddresses"],
     iconSrc: "chrome://browser/skin/notification-icons/geo.svg",
   },
   translations: {
+    skip: true, // Skip translations. tor-browser#44710.
     parent: "general",
     l10nId: "settings-translations-subpage-header",
     groupIds: [
@@ -276,6 +280,7 @@ const CONFIG_PANES = Object.freeze({
     iconSrc: "chrome://browser/skin/translations.svg",
   },
   ai: {
+    skip: true, // Skip AI pane. tor-browser#44709.
     l10nId: "preferences-ai-controls-header",
     iconSrc: "chrome://global/skin/icons/highlights.svg",
     groupIds: ["aiControlsDescription", "aiFeatures", "aiStatesDescription"],
@@ -362,6 +367,22 @@ function init_all() {
   register_module("paneContainers", gContainersPane);
 
   for (let [id, config] of Object.entries(CONFIG_PANES)) {
+    // Skip over configs we do not want, including all its children.
+    // See tor-browser#44711.
+    let skip = false;
+    let parentConfig = config;
+    while (parentConfig) {
+      skip = parentConfig.skip;
+      if (skip) {
+        break;
+      }
+      parentConfig = parentConfig.parent
+        ? CONFIG_PANES[parentConfig.parent]
+        : undefined;
+    }
+    if (skip) {
+      continue;
+    }
     SettingPaneManager.registerPane(id, config);
   }
 
