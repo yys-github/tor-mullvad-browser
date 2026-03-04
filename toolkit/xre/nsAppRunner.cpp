04a2b859fc1583e703108f61edf447de736bbf89
Pier Angelo Vendrame <pierov@torproject.org>
2026-03-04 11:20:30 +0100

    fixup! TB 43107: Disable remoting by default
    TB 44706: Fix a crash when creating new profiles.
    
    Rework the patch to disable remoting at runtime to allow using some of
    the service's functionalities, while still refusing to create any
    remoting server/client.

--- toolkit/xre/nsAppRunner.cpp
+++ toolkit/xre/nsAppRunner.cpp
@@ -314,7 +314,7 @@ static nsIProfileLock* gProfileLock;
 constinit static RefPtr<nsRemoteService> gRemoteService;
 constinit static RefPtr<nsStartupLock> gStartupLock;
 // tor-browser#43107: Disable remoting by default.
-bool gDisableRemoting = true;
+bool gEnableRemoting = false;
 #endif
 
 int gRestartArgc;
@@ -2095,8 +2095,8 @@ nsresult ScopedXPCOMStartup::SetWindowCreator(nsINativeAppSupport* native) {
 /* static */ already_AddRefed<nsIRemoteService> GetRemoteService() {
   AssertIsOnMainThread();
 
-  if (!gRemoteService && !gDisableRemoting) {
-    gRemoteService = new nsRemoteService();
+  if (!gRemoteService) {
+    gRemoteService = new nsRemoteService(gEnableRemoting);
   }
   nsCOMPtr<nsIRemoteService> remoteService = gRemoteService.get();
   return remoteService.forget();
@@ -4487,7 +4487,7 @@ int XREMain::XRE_mainInit(bool* aExitFlag) {
   // The user can still enable remoting if they want to, by adding the
   // allow-remote parameter to the command line.
   if (CheckArg("allow-remote") == ARG_FOUND) {
-    gDisableRemoting = false;
+    gEnableRemoting = true;
   }
 #else
   // These arguments do nothing in platforms with no remoting support but we
@@ -4882,7 +4882,7 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
 
 #ifdef MOZ_HAS_REMOTE
   if (gfxPlatform::IsHeadless()) {
-    gDisableRemoting = true;
+    gEnableRemoting = false;
   }
 #endif
 
@@ -5004,10 +5004,8 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
   }
 #endif
 #if defined(MOZ_HAS_REMOTE)
-  if (!gDisableRemoting) {
-    // handle --remote now that xpcom is fired up
-    gRemoteService = new nsRemoteService();
-  }
+  // handle --remote now that xpcom is fired up
+  gRemoteService = new nsRemoteService(gEnableRemoting);
   if (gRemoteService) {
     gRemoteService->SetProgram(gAppData->remotingName);
     gStartupLock = gRemoteService->LockStartup();
@@ -5092,7 +5090,7 @@ int XREMain::XRE_mainStartup(bool* aExitFlag) {
     if (NS_SUCCEEDED(rv)) {
       gRemoteService->SetProfile(profilePath);
 
-      if (!gDisableRemoting) {
+      if (gEnableRemoting) {
         // Try to remote the entire command line. If this fails, start up
         // normally.
 #  ifdef MOZ_WIDGET_GTK
