5349a0bbd85899a63cce6661d38b40534520f56d
Pier Angelo Vendrame <pierov@torproject.org>
2026-03-11 15:54:26 +0100

    fixup! TB 41736: Customize toolbar for tor-browser.
    TB 44720: Move new identity to Tor Browser.

--- browser/components/customizableui/CustomizableUI.sys.mjs
+++ browser/components/customizableui/CustomizableUI.sys.mjs
@@ -361,6 +361,7 @@ var CustomizableUIInternal = {
       // Or if the change is only meant for tor-browser, bump kVersionTorBrowser
       // instead and update the existing saved states in _updateForTorBrowser.
       "security-level-button",
+      "new-identity-button",
       "downloads-button",
       AppConstants.MOZ_DEV_EDITION ? "developer-button" : null,
       "fxa-toolbar-menu-button",
