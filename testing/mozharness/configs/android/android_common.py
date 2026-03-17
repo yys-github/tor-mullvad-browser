891927e531630ea2fd27503fd19768732d73af8a
Beatriz Rizental <bea@torproject.org>
2026-03-17 18:47:35 -0300

    BB 43243: Modify mozharness scripts for Base Browser

--- testing/mozharness/configs/android/android_common.py
+++ testing/mozharness/configs/android/android_common.py
@@ -310,6 +310,19 @@ config = {
                 "--deviceSerial=%(device_serial)s",
             ],
         },
+        "marionette": {
+            "run_filename": "runtests.py",
+            "testsdir": "marionette/harness/marionette_harness",
+            "install": True,
+            "options": [
+                "-vv",
+                "--address=127.0.0.1:2828",
+                "--app=fennec",
+            ],
+            "tests": [
+                "%(abs_marionette_manifest_dir)s/unit-tests.toml",
+            ],
+        },
     },  # end suite_definitions
     "unstructured_suites": [
         "jittest",
