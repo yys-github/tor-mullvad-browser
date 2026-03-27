50475e38591b3d4056ebae712c88b1878031dd14
Tom Schuster <tschuster@mozilla.com>
2026-03-27 16:20:29 +0000

    Bug 2025281 - Cleanup MediaIPCUtils.  a=diannaS
    Original Revision: https://phabricator.services.mozilla.com/D289190
    
    Differential Revision: https://phabricator.services.mozilla.com/D290479

--- dom/media/ipc/MediaIPCUtils.h
+++ dom/media/ipc/MediaIPCUtils.h
@@ -42,32 +42,26 @@ struct ParamTraits<mozilla::VideoInfo> {
     WriteParam(aWriter, aParam.mColorPrimaries);
     WriteParam(aWriter, aParam.mTransferFunction);
     WriteParam(aWriter, aParam.mColorRange);
-    WriteParam(aWriter, aParam.HasAlpha());
+    WriteParam(aWriter, aParam.mAlphaPresent);
     WriteParam(aWriter, aParam.mCrypto);
   }
 
   static bool Read(MessageReader* aReader, paramType* aResult) {
-    mozilla::gfx::IntRect imageRect;
-    bool alphaPresent;
-    if (ReadParam(aReader, &aResult->mMimeType) &&
-        ReadParam(aReader, &aResult->mDisplay) &&
-        ReadParam(aReader, &aResult->mStereoMode) &&
-        ReadParam(aReader, &aResult->mImage) &&
-        ReadParam(aReader, &aResult->mImageRect) &&
-        ReadParam(aReader, aResult->mCodecSpecificConfig.get()) &&
-        ReadParam(aReader, aResult->mExtraData.get()) &&
-        ReadParam(aReader, &aResult->mRotation) &&
-        ReadParam(aReader, &aResult->mColorDepth) &&
-        ReadParam(aReader, &aResult->mColorSpace) &&
-        ReadParam(aReader, &aResult->mColorPrimaries) &&
-        ReadParam(aReader, &aResult->mTransferFunction) &&
-        ReadParam(aReader, &aResult->mColorRange) &&
-        ReadParam(aReader, &alphaPresent) &&
-        ReadParam(aReader, &aResult->mCrypto)) {
-      aResult->SetAlpha(alphaPresent);
-      return true;
-    }
-    return false;
+    return ReadParam(aReader, &aResult->mMimeType) &&
+           ReadParam(aReader, &aResult->mDisplay) &&
+           ReadParam(aReader, &aResult->mStereoMode) &&
+           ReadParam(aReader, &aResult->mImage) &&
+           ReadParam(aReader, &aResult->mImageRect) &&
+           ReadParam(aReader, aResult->mCodecSpecificConfig.get()) &&
+           ReadParam(aReader, aResult->mExtraData.get()) &&
+           ReadParam(aReader, &aResult->mRotation) &&
+           ReadParam(aReader, &aResult->mColorDepth) &&
+           ReadParam(aReader, &aResult->mColorSpace) &&
+           ReadParam(aReader, &aResult->mColorPrimaries) &&
+           ReadParam(aReader, &aResult->mTransferFunction) &&
+           ReadParam(aReader, &aResult->mColorRange) &&
+           ReadParam(aReader, &aResult->mAlphaPresent) &&
+           ReadParam(aReader, &aResult->mCrypto);
   }
 };
 
@@ -236,6 +230,10 @@ struct ParamTraits<mozilla::media::TimeUnit> {
     if (ReadParam(aReader, &valid) && ReadParam(aReader, &ticks) &&
         ReadParam(aReader, &base)) {
       if (valid) {
+        if (base <= 0 || base > int64_t(UINT32_MAX)) {
+          return false;
+        }
+
         *aResult = mozilla::media::TimeUnit(ticks, base);
       } else {
         *aResult = mozilla::media::TimeUnit::Invalid();
@@ -320,22 +318,24 @@ struct ParamTraits<mozilla::DecoderDoctorDiagnostics::DiagnosticsType>
 };
 
 template <>
+struct ParamTraits<mozilla::DecoderDoctorEvent::Domain>
+    : public ContiguousEnumSerializerInclusive<
+          mozilla::DecoderDoctorEvent::Domain,
+          mozilla::DecoderDoctorEvent::Domain::eAudioSinkStartup,
+          mozilla::DecoderDoctorEvent::Domain::eAudioSinkStartup> {};
+
+template <>
 struct ParamTraits<mozilla::DecoderDoctorEvent> {
   typedef mozilla::DecoderDoctorEvent paramType;
 
   static void Write(MessageWriter* aWriter, const paramType& aParam) {
-    int domain = aParam.mDomain;
-    WriteParam(aWriter, domain);
+    WriteParam(aWriter, aParam.mDomain);
     WriteParam(aWriter, aParam.mResult);
   }
 
   static bool Read(MessageReader* aReader, paramType* aResult) {
-    int domain = 0;
-    if (ReadParam(aReader, &domain) && ReadParam(aReader, &aResult->mResult)) {
-      aResult->mDomain = paramType::Domain(domain);
-      return true;
-    }
-    return false;
+    return ReadParam(aReader, &aResult->mDomain) &&
+           ReadParam(aReader, &aResult->mResult);
   };
 };
 
