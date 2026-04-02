780ac117ceeb5fa12d345bc35cfa69a4bd0b0aff
Henry Wilkes <henry@torproject.org>
2026-04-02 20:14:30 +0100

    fixup! BB 42070: Hide "Use smooth scrolling" from settings
    BB 44630: Stop commenting out setting controls.

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -2984,10 +2984,10 @@ SettingGroupManager.registerGroups({
         id: "useAutoScroll",
         l10nId: "browsing-use-autoscroll",
       },
-      // {
-      //   id: "useSmoothScrolling",
-      //   l10nId: "browsing-use-smooth-scrolling",
-      // },
+      {
+        id: "useSmoothScrolling",
+        l10nId: "browsing-use-smooth-scrolling",
+      },
       {
         id: "useOverlayScrollbars",
         l10nId: "browsing-gtk-use-non-overlay-scrollbars",
