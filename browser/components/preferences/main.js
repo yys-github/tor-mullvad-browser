9f6f8849a1ca2cb767d7749968f65e877d9c96de
Henry Wilkes <henry@torproject.org>
2026-04-02 20:16:19 +0100

    fixup! BB 43117: Hide "Always underline links" from settings.
    BB 44630: Stop commenting out setting controls.

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -3000,10 +3000,10 @@ SettingGroupManager.registerGroups({
         id: "useFullKeyboardNavigation",
         l10nId: "browsing-use-full-keyboard-navigation",
       },
-      // {
-      //   id: "alwaysUnderlineLinks",
-      //   l10nId: "browsing-always-underline-links",
-      // },
+      {
+        id: "alwaysUnderlineLinks",
+        l10nId: "browsing-always-underline-links",
+      },
       {
         id: "searchStartTyping",
         l10nId: "browsing-search-on-start-typing",
