1702fd365d7494bcf73fb9bd9924000fc178a1e8
Chris Martin <marti4d@live.ca>
2026-04-02 12:44:27 +0000

    Bug 2024240 - Clean up LinuxGamepadService lifecycle interactions with event dispatching.  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D290238
    
    Differential Revision: https://phabricator.services.mozilla.com/D291596

--- dom/gamepad/linux/LinuxGamepad.cpp
+++ dom/gamepad/linux/LinuxGamepad.cpp
@@ -118,7 +118,8 @@ class LinuxGamepadService {
 };
 
 // singleton instance
-LinuxGamepadService* gService = nullptr;
+LinuxGamepadService* gService MOZ_GUARDED_BY(mozilla::sMainThreadCapability) =
+    nullptr;
 
 void LinuxGamepadService::AddDevice(struct udev_device* dev) {
   RefPtr<GamepadPlatformService> service =
@@ -501,7 +502,9 @@ gboolean LinuxGamepadService::OnGamepadData(GIOChannel* source,
 gboolean LinuxGamepadService::OnUdevMonitor(GIOChannel* source,
                                             GIOCondition condition,
                                             gpointer data) {
-  if (condition & (G_IO_ERR | G_IO_HUP)) {
+  mozilla::AssertIsOnMainThread();
+
+  if (!gService || condition & (G_IO_ERR | G_IO_HUP)) {
     return FALSE;
   }
 
@@ -514,20 +517,28 @@ gboolean LinuxGamepadService::OnUdevMonitor(GIOChannel* source,
 namespace mozilla::dom {
 
 void StartGamepadMonitoring() {
-  if (gService) {
-    return;
-  }
-  gService = new LinuxGamepadService();
-  gService->Startup();
+  MOZ_ASSERT(!NS_IsMainThread());
+
+  NS_DispatchToMainThread(NS_NewRunnableFunction("StartGamepadMonitoring", [] {
+    AssertIsOnMainThread();
+    if (!gService) {
+      gService = new LinuxGamepadService();
+      gService->Startup();
+    }
+  }));
 }
 
 void StopGamepadMonitoring() {
-  if (!gService) {
-    return;
-  }
-  gService->Shutdown();
-  delete gService;
-  gService = nullptr;
+  MOZ_ASSERT(!NS_IsMainThread());
+
+  NS_DispatchToMainThread(NS_NewRunnableFunction("StopGamepadMonitoring", [] {
+    AssertIsOnMainThread();
+    if (gService) {
+      gService->Shutdown();
+      delete gService;
+      gService = nullptr;
+    }
+  }));
 }
 
 void SetGamepadLightIndicatorColor(const Tainted<GamepadHandle>& aGamepadHandle,
