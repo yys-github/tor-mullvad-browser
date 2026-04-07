70e44047a8eb47ab245afd2576eb267e0a79bf3a
Pier Angelo Vendrame <pierov@torproject.org>
2026-04-07 15:48:33 +0200

    fixup! TB 40458: Implement .tor.onion aliases
    BB 43824: Move from resources:// to moz-src:///.

--- browser/components/DesktopActorRegistry.sys.mjs
+++ browser/components/DesktopActorRegistry.sys.mjs
@@ -728,10 +728,12 @@ let JSWINDOWACTORS = {
 
   Rulesets: {
     parent: {
-      esModuleURI: "moz-src:///browser/components/rulesets/RulesetsParent.sys.mjs",
+      esModuleURI:
+        "moz-src:///browser/components/rulesets/RulesetsParent.sys.mjs",
     },
     child: {
-      esModuleURI: "moz-src:///browser/components/rulesets/RulesetsChild.sys.mjs",
+      esModuleURI:
+        "moz-src:///browser/components/rulesets/RulesetsChild.sys.mjs",
       events: {
         DOMWindowCreated: {},
       },
