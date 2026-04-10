be53af573e2ba901a744dabe71c101dd19206122
clairehurst <clairehurst@torproject.org>
2026-04-09 19:13:20 -0600

    TB 44615: [android] Remove review prompt

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Components.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Components.kt
@@ -109,6 +109,12 @@ import org.mozilla.fenix.utils.isLargeScreenSize
 import org.mozilla.fenix.wifi.WifiConnectionMonitor
 import java.util.concurrent.TimeUnit
 
+import android.app.Activity
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.android.play.core.review.ReviewInfo
+import com.google.android.play.core.review.ReviewManager
+
 private const val AMO_COLLECTION_MAX_CACHE_AGE = 2 * 24 * 60L // Two days in minutes
 
 /**
@@ -273,7 +279,7 @@ class Components(private val context: Context) {
 
     val playStoreReviewPromptController by lazyMonitored {
         PlayStoreReviewPromptController(
-            manager = ReviewManagerFactory.create(context),
+            manager = NoopReviewManager(context),
             numberOfAppLaunches = { settings.numberOfAppLaunches },
         )
     }
@@ -517,6 +523,19 @@ class Components(private val context: Context) {
             settings = settings,
             context = context,
         )
+    }
+}
+
+class NoopReviewManager(val value: Context) : ReviewManager {
+    override fun launchReviewFlow(
+        p0: Activity,
+        p1: ReviewInfo
+    ): Task<Void?> {
+        return Tasks.forResult(null)
+    }
+
+    override fun requestReviewFlow(): Task<ReviewInfo?> {
+        return Tasks.forResult(null)
     }
 }
 
--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/settings/SettingsFragment.kt
@@ -549,12 +549,6 @@ class SettingsFragment : PreferenceFragmentCompat(), SystemInsetsPaddedFragment 
                 SettingsFragmentDirections.actionSettingsFragmentToSyncDebugFragment()
             }
 
-            // About preferences
-            resources.getString(R.string.pref_key_rate) -> {
-                components.playStoreReviewPromptController.tryLaunchPlayStoreReview(requireActivity(), ::openInNewTab)
-                null
-            }
-
             resources.getString(R.string.pref_key_about) -> {
                 SettingsFragmentDirections.actionSettingsFragmentToAboutFragment()
             }
