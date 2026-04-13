b04deb0c982385a9f213cf7aaff60c24d886e4da
Henry Wilkes <henry@torproject.org>
2026-04-13 15:47:42 +0100

    fixup! BB 32308: Use direct browser sizing for letterboxing.
    BB 44685: Use border radius for letterboxing. This allows us to drop
    browserDecorator.
    
    BB 44847: Drop the custom sidebar border radius logic since upstream now
    permanently uses a rounded corner for the browserContainer element when
    the sidebar is shown. We move the letterboxing background styling to
    browserContainer as well, so it does not leak out the corners.

--- browser/components/tabbrowser/content/tabbrowser.js
+++ browser/components/tabbrowser/content/tabbrowser.js
@@ -2807,10 +2807,6 @@
       stack.className = "browserStack";
       stack.appendChild(b);
 
-      let decorator = document.createXULElement("hbox");
-      decorator.className = "browserDecorator";
-      stack.appendChild(decorator);
-
       let browserContainer = document.createXULElement("vbox");
       browserContainer.className = "browserContainer";
       browserContainer.appendChild(stack);
