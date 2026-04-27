3cc7cb4c06d69266f22a691593051c7b53c415a4
Henry Wilkes <henry@torproject.org>
2026-04-27 15:50:06 +0100

    fixup! TB 30237: Add v3 onion services client authentication prompt
    TB 44904: Use setting config for onion site settings.

--- browser/components/preferences/config/privacy.mjs
+++ browser/components/preferences/config/privacy.mjs
@@ -2109,6 +2109,18 @@ SettingGroupManager.registerGroups({
       },
     ],
   },
+  onionSiteAuthentication: {
+    l10nId: "onion-site-authentication-group",
+    headingLevel: 2,
+    supportPage: "tor-manual:onion-services_onion-service-authentication",
+    items: [
+      {
+        id: "onionSiteSavedKeys",
+        control: "moz-box-button",
+        l10nId: "onion-site-authentication-saved-keys-button",
+      },
+    ],
+  },
   // Bug 1968111: move this elsewhere
   managePayments: {
     // Hide the payments settings. tor-browser#44460.
@@ -2293,6 +2305,16 @@ Preferences.addSetting({
   id: "breachAlerts",
   pref: "signon.management.page.breach-alerts.enabled",
 });
+
+Preferences.addSetting({
+  id: "onionSiteSavedKeys",
+  onUserClick: () => {
+    gSubDialog.open(
+      "chrome://browser/content/onionservices/savedKeysDialog.xhtml"
+    );
+  },
+});
+
 /**
  * This class is used to create Settings that are used to warn the user about
  * potential misconfigurations. It should be passed into Preferences.addSetting
