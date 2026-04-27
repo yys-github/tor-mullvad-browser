f6fbacbb9226cdec0bdef73869296ee2f01ef2f6
Pier Angelo Vendrame <pierov@torproject.org>
2026-04-27 09:06:32 +0200

    fixup! BB 44772: Disable efficient randomization for canvases.
    BB 44895: Revert "BB 44772: Disable efficient randomization for canvases."
    
    This reverts commit ced1d9a35c7d8cbb727d8c1c450b2dce62cd7289.
    
    We have investigated this issue more, and this change was not needed
    after all, therefore we can just revert this patch.

--- dom/canvas/CanvasUtils.cpp
+++ dom/canvas/CanvasUtils.cpp
@@ -382,9 +382,14 @@ ImageExtraction ImageExtractionResult(dom::HTMLCanvasElement* aCanvasElement,
     return ImageExtraction::Placeholder;
   }
 
-  if ((ownerDoc->ShouldResistFingerprinting(
-           RFPTarget::EfficientCanvasRandomization) ||
-       ownerDoc->ShouldResistFingerprinting(RFPTarget::CanvasRandomization) ||
+  if (ownerDoc->ShouldResistFingerprinting(
+          RFPTarget::EfficientCanvasRandomization) &&
+      GetCanvasExtractDataPermission(aPrincipal) !=
+          nsIPermissionManager::ALLOW_ACTION) {
+    return ImageExtraction::EfficientRandomize;
+  }
+
+  if ((ownerDoc->ShouldResistFingerprinting(RFPTarget::CanvasRandomization) ||
        ownerDoc->ShouldResistFingerprinting(RFPTarget::WebGLRandomization)) &&
       GetCanvasExtractDataPermission(aPrincipal) !=
           nsIPermissionManager::ALLOW_ACTION) {
