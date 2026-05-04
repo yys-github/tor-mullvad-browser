11e16f5f92b2a6a3810fbc5ba3be44fc66b8e70a
Dan Ballard <dan@mindstab.net>
2026-05-04 15:45:01 -0700

    fixup! [android] Disable features and functionality
    TB 44323: Audit prefs 128-140 in android

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -2691,7 +2691,7 @@ class Settings(
      */
     var isFirstTimeEngagingWithSignup: Boolean by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_first_time_engage_with_signup),
-        default = true,
+        default = false,
     )
 
     /**
