17a120e392da226b92f5705d3edb8271c33be851
Dan Ballard <dan@mindstab.net>
2026-05-04 15:46:01 -0700

    fixup! TB 41972: [android] Disable Mozilla onboarding
    TB 44323: Audit prefs 128 to 140

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -2378,7 +2378,7 @@ class Settings(
      */
     var shouldShowMarketingOnboarding by booleanPreference(
         appContext.getPreferenceKey(R.string.pref_key_should_show_marketing_onboarding),
-        default = true,
+        default = false,
     )
 
     val shouldUseComposableToolbar = true
