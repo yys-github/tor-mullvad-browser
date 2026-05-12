daf81ee425f82ae870603edaab1ff696d3ae658c
Randell Jesup <rjesup@mozilla.com>
2026-05-12 17:44:44 +0000

    Bug 2036666: Fix UrlClassifierFeatureBase mTables/mHosts  a=dmeehan
    Original Revision: https://phabricator.services.mozilla.com/D298167
    
    Differential Revision: https://phabricator.services.mozilla.com/D300085

--- netwerk/url-classifier/UrlClassifierFeatureBase.h
+++ netwerk/url-classifier/UrlClassifierFeatureBase.h
@@ -10,6 +10,7 @@
 #include "nsIUrlClassifierFeature.h"
 #include "nsIUrlClassifierExceptionListService.h"
 #include "nsIUrlClassifierExceptionList.h"
+#include "mozilla/Mutex.h"
 #include "nsCOMPtr.h"
 #include "nsTArray.h"
 #include "nsString.h"
@@ -61,16 +62,26 @@ class UrlClassifierFeatureBase : public nsIUrlClassifierFeature,
 
   nsCString mName;
 
+ public:
+  struct PrefCallbackData {
+    mozilla::Mutex* mMutex;
+    nsTArray<nsCString>* mArray;
+  };
+
  private:
   nsCString mPrefExceptionHosts;
 
+  mozilla::Mutex mDataMutex{"UrlClassifierFeatureBase::mDataMutex"};
+
   // 2: blocklist and entitylist.
   nsCString mPrefTables[2];
-  nsTArray<nsCString> mTables[2];
+  nsTArray<nsCString> mTables[2] MOZ_GUARDED_BY(mDataMutex);
+  PrefCallbackData mTablesCbData[2];
 
   nsCString mPrefHosts[2];
   nsCString mPrefTableNames[2];
-  nsTArray<nsCString> mHosts[2];
+  nsTArray<nsCString> mHosts[2] MOZ_GUARDED_BY(mDataMutex);
+  PrefCallbackData mHostsCbData[2];
 
   nsCOMPtr<nsIUrlClassifierExceptionList> mExceptionList;
 };
