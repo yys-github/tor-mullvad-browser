dde23870e185f081c8533d3ca5cec2b1add2ffa6
Beatriz Rizental <bea@torproject.org>
2026-05-12 17:55:49 -0300

    fixup! BB 43243: Modify mozharness scripts for Base Browser
    Bug 44212: Refactor a bit of the marionette on android mozharness support

--- testing/mozharness/scripts/android_emulator_unittest.py
+++ testing/mozharness/scripts/android_emulator_unittest.py
@@ -13,6 +13,8 @@ import sys
 import tempfile
 import time
 
+import yaml
+
 # load modules from parent dir
 here = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(1, os.path.dirname(here))
@@ -512,12 +514,9 @@ class AndroidEmulatorTest(
         self.run_command([adb, "forward", "tcp:2828", "tcp:2828"])
 
         with tempfile.NamedTemporaryFile(suffix=".yaml") as tmp_file:
-            tmp_file.write(
-                b"""args:
-- --marionette
-- --remote-allow-system-access
-"""
-            )
+            config = {"args": ["--marionette", "--remote-allow-system-access"]}
+
+            tmp_file.write(yaml.dump(config, encoding="utf-8"))
             tmp_file.flush()
 
             remote_path = f"/data/local/tmp/{self.package_name}-geckoview-config.yaml"
@@ -574,7 +573,7 @@ class AndroidEmulatorTest(
         if requirements:
             self.register_virtualenv_module(requirements=[requirements])
 
-        if ("marionette", "marionette") in suites:
+        if any("marionette" in suite_name for _, suite_name in self._query_suites()):
             self._configure_marionette_virtualenv(action)
 
     def download_and_extract(self):
@@ -614,7 +613,7 @@ class AndroidEmulatorTest(
         for per_test_suite, suite in suites:
             self.test_suite = suite
 
-            if self.test_suite == "marionette":
+            if "marionette" in self.test_suite:
                 self._marionette_setup()
 
             try:
@@ -698,7 +697,7 @@ class AndroidEmulatorTest(
 
     @PostScriptAction("run-tests")
     def marionette_teardown(self, *args, **kwargs):
-        if ("marionette", "marionette") in self._query_suites():
+        if any("marionette" in suite_name for _, suite_name in self._query_suites()):
             adb = self.query_exe("adb")
             self.run_command([adb, "shell", "am", "force-stop", self.package_name])
             self.run_command([adb, "shell", "am", "clear-debug-app"])
