Kevinisagirl21/tor-browser

TB 42247: Android helpers for the TorProvider
GeckoView is missing some API we use on desktop for the integration
with the tor daemon, such as subprocess.
Therefore, we need to implement them in Java and plumb the data
back and forth between JS and Java.

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -238,6 +238,8 @@ public final class GeckoRuntime implements Parcelable {
   private final ProfilerController mProfilerController;
   private final GeckoScreenChangeListener mScreenChangeListener;
 
+  private TorIntegrationAndroid mTorIntegration;
+
   private GeckoRuntime() {
     mWebExtensionController = new WebExtensionController(this);
     mContentBlockingController = new ContentBlockingController();
@@ -487,6 +489,8 @@ public final class GeckoRuntime implements Parcelable {
       mScreenChangeListener.enable();
     }
 
+    mTorIntegration = new TorIntegrationAndroid(context);
+
     mProfilerController.addMarker(
         "GeckoView Initialization START", mProfilerController.getProfilerTime());
     return true;
@@ -595,6 +599,10 @@ public final class GeckoRuntime implements Parcelable {
 
     if (mScreenChangeListener != null) {
       mScreenChangeListener.disable();
+    }
+
+    if (mTorIntegration != null) {
+      mTorIntegration.shutdown();
     }
 
     GeckoThread.forceQuit();
@@ -995,6 +1003,14 @@ public final class GeckoRuntime implements Parcelable {
     }
 
     return mPushController;
+  }
+
+  /**
+   * Get the Tor integration controller for this runtime.
+   */
+  @UiThread
+  public @NonNull TorIntegrationAndroid getTorIntegrationController() {
+    return mTorIntegration;
   }
 
   /**

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -2601,6 +2601,24 @@ public class GeckoSession {
   }
 
   /**
+   * Try to get last circuit used in this session, if possible.
+   *
+   * @return The circuit information as a {@link GeckoResult} object.
+   */
+  @AnyThread
+  public @NonNull GeckoResult<GeckoBundle> getTorCircuit() {
+    return mEventDispatcher.queryBundle("GeckoView:GetTorCircuit");
+  }
+
+  /**
+   * Change the circuit for this session.
+   */
+  @UiThread
+  public void newTorCircuit() {
+    mEventDispatcher.dispatch("GeckoView:NewTorCircuit", null);
+  }
+
+  /**
    * Set this GeckoSession as active or inactive, which represents if the session is currently
    * visible or not. Setting a GeckoSession to inactive will significantly reduce its memory
    * footprint, but should only be done if the GeckoSession is not currently visible. Note that a

mobile/shared/modules/geckoview/GeckoViewContent.sys.mjs
@@ -8,6 +8,7 @@ const lazy = {};
 ChromeUtils.defineESModuleGetters(lazy, {
   isProductURL: "chrome://global/content/shopping/ShoppingProduct.mjs",
   ShoppingProduct: "chrome://global/content/shopping/ShoppingProduct.mjs",
+  TorDomainIsolator: "resource://gre/modules/TorDomainIsolator.sys.mjs",
 });
 
 export class GeckoViewContent extends GeckoViewModule {
@@ -38,6 +39,8 @@ export class GeckoViewContent extends GeckoViewModule {
       "GeckoView:UpdateInitData",
       "GeckoView:ZoomToInput",
       "GeckoView:IsPdfJs",
+      "GeckoView:GetTorCircuit",
+      "GeckoView:NewTorCircuit",
     ]);
   }
 
@@ -308,6 +311,12 @@ export class GeckoViewContent extends GeckoViewModule {
       case "GeckoView:HasCookieBannerRuleForBrowsingContextTree":
         this._hasCookieBannerRuleForBrowsingContextTree(aCallback);
         break;
+      case "GeckoView:GetTorCircuit":
+        this._getTorCircuit(aCallback);
+        break;
+      case "GeckoView:NewTorCircuit":
+        this._newTorCircuit(aCallback);
+        break;
     }
   }
 
@@ -416,6 +425,25 @@ export class GeckoViewContent extends GeckoViewModule {
         break;
       }
     }
+  }
+
+  _getTorCircuit(aCallback) {
+    if (this.browser && aCallback) {
+      const domain = lazy.TorDomainIsolator.getDomainForBrowser(this.browser);
+      const nodes = lazy.TorDomainIsolator.getCircuit(
+        this.browser,
+        domain,
+        this.browser.contentPrincipal.originAttributes.userContextId
+      );
+      aCallback?.onSuccess({ domain, nodes });
+    } else {
+      aCallback?.onSuccess(null);
+    }
+  }
+
+  _newTorCircuit(aCallback) {
+    lazy.TorDomainIsolator.newCircuitForBrowser(this.browser);
+    aCallback?.onSuccess();
   }
 
   async _containsFormData(aCallback) {

yys-github/tor-mullvad-browser

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -266,6 +266,8 @@ public final class GeckoRuntime implements Parcelable {
   private final CrashPullController.CrashPullProxy mCrashPullProxy;
   private final GeckoScreenChangeListener mScreenChangeListener;
 
+  private TorAndroidIntegration mTorIntegration;
+
   private GeckoRuntime() {
     mWebExtensionController = new WebExtensionController(this);
     mContentBlockingController = new ContentBlockingController();
@@ -582,6 +584,8 @@ public final class GeckoRuntime implements Parcelable {
     // Warm up window context of default display.
     GeckoAppShell.maybeInitScreen();
 
+    mTorIntegration = new TorAndroidIntegration(context);
+
     ProfilerController.addMarker(
         "GeckoView Initialization START", ProfilerController.getProfilerTime());
     return true;
@@ -695,6 +699,10 @@ public final class GeckoRuntime implements Parcelable {
 
     if (mScreenChangeListener != null) {
       mScreenChangeListener.disable();
+    }
+
+    if (mTorIntegration != null) {
+      mTorIntegration.shutdown();
     }
 
     GeckoThread.forceQuit();
@@ -1163,6 +1171,14 @@ public final class GeckoRuntime implements Parcelable {
   public void notifyTelemetryPrefChanged(final boolean isEnabled) {
     GeckoAppShell.notifyObservers(
         "mobile-telemetry-pref-changed", isEnabled ? "enabled" : "disabled");
+  }
+
+  /**
+   * Get the Tor integration controller for this runtime.
+   */
+  @UiThread
+  public @NonNull TorAndroidIntegration getTorIntegrationController() {
+    return mTorIntegration;
   }
 
   /**

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -2815,6 +2815,24 @@ public class GeckoSession {
   }
 
   /**
+   * Try to get last circuit used in this session, if possible.
+   *
+   * @return The circuit information as a {@link GeckoResult} object.
+   */
+  @AnyThread
+  public @NonNull GeckoResult<GeckoBundle> getTorCircuits() {
+    return mEventDispatcher.queryBundle("GeckoView:GetTorCircuits");
+  }
+
+  /**
+   * Change the circuit for this session.
+   */
+  @UiThread
+  public void newTorCircuit() {
+    mEventDispatcher.dispatch("GeckoView:NewTorCircuit", null);
+  }
+
+  /**
    * Set this GeckoSession as active or inactive, which represents if the session is currently
    * visible or not. Setting a GeckoSession to inactive will significantly reduce its memory
    * footprint, but should only be done if the GeckoSession is not currently visible. Note that a

mobile/shared/modules/geckoview/GeckoViewContent.sys.mjs
@@ -4,6 +4,11 @@
 
 import { GeckoViewModule } from "resource://gre/modules/GeckoViewModule.sys.mjs";
 
+const lazy = {};
+ChromeUtils.defineESModuleGetters(lazy, {
+  TorDomainIsolator: "resource://gre/modules/TorDomainIsolator.sys.mjs",
+});
+
 export class GeckoViewContent extends GeckoViewModule {
   onInit() {
     this.registerListener([
@@ -25,6 +30,8 @@ export class GeckoViewContent extends GeckoViewModule {
       "GeckoView:IsPdfJs",
       "GeckoView:GetWebCompatInfo",
       "GeckoView:SendMoreWebCompatInfo",
+      "GeckoView:GetTorCircuit",
+      "GeckoView:NewTorCircuit",
     ]);
   }
 
@@ -294,6 +301,12 @@ export class GeckoViewContent extends GeckoViewModule {
       case "GeckoView:ProcessBackPressed":
         this._processBackPressed(aCallback);
         break;
+      case "GeckoView:GetTorCircuits":
+        this._getTorCircuits(aCallback);
+        break;
+      case "GeckoView:NewTorCircuit":
+        this._newTorCircuit(aCallback);
+        break;
     }
   }
 
@@ -449,6 +462,25 @@ export class GeckoViewContent extends GeckoViewModule {
     } catch (error) {
       aCallback.onError(`Cannot send more web compat info, error: ${error}`);
     }
+  }
+
+  _getTorCircuits(aCallback) {
+    if (this.browser && aCallback) {
+      const domain = lazy.TorDomainIsolator.getDomainForBrowser(this.browser);
+      const circuits = lazy.TorDomainIsolator.getCircuits(
+        this.browser,
+        domain,
+        this.browser.contentPrincipal.originAttributes.userContextId
+      );
+      aCallback?.onSuccess({ domain, circuits });
+    } else {
+      aCallback?.onSuccess(null);
+    }
+  }
+
+  _newTorCircuit(aCallback) {
+    lazy.TorDomainIsolator.newCircuitForBrowser(this.browser);
+    aCallback?.onSuccess();
   }
 
   async _containsFormData(aCallback) {