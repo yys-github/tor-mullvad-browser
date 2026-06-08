5bfa4d9848ab0af8ea8e7f75df885f93f40b3a49
Henry Wilkes <henry@torproject.org>
2026-06-08 15:32:40 +0100

    fixup! BB 44711: Hide unwanted setting controls in Base Browser.
    BB 45043: Re-add missing changes to settings after 152 rebase.
    
    Drop changes to "addresses" and "payments" since these now properly
    hidden without the formautofill extension being in the build.

--- browser/components/preferences/config/passwords-autofill.mjs
+++ browser/components/preferences/config/passwords-autofill.mjs
@@ -781,9 +781,6 @@ SettingGroupManager.registerGroups({
     ],
   },
   managePayments: {
-    // Hide the payments settings. tor-browser#44460.
-    hidden: true,
-    hiddenFromSearch: true,
     items: [
       {
         id: "add-payment-button",
@@ -800,9 +797,6 @@ SettingGroupManager.registerGroups({
     ],
   },
   manageAddresses: {
-    // Hide the addresses settings. tor-browser#44460.
-    hidden: true,
-    hiddenFromSearch: true,
     items: [
       {
         id: "add-address-button",
