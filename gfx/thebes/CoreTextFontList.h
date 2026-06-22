55457cf918695ca6b552f50ac6e0e10a311b4093
Jonathan Kew <jkew@mozilla.com>
2026-06-22 15:52:27 +0200

    BB 44728: Activate app-bundled fonts in the GPU process on macOS.
    Bugzilla: https://bugzilla.mozilla.org/show_bug.cgi?id=2032466
    
    Differential Revision: https://phabricator.services.mozilla.com/D308045

--- gfx/thebes/CoreTextFontList.h
+++ gfx/thebes/CoreTextFontList.h
@@ -181,6 +181,10 @@ class CoreTextFontList : public gfxPlatformFontList {
   };
   void ReadSystemFontList(mozilla::dom::SystemFontList*);
 
+  static void ActivateFontsFromDir(
+      const nsACString& aDir,
+      nsTHashSet<nsCStringHashKey>* aLoadedFamilies = nullptr);
+
  protected:
   CoreTextFontList();
   virtual ~CoreTextFontList();
@@ -238,10 +242,6 @@ class CoreTextFontList : public gfxPlatformFontList {
 
   void AddFamily(const nsACString& aFamilyName, FontVisibility aVisibility)
       MOZ_REQUIRES(mLock);
-
-  static void ActivateFontsFromDir(
-      const nsACString& aDir,
-      nsTHashSet<nsCStringHashKey>* aLoadedFamilies = nullptr);
 
   already_AddRefed<gfxFontEntry> CreateFontEntry(
       mozilla::fontlist::Face* aFace,
