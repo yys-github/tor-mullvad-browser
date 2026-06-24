5015ae65ab4f4048b7d67d20f81ee4ef33d8eef5
Masayuki Nakano <masayuki@d-toybox.com>
2026-06-24 22:20:45 +0000

    Bug 2045619 - Make `CompositionTransaction::DoTransaction` scan the replace start position as far as possible  a=RyanVM DONTBUILD
    The replace start may be in the following `Text`. Therefore, we should
    scan the following `Text` before inserting the new composition string.
    
    Original Revision: https://phabricator.services.mozilla.com/D305783
    
    Differential Revision: https://phabricator.services.mozilla.com/D307458

--- editor/libeditor/EditorBase.cpp
+++ editor/libeditor/EditorBase.cpp
@@ -3527,10 +3527,8 @@ EditorBase::InsertTextIntoTextNodeWithTransaction(
     if (NS_WARN_IF(!mComposition->GetContainerTextNode())) {
       return aPointToInsert;
     }
-    return EditorDOMPointInText(
-        mComposition->GetContainerTextNode(),
-        std::min(mComposition->XPOffsetInTextNode(),
-                 mComposition->GetContainerTextNode()->TextDataLength()));
+    return EditorDOMPointInText(mComposition->GetContainerTextNode(),
+                                mComposition->ClampedStartOffsetInTextNode());
   }();
 
   EditorDOMPoint endOfInsertedText(
@@ -4120,9 +4118,9 @@ nsresult EditorBase::OnCompositionChange(
   if (IsHTMLEditor() && mComposition->GetContainerTextNode()) {
     RefPtr<StaticRange> targetRange = StaticRange::Create(
         mComposition->GetContainerTextNode(),
-        mComposition->XPOffsetInTextNode(),
+        mComposition->ClampedStartOffsetInTextNode(),
         mComposition->GetContainerTextNode(),
-        mComposition->XPEndOffsetInTextNode(), IgnoreErrors());
+        mComposition->ClampedEndOffsetInTextNode(), IgnoreErrors());
     NS_WARNING_ASSERTION(targetRange && targetRange->IsPositioned(),
                          "StaticRange::Create() failed");
     if (targetRange && targetRange->IsPositioned()) {
@@ -5902,18 +5900,19 @@ nsresult EditorBase::InitializeSelection(
     MOZ_ASSERT(textNode,
                "There must be text node if composition string is not empty");
     if (textNode) {
-      MOZ_ASSERT(textNode->Length() >= mComposition->XPEndOffsetInTextNode(),
+      MOZ_ASSERT(textNode->Length() >=
+                     mComposition->EndOffsetMaybeInFollowingTextNode(),
                  "The text node must be different from the old text node");
       RefPtr<TextRangeArray> ranges = mComposition->GetRanges();
       DebugOnly<nsresult> rvIgnored = CompositionTransaction::SetIMESelection(
-          *this, textNode, mComposition->XPOffsetInTextNode(),
-          mComposition->XPLengthInTextNode(), ranges);
+          *this, textNode, mComposition->StartOffsetMaybeInFollowingTextNode(),
+          mComposition->LengthMaybeInFollowingTextNode(), ranges);
       NS_WARNING_ASSERTION(
           NS_SUCCEEDED(rvIgnored),
           "CompositionTransaction::SetIMESelection() failed, but ignored");
       mComposition->OnUpdateCompositionInEditor(
           mComposition->String(), *textNode,
-          mComposition->XPOffsetInTextNode());
+          mComposition->StartOffsetMaybeInFollowingTextNode());
     }
   }
 
--- editor/libeditor/TextEditSubActionHandler.cpp
+++ editor/libeditor/TextEditSubActionHandler.cpp
@@ -379,7 +379,7 @@ Result<EditActionResult, nsresult> TextEditor::HandleInsertText(
   uint32_t start = 0;
   if (IsPasswordEditor()) {
     if (GetComposition() && !GetComposition()->String().IsEmpty()) {
-      start = GetComposition()->XPOffsetInTextNode();
+      start = GetComposition()->ClampedStartOffsetInTextNode();
     } else {
       uint32_t end = 0;
       nsContentUtils::GetSelectionInTextControl(&SelectionRef(), GetRoot(),
