Kevinisagirl21/tor-browser

TB 31286: Implementation of bridge, proxy, and firewall settings in about:preferences#connection
This patch adds a new about:preferences#connection page which allows
modifying bridge, proxy, and firewall settings from within Tor Browser.
All of the functionality present in tor-launcher's Network
Configuration panel is present:

 - Setting built-in bridges
 - Requesting bridges from BridgeDB via moat
 - Using user-provided bridges
 - Configuring SOCKS4, SOCKS5, and HTTP/HTTPS proxies
 - Setting firewall ports
 - Viewing and Copying Tor's logs
 - The Networking Settings in General preferences has been removed

Bug 40774: Update about:preferences page to match new UI designs

browser/components/torpreferences/content/builtinBridgeDialog.js
@@ -1,0 +1,140 @@
+"use strict";
+
+const { TorSettings, TorBridgeSource } = ChromeUtils.importESModule(
+  "resource://gre/modules/TorSettings.sys.mjs"
+);
+
+const { TorConnect, TorConnectTopics } = ChromeUtils.importESModule(
+  "resource://gre/modules/TorConnect.sys.mjs"
+);
+
+const gBuiltinBridgeDialog = {
+  init() {
+    this._result = window.arguments[0];
+
+    this._radioGroup = document.getElementById(
+      "torPreferences-builtinBridge-typeSelection"
+    );
+
+    const currentBuiltinType =
+      TorSettings.bridges.enabled &&
+      TorSettings.bridges.source == TorBridgeSource.BuiltIn
+        ? TorSettings.bridges.builtin_type
+        : null;
+
+    for (const optionEl of this._radioGroup.querySelectorAll(
+      ".builtin-bridges-option"
+    )) {
+      const radio = optionEl.querySelector("radio");
+      const type = radio.value;
+      optionEl.hidden = !TorSettings.builtinBridgeTypes.includes(type);
+
+      const descriptionEl = optionEl.querySelector(
+        ".builtin-bridges-option-description"
+      );
+      // Set an id to be used for the aria-describedby.
+      descriptionEl.id = `builtin-bridges-description-${type}`;
+      const currentBadge = optionEl.querySelector(".bridge-status-badge");
+      if (type === currentBuiltinType) {
+        const currentLabelEl = optionEl.querySelector(
+          ".torPreferences-current-bridge-label"
+        );
+        currentLabelEl.id = `builtin-bridges-current-${type}`;
+        // Described by both the current badge and the full description.
+        // These will be concatenated together in the screen reader output.
+        radio.setAttribute(
+          "aria-describedby",
+          `${currentLabelEl.id} ${descriptionEl.id}`
+        );
+        // Make visible.
+        currentBadge.classList.add("bridge-status-current-built-in");
+      } else {
+        // No visible badge.
+        radio.setAttribute("aria-describedby", descriptionEl.id);
+        currentBadge.classList.remove("bridge-status-current-built-in");
+      }
+    }
+
+    if (currentBuiltinType) {
+      this._radioGroup.value = currentBuiltinType;
+    } else {
+      this._radioGroup.selectedItem = null;
+    }
+
+    this._radioGroup.addEventListener("select", () => this.onSelectChange());
+
+    const dialog = document.getElementById(
+      "torPreferences-builtinBridge-dialog"
+    );
+    dialog.addEventListener("dialogaccept", () => {
+      this._result.accepted = true;
+    });
+
+    // Add styling for tor-button to the dialog shadow root.
+    const styleLink = document.createElement("link");
+    styleLink.rel = "stylesheet";
+    styleLink.href =
+      "chrome://browser/content/torpreferences/torPreferences.css";
+    dialog.shadowRoot.append(styleLink);
+
+    this._acceptButton = dialog.getButton("accept");
+
+    Services.obs.addObserver(this, TorConnectTopics.StageChange);
+
+    this.onSelectChange();
+    this.onAcceptStateChange();
+  },
+
+  uninit() {
+    Services.obs.removeObserver(this, TorConnectTopics.StageChange);
+  },
+
+  onSelectChange() {
+    const value = this._radioGroup.value;
+    this._acceptButton.disabled = !value;
+    this._result.type = value;
+  },
+
+  onAcceptStateChange() {
+    const connect = TorConnect.canBeginBootstrap;
+    this._result.connect = connect;
+    this._acceptButton.setAttribute(
+      "data-l10n-id",
+      connect ? "bridge-dialog-button-connect" : "bridge-dialog-button-accept"
+    );
+    this._acceptButton.classList.toggle("tor-button", connect);
+  },
+
+  observe(subject, topic) {
+    switch (topic) {
+      case TorConnectTopics.StageChange:
+        this.onAcceptStateChange();
+        break;
+    }
+  },
+};
+
+// Initial focus is not visible, even if opened with a keyboard. We avoid the
+// default handler and manage the focus ourselves, which will paint the focus
+// ring by default.
+// NOTE: A side effect is that the focus ring will show even if the user opened
+// with a mouse event.
+// TODO: Remove this once bugzilla bug 1708261 is resolved.
+document.subDialogSetDefaultFocus = () => {
+  document.getElementById("torPreferences-builtinBridge-typeSelection").focus();
+};
+
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    gBuiltinBridgeDialog.init();
+    window.addEventListener(
+      "unload",
+      () => {
+        gBuiltinBridgeDialog.uninit();
+      },
+      { once: true }
+    );
+  },
+  { once: true }
+);

browser/components/torpreferences/content/requestBridgeDialog.js
@@ -1,0 +1,210 @@
+"use strict";
+
+const { BridgeDB } = ChromeUtils.importESModule(
+  "resource://gre/modules/BridgeDB.sys.mjs"
+);
+
+const { TorConnect, TorConnectTopics } = ChromeUtils.importESModule(
+  "resource://gre/modules/TorConnect.sys.mjs"
+);
+
+const log = console.createInstance({
+  maxLogLevel: "Warn",
+  prefix: "requestBridgeDialog",
+});
+
+const gRequestBridgeDialog = {
+  selectors: {
+    dialogHeader: "#torPreferences-requestBridge-header",
+    captchaImage: "image#torPreferences-requestBridge-captchaImage",
+    captchaEntryTextbox: "input#torPreferences-requestBridge-captchaTextbox",
+    refreshCaptchaButton:
+      "button#torPreferences-requestBridge-refreshCaptchaButton",
+    incorrectCaptchaHbox:
+      "hbox#torPreferences-requestBridge-incorrectCaptchaHbox",
+  },
+
+  init() {
+    this._result = window.arguments[0];
+
+    const selectors = this.selectors;
+
+    this._dialog = document.getElementById(
+      "torPreferences-requestBridge-dialog"
+    );
+
+    // Add styling for tor-button to the dialog shadow root.
+    const styleLink = document.createElement("link");
+    styleLink.rel = "stylesheet";
+    styleLink.href =
+      "chrome://browser/content/torpreferences/torPreferences.css";
+    this._dialog.shadowRoot.append(styleLink);
+
+    // user may have opened a Request Bridge dialog in another tab, so update the
+    // CAPTCHA image or close out the dialog if we have a bridge list
+    this._dialog.addEventListener("focusin", () => {
+      const uri = BridgeDB.currentCaptchaImage;
+      const bridges = BridgeDB.currentBridges;
+
+      // new captcha image
+      if (uri) {
+        this._setcaptchaImage(uri);
+      } else if (bridges) {
+        this._dialog.cancelDialog();
+      }
+    });
+
+    this._submitButton = this._dialog.getButton("accept");
+    this._submitButton.disabled = true;
+    this._dialog.addEventListener("dialogaccept", e => {
+      e.preventDefault();
+      this.onSubmitCaptcha();
+    });
+
+    this._dialogHeader = this._dialog.querySelector(selectors.dialogHeader);
+
+    this._captchaImage = this._dialog.querySelector(selectors.captchaImage);
+
+    // request captcha from bridge db
+    BridgeDB.requestNewCaptchaImage().then(uri => {
+      this._setcaptchaImage(uri);
+    });
+
+    this._captchaEntryTextbox = this._dialog.querySelector(
+      selectors.captchaEntryTextbox
+    );
+    this._captchaEntryTextbox.disabled = true;
+    // disable submit if entry textbox is empty
+    this._captchaEntryTextbox.oninput = () => {
+      this._submitButton.disabled = this._captchaEntryTextbox.value == "";
+    };
+
+    this._captchaRefreshButton = this._dialog.querySelector(
+      selectors.refreshCaptchaButton
+    );
+    this._captchaRefreshButton.disabled = true;
+    this._captchaRefreshButton.addEventListener("command", () => {
+      this.onRefreshCaptcha();
+    });
+
+    this._incorrectCaptchaHbox = this._dialog.querySelector(
+      selectors.incorrectCaptchaHbox
+    );
+
+    Services.obs.addObserver(this, TorConnectTopics.StageChange);
+    this.onAcceptStateChange();
+  },
+
+  uninit() {
+    BridgeDB.close();
+    // Unregister our observer topics.
+    Services.obs.removeObserver(this, TorConnectTopics.StageChange);
+  },
+
+  onAcceptStateChange() {
+    const connect = TorConnect.canBeginBootstrap;
+    this._result.connect = connect;
+    this._submitButton.setAttribute(
+      "data-l10n-id",
+      connect ? "bridge-dialog-button-connect" : "bridge-dialog-button-submit"
+    );
+    this._submitButton.classList.toggle("tor-button", connect);
+  },
+
+  observe(subject, topic) {
+    switch (topic) {
+      case TorConnectTopics.StageChange:
+        this.onAcceptStateChange();
+        break;
+    }
+  },
+
+  _setcaptchaImage(uri) {
+    if (uri != this._captchaImage.src) {
+      this._captchaImage.src = uri;
+      this._dialogHeader.setAttribute(
+        "data-l10n-id",
+        "request-bridge-dialog-top-solve"
+      );
+      this._setUIDisabled(false);
+      this._captchaEntryTextbox.focus();
+      this._captchaEntryTextbox.select();
+    }
+  },
+
+  _setUIDisabled(disabled) {
+    this._submitButton.disabled = this._captchaGuessIsEmpty() || disabled;
+    this._captchaEntryTextbox.disabled = disabled;
+    this._captchaRefreshButton.disabled = disabled;
+  },
+
+  _captchaGuessIsEmpty() {
+    return this._captchaEntryTextbox.value == "";
+  },
+
+  /*
+    Event Handlers
+  */
+  onSubmitCaptcha() {
+    let captchaText = this._captchaEntryTextbox.value.trim();
+    // noop if the field is empty
+    if (captchaText == "") {
+      return;
+    }
+
+    // freeze ui while we make request
+    this._setUIDisabled(true);
+    this._incorrectCaptchaHbox.style.visibility = "hidden";
+
+    BridgeDB.submitCaptchaGuess(captchaText)
+      .then(aBridges => {
+        if (aBridges && aBridges.length) {
+          this._result.accepted = true;
+          this._result.bridges = aBridges;
+          this._submitButton.disabled = false;
+          // This was successful, but use cancelDialog() to close, since
+          // we intercept the `dialogaccept` event.
+          this._dialog.cancelDialog();
+        } else {
+          this._setUIDisabled(false);
+          this._incorrectCaptchaHbox.style.visibility = "visible";
+        }
+      })
+      .catch(aError => {
+        // TODO: handle other errors properly here when we do the bridge settings re-design
+        this._setUIDisabled(false);
+        this._incorrectCaptchaHbox.style.visibility = "visible";
+        log.error(aError);
+      });
+  },
+
+  onRefreshCaptcha() {
+    this._setUIDisabled(true);
+    this._captchaImage.src = "";
+    this._dialogHeader.setAttribute(
+      "data-l10n-id",
+      "request-bridge-dialog-top-wait"
+    );
+    this._captchaEntryTextbox.value = "";
+    this._incorrectCaptchaHbox.style.visibility = "hidden";
+
+    BridgeDB.requestNewCaptchaImage().then(uri => {
+      this._setcaptchaImage(uri);
+    });
+  },
+};
+
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    gRequestBridgeDialog.init();
+    window.addEventListener(
+      "unload",
+      () => {
+        gRequestBridgeDialog.uninit();
+      },
+      { once: true }
+    );
+  },
+  { once: true }
+);

yys-github/tor-mullvad-browser

browser/components/torpreferences/content/builtinBridgeDialog.js
@@ -1,0 +1,139 @@
+"use strict";
+
+const { TorSettings, TorBridgeSource } = ChromeUtils.importESModule(
+  "moz-src:///toolkit/modules/TorSettings.sys.mjs"
+);
+
+const { TorConnect, TorConnectStage, TorConnectTopics } =
+  ChromeUtils.importESModule("moz-src:///toolkit/modules/TorConnect.sys.mjs");
+
+const gBuiltinBridgeDialog = {
+  init() {
+    this._result = window.arguments[0];
+
+    this._radioGroup = document.getElementById(
+      "torPreferences-builtinBridge-typeSelection"
+    );
+
+    const currentBuiltinType =
+      TorSettings.bridges.enabled &&
+      TorSettings.bridges.source == TorBridgeSource.BuiltIn
+        ? TorSettings.bridges.builtin_type
+        : null;
+
+    for (const optionEl of this._radioGroup.querySelectorAll(
+      ".builtin-bridges-option"
+    )) {
+      const radio = optionEl.querySelector("radio");
+      const type = radio.value;
+      optionEl.hidden = !TorSettings.builtinBridgeTypes.includes(type);
+
+      const descriptionEl = optionEl.querySelector(
+        ".builtin-bridges-option-description"
+      );
+      // Set an id to be used for the aria-describedby.
+      descriptionEl.id = `builtin-bridges-description-${type}`;
+      const currentBadge = optionEl.querySelector(".bridge-status-badge");
+      if (type === currentBuiltinType) {
+        const currentLabelEl = optionEl.querySelector(
+          ".torPreferences-current-bridge-label"
+        );
+        currentLabelEl.id = `builtin-bridges-current-${type}`;
+        // Described by both the current badge and the full description.
+        // These will be concatenated together in the screen reader output.
+        radio.setAttribute(
+          "aria-describedby",
+          `${currentLabelEl.id} ${descriptionEl.id}`
+        );
+        // Make visible.
+        currentBadge.classList.add("bridge-status-current-built-in");
+      } else {
+        // No visible badge.
+        radio.setAttribute("aria-describedby", descriptionEl.id);
+        currentBadge.classList.remove("bridge-status-current-built-in");
+      }
+    }
+
+    if (currentBuiltinType) {
+      this._radioGroup.value = currentBuiltinType;
+    } else {
+      this._radioGroup.selectedItem = null;
+    }
+
+    this._radioGroup.addEventListener("select", () => this.onSelectChange());
+
+    const dialog = document.getElementById(
+      "torPreferences-builtinBridge-dialog"
+    );
+    dialog.addEventListener("dialogaccept", () => {
+      this._result.accepted = true;
+    });
+
+    // Add styling for tor-button to the dialog shadow root.
+    const styleLink = document.createElement("link");
+    styleLink.rel = "stylesheet";
+    styleLink.href =
+      "chrome://browser/content/torpreferences/torPreferences.css";
+    dialog.shadowRoot.append(styleLink);
+
+    this._acceptButton = dialog.getButton("accept");
+
+    Services.obs.addObserver(this, TorConnectTopics.StageChange);
+
+    this.onSelectChange();
+    this.onAcceptStateChange();
+  },
+
+  uninit() {
+    Services.obs.removeObserver(this, TorConnectTopics.StageChange);
+  },
+
+  onSelectChange() {
+    const value = this._radioGroup.value;
+    this._acceptButton.disabled = !value;
+    this._result.type = value;
+  },
+
+  onAcceptStateChange() {
+    const connect = TorConnect.stageName !== TorConnectStage.Bootstrapped;
+    this._result.connect = connect;
+    this._acceptButton.setAttribute(
+      "data-l10n-id",
+      connect ? "bridge-dialog-button-connect2" : "bridge-dialog-button-accept2"
+    );
+    this._acceptButton.classList.toggle("tor-button", connect);
+  },
+
+  observe(subject, topic) {
+    switch (topic) {
+      case TorConnectTopics.StageChange:
+        this.onAcceptStateChange();
+        break;
+    }
+  },
+};
+
+// Initial focus is not visible, even if opened with a keyboard. We avoid the
+// default handler and manage the focus ourselves, which will paint the focus
+// ring by default.
+// NOTE: A side effect is that the focus ring will show even if the user opened
+// with a mouse event.
+// TODO: Remove this once bugzilla bug 1708261 is resolved.
+document.subDialogSetDefaultFocus = () => {
+  document.getElementById("torPreferences-builtinBridge-typeSelection").focus();
+};
+
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    gBuiltinBridgeDialog.init();
+    window.addEventListener(
+      "unload",
+      () => {
+        gBuiltinBridgeDialog.uninit();
+      },
+      { once: true }
+    );
+  },
+  { once: true }
+);

browser/components/torpreferences/content/requestBridgeDialog.js
@@ -1,0 +1,212 @@
+"use strict";
+
+const { BridgeDB } = ChromeUtils.importESModule(
+  "moz-src:///toolkit/modules/BridgeDB.sys.mjs"
+);
+
+const { TorConnect, TorConnectStage, TorConnectTopics } =
+  ChromeUtils.importESModule("moz-src:///toolkit/modules/TorConnect.sys.mjs");
+
+const log = console.createInstance({
+  maxLogLevel: "Warn",
+  prefix: "requestBridgeDialog",
+});
+
+const gRequestBridgeDialog = {
+  selectors: {
+    dialogHeader: "#torPreferences-requestBridge-header",
+    captchaImage: "image#torPreferences-requestBridge-captchaImage",
+    captchaEntryTextbox: "input#torPreferences-requestBridge-captchaTextbox",
+    refreshCaptchaButton:
+      "button#torPreferences-requestBridge-refreshCaptchaButton",
+    incorrectCaptchaHbox:
+      "hbox#torPreferences-requestBridge-incorrectCaptchaHbox",
+  },
+
+  init() {
+    this._result = window.arguments[0];
+
+    const selectors = this.selectors;
+
+    this._dialog = document.getElementById(
+      "torPreferences-requestBridge-dialog"
+    );
+
+    // Add styling for tor-button to the dialog shadow root.
+    const styleLink = document.createElement("link");
+    styleLink.rel = "stylesheet";
+    styleLink.href =
+      "chrome://browser/content/torpreferences/torPreferences.css";
+    this._dialog.shadowRoot.append(styleLink);
+
+    // user may have opened a Request Bridge dialog in another tab, so update the
+    // CAPTCHA image or close out the dialog if we have a bridge list
+    this._dialog.addEventListener("focusin", () => {
+      const uri = BridgeDB.currentCaptchaImage;
+      const bridges = BridgeDB.currentBridges;
+
+      // new captcha image
+      if (uri) {
+        this._setcaptchaImage(uri);
+      } else if (bridges) {
+        this._dialog.cancelDialog();
+      }
+    });
+
+    this._submitButton = this._dialog.getButton("accept");
+    this._submitButton.disabled = true;
+    this._dialog.addEventListener("dialogaccept", e => {
+      e.preventDefault();
+      this.onSubmitCaptcha();
+    });
+
+    this._dialogHeader = this._dialog.querySelector(selectors.dialogHeader);
+
+    this._captchaImage = this._dialog.querySelector(selectors.captchaImage);
+
+    // request captcha from bridge db
+    BridgeDB.requestNewCaptchaImage().then(uri => {
+      this._setcaptchaImage(uri);
+    });
+
+    this._captchaEntryTextbox = this._dialog.querySelector(
+      selectors.captchaEntryTextbox
+    );
+    this._captchaEntryTextbox.disabled = true;
+    // disable submit if entry textbox is empty
+    this._captchaEntryTextbox.oninput = () => {
+      this._submitButton.disabled = this._captchaEntryTextbox.value == "";
+    };
+
+    this._captchaRefreshButton = this._dialog.querySelector(
+      selectors.refreshCaptchaButton
+    );
+    this._captchaRefreshButton.disabled = true;
+    this._captchaRefreshButton.addEventListener("command", () => {
+      this.onRefreshCaptcha();
+    });
+
+    this._incorrectCaptchaHbox = this._dialog.querySelector(
+      selectors.incorrectCaptchaHbox
+    );
+
+    Services.obs.addObserver(this, TorConnectTopics.StageChange);
+    this.onAcceptStateChange();
+  },
+
+  uninit() {
+    BridgeDB.close();
+    // Unregister our observer topics.
+    Services.obs.removeObserver(this, TorConnectTopics.StageChange);
+  },
+
+  onAcceptStateChange() {
+    const connect = TorConnect.stageName !== TorConnectStage.Bootstrapped;
+    this._result.connect = connect;
+    this._submitButton.setAttribute(
+      "data-l10n-id",
+      connect ? "bridge-dialog-button-connect2" : "bridge-dialog-button-submit2"
+    );
+    this._submitButton.classList.toggle("tor-button", connect);
+  },
+
+  observe(subject, topic) {
+    switch (topic) {
+      case TorConnectTopics.StageChange:
+        this.onAcceptStateChange();
+        break;
+    }
+  },
+
+  _setcaptchaImage(uri) {
+    if (!uri) {
+      return;
+    }
+    if (uri != this._captchaImage.src) {
+      this._captchaImage.src = uri;
+      this._dialogHeader.setAttribute(
+        "data-l10n-id",
+        "request-bridge-dialog-top-solve"
+      );
+      this._setUIDisabled(false);
+      this._captchaEntryTextbox.focus();
+      this._captchaEntryTextbox.select();
+    }
+  },
+
+  _setUIDisabled(disabled) {
+    this._submitButton.disabled = this._captchaGuessIsEmpty() || disabled;
+    this._captchaEntryTextbox.disabled = disabled;
+    this._captchaRefreshButton.disabled = disabled;
+  },
+
+  _captchaGuessIsEmpty() {
+    return this._captchaEntryTextbox.value == "";
+  },
+
+  /*
+    Event Handlers
+  */
+  onSubmitCaptcha() {
+    let captchaText = this._captchaEntryTextbox.value.trim();
+    // noop if the field is empty
+    if (captchaText == "") {
+      return;
+    }
+
+    // freeze ui while we make request
+    this._setUIDisabled(true);
+    this._incorrectCaptchaHbox.style.visibility = "hidden";
+
+    BridgeDB.submitCaptchaGuess(captchaText)
+      .then(aBridges => {
+        if (aBridges && aBridges.length) {
+          this._result.accepted = true;
+          this._result.bridges = aBridges;
+          this._submitButton.disabled = false;
+          // This was successful, but use cancelDialog() to close, since
+          // we intercept the `dialogaccept` event.
+          this._dialog.cancelDialog();
+        } else {
+          this._setUIDisabled(false);
+          this._incorrectCaptchaHbox.style.visibility = "visible";
+        }
+      })
+      .catch(aError => {
+        // TODO: handle other errors properly here when we do the bridge settings re-design
+        this._setUIDisabled(false);
+        this._incorrectCaptchaHbox.style.visibility = "visible";
+        log.error(aError);
+      });
+  },
+
+  onRefreshCaptcha() {
+    this._setUIDisabled(true);
+    this._captchaImage.src = "";
+    this._dialogHeader.setAttribute(
+      "data-l10n-id",
+      "request-bridge-dialog-top-wait"
+    );
+    this._captchaEntryTextbox.value = "";
+    this._incorrectCaptchaHbox.style.visibility = "hidden";
+
+    BridgeDB.requestNewCaptchaImage().then(uri => {
+      this._setcaptchaImage(uri);
+    });
+  },
+};
+
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    gRequestBridgeDialog.init();
+    window.addEventListener(
+      "unload",
+      () => {
+        gRequestBridgeDialog.uninit();
+      },
+      { once: true }
+    );
+  },
+  { once: true }
+);