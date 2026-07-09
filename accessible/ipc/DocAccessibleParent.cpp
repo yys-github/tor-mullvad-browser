1d329b4b21105f94bb06edbaf626e8d245c7bb86
Eitan Isaacson <eitan@monotonous.org>
2026-07-09 11:54:05 +0000

    Bug 2045392 - Don't allow attaching children to outerdoc.  a=dmeehan DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D305621

--- accessible/ipc/DocAccessibleParent.cpp
+++ accessible/ipc/DocAccessibleParent.cpp
@@ -119,10 +119,6 @@ mozilla::ipc::IPCResult DocAccessibleParent::ProcessShowEvent(
 #endif
     }
 
-    if (parent->IsOuterDoc()) {
-      return IPC_FAIL(this, "Cannot attach non-doc to OuterDoc");
-    }
-
     lastParent = parent;
     lastParentID = accData.ParentID();
 
@@ -276,6 +272,21 @@ RemoteAccessible* DocAccessibleParent::CreateAcc(
 bool DocAccessibleParent::AttachChild(RemoteAccessible* aParent,
                                       uint32_t aIndex,
                                       RemoteAccessible* aChild) {
+  if (!aParent || !aChild) {
+    MOZ_ASSERT_UNREACHABLE("Null parent or child");
+    return false;
+  }
+
+  if (aParent->IsOuterDoc()) {
+    MOZ_ASSERT_UNREACHABLE("Cannot attach non-doc to OuterDoc");
+    return false;
+  }
+
+  if (aIndex > aParent->ChildCount()) {
+    MOZ_ASSERT_UNREACHABLE("Invalid index for attached child");
+    return false;
+  }
+
   if (aChild->RemoteParent()) {
     MOZ_ASSERT_UNREACHABLE(
         "Attempt to attach child which already has a parent!");
