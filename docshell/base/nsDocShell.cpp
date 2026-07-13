2d3195015292e026aa3618d59024665a1bdb8f82
Vincent Hilla <vhilla@mozilla.com>
2026-07-13 08:28:36 +0000

    Bug 2048851 - Clear principalToInherit in LoadPageAsViewSource.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D309163
    
    Differential Revision: https://phabricator.services.mozilla.com/D311786

--- docshell/base/nsDocShell.cpp
+++ docshell/base/nsDocShell.cpp
@@ -4326,6 +4326,8 @@ nsDocShell::LoadPageAsViewSource(nsIDocShell* aOtherDocShell,
   // is only exposed to system code.  The triggering principal for this load
   // should be the system principal.
   loadState->SetTriggeringPrincipal(nsContentUtils::GetSystemPrincipal());
+  loadState->SetPrincipalToInherit(nullptr);
+  loadState->SetPartitionedPrincipalToInherit(nullptr);
   loadState->SetOriginalURI(nullptr);
   loadState->SetResultPrincipalURI(nullptr);
 
