1b3e57357c8a04fb4e1bb55ef2f6117e60ee35e4
Dan Ballard <dan@mindstab.net>
2026-07-22 11:58:02 -0700

    fixup! [android] Disable features and functionality
    TB 44758: Disable email mask feature

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/utils/Settings.kt
@@ -2630,7 +2630,7 @@ class Settings(
      */
     var isEmailMaskSuggestionEnabled by booleanPreference(
         key = appContext.getPreferenceKey(R.string.pref_key_email_mask_suggestion),
-        default = true,
+        default = false,
     )
 
     /**
@@ -2638,7 +2638,7 @@ class Settings(
      */
     var shouldShowEmailMaskCfr by booleanPreference(
         key = appContext.getPreferenceKey(R.string.pref_key_should_show_email_mask_cfr),
-        default = true,
+        default = false,
     )
 
     /**
