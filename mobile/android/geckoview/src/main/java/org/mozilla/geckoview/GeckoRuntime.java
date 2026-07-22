Kevinisagirl21/tor-browser

TB 42247: Android helpers for the TorProvider
GeckoView is missing some API we use on desktop for the integration
with the tor daemon, such as subprocess.
Therefore, we need to implement them in Java and plumb the data
back and forth between JS and Java.

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -238,6 +238,8 @@ public final class GeckoRuntime implements Parcelable {
   private final ProfilerController mProfilerController;
   private final GeckoScreenChangeListener mScreenChangeListener;
 
+  private TorIntegrationAndroid mTorIntegration;
+
   private GeckoRuntime() {
     mWebExtensionController = new WebExtensionController(this);
     mContentBlockingController = new ContentBlockingController();
@@ -487,6 +489,8 @@ public final class GeckoRuntime implements Parcelable {
       mScreenChangeListener.enable();
     }
 
+    mTorIntegration = new TorIntegrationAndroid(context);
+
     mProfilerController.addMarker(
         "GeckoView Initialization START", mProfilerController.getProfilerTime());
     return true;
@@ -595,6 +599,10 @@ public final class GeckoRuntime implements Parcelable {
 
     if (mScreenChangeListener != null) {
       mScreenChangeListener.disable();
+    }
+
+    if (mTorIntegration != null) {
+      mTorIntegration.shutdown();
     }
 
     GeckoThread.forceQuit();
@@ -995,6 +1003,14 @@ public final class GeckoRuntime implements Parcelable {
     }
 
     return mPushController;
+  }
+
+  /**
+   * Get the Tor integration controller for this runtime.
+   */
+  @UiThread
+  public @NonNull TorIntegrationAndroid getTorIntegrationController() {
+    return mTorIntegration;
   }
 
   /**

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -2601,6 +2601,24 @@ public class GeckoSession {
   }
 
   /**
+   * Try to get last circuit used in this session, if possible.
+   *
+   * @return The circuit information as a {@link GeckoResult} object.
+   */
+  @AnyThread
+  public @NonNull GeckoResult<GeckoBundle> getTorCircuit() {
+    return mEventDispatcher.queryBundle("GeckoView:GetTorCircuit");
+  }
+
+  /**
+   * Change the circuit for this session.
+   */
+  @UiThread
+  public void newTorCircuit() {
+    mEventDispatcher.dispatch("GeckoView:NewTorCircuit", null);
+  }
+
+  /**
    * Set this GeckoSession as active or inactive, which represents if the session is currently
    * visible or not. Setting a GeckoSession to inactive will significantly reduce its memory
    * footprint, but should only be done if the GeckoSession is not currently visible. Note that a

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/androidlegacysettings/TorLegacyAndroidSettings.java
@@ -1,0 +1,74 @@
+package org.mozilla.geckoview.androidlegacysettings;
+
+import org.mozilla.geckoview.TorSettings;
+
+public class TorLegacyAndroidSettings {
+
+  private static String PREF_USE_MOZ_PREFS = "tor_use_moz_prefs";
+
+  public static boolean unmigrated() {
+    return !Prefs.getBoolean(PREF_USE_MOZ_PREFS, false);
+  }
+
+  public static void setUnmigrated() {
+    Prefs.putBoolean(PREF_USE_MOZ_PREFS, false);
+  }
+
+  public static void setMigrated() {
+    Prefs.putBoolean(PREF_USE_MOZ_PREFS, true);
+  }
+
+  public static TorSettings loadTorSettings() {
+    TorSettings settings = new TorSettings();
+
+    // always true, tor is enabled in TB
+    settings.enabled = true;
+
+    // firefox-android disconnected quick start a while ago so it's untracked
+    settings.quickstart = false;
+
+    settings.bridgesEnabled = Prefs.bridgesEnabled();
+
+    // tor-android-service CustomTorInstaller.java
+    /*
+       BridgesList is an overloaded field, which can cause some confusion.
+       The list can be:
+         1) a filter like obfs4, meek, or snowflake OR
+         2) it can be a custom bridge
+       For (1), we just pass back all bridges, the filter will occur
+         elsewhere in the library.
+       For (2) we return the bridge list as a raw stream.
+       If length is greater than 9, then we know this is a custom bridge
+    */
+    String userDefinedBridgeList = Prefs.getBridgesList();
+    boolean userDefinedBridge = userDefinedBridgeList.length() > 9;
+    // Terrible hack. Must keep in sync with topl::addBridgesFromResources.
+    if (!userDefinedBridge) {
+      settings.bridgesSource = TorSettings.BridgeSource.BuiltIn;
+      switch (userDefinedBridgeList) {
+        case "obfs4":
+        case "snowflake":
+          settings.bridgesBuiltinType =
+              TorSettings.BridgeBuiltinType.fromString(userDefinedBridgeList);
+          break;
+        case "meek":
+          settings.bridgesBuiltinType = TorSettings.BridgeBuiltinType.MeekAzure;
+          break;
+        default:
+          settings.bridgesSource = TorSettings.BridgeSource.Invalid;
+          break;
+      }
+    } else {
+      settings.bridgesSource = TorSettings.BridgeSource.UserProvided; // user provided
+      settings.bridgeBridgeStrings = userDefinedBridgeList.split("\r\n");
+    }
+
+    // Tor Browser Android doesn't take proxy and firewall settings
+    settings.proxyEnabled = false;
+
+    settings.firewallEnabled = false;
+    settings.firewallAllowedPorts = new int[0];
+
+    return settings;
+  }
+}

yys-github/tor-mullvad-browser

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoRuntime.java
@@ -270,6 +270,8 @@ public final class GeckoRuntime implements Parcelable {
   private final CrashPullController.CrashPullProxy mCrashPullProxy;
   private final GeckoScreenChangeListener mScreenChangeListener;
 
+  private TorAndroidIntegration mTorIntegration;
+
   private GeckoRuntime() {
     mWebExtensionController = new WebExtensionController(this);
     mContentBlockingController = new ContentBlockingController();
@@ -593,6 +595,8 @@ public final class GeckoRuntime implements Parcelable {
     // Warm up window context of default display.
     GeckoAppShell.maybeInitScreen();
 
+    mTorIntegration = new TorAndroidIntegration(context);
+
     ProfilerController.addMarker(
         "GeckoView Initialization START", ProfilerController.getProfilerTime());
     return true;
@@ -706,6 +710,10 @@ public final class GeckoRuntime implements Parcelable {
 
     if (mScreenChangeListener != null) {
       mScreenChangeListener.disable();
+    }
+
+    if (mTorIntegration != null) {
+      mTorIntegration.shutdown();
     }
 
     GeckoThread.forceQuit();
@@ -1196,6 +1204,14 @@ public final class GeckoRuntime implements Parcelable {
   public void notifyTelemetryPrefChanged(final boolean isEnabled) {
     GeckoAppShell.notifyObservers(
         "mobile-telemetry-pref-changed", isEnabled ? "enabled" : "disabled");
+  }
+
+  /**
+   * Get the Tor integration controller for this runtime.
+   */
+  @UiThread
+  public @NonNull TorAndroidIntegration getTorIntegrationController() {
+    return mTorIntegration;
   }
 
   /**

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/GeckoSession.java
@@ -2823,6 +2823,24 @@ public class GeckoSession {
   }
 
   /**
+   * Try to get last circuit used in this session, if possible.
+   *
+   * @return The circuit information as a {@link GeckoResult} object.
+   */
+  @AnyThread
+  public @NonNull GeckoResult<GeckoBundle> getTorCircuits() {
+    return mEventDispatcher.queryBundle("GeckoView:GetTorCircuits");
+  }
+
+  /**
+   * Change the circuit for this session.
+   */
+  @UiThread
+  public void newTorCircuit() {
+    mEventDispatcher.dispatch("GeckoView:NewTorCircuit", null);
+  }
+
+  /**
    * Set this GeckoSession as active or inactive, which represents if the session is currently
    * visible or not. Setting a GeckoSession to inactive will significantly reduce its memory
    * footprint, but should only be done if the GeckoSession is not currently visible. Note that a

mobile/android/geckoview/src/main/java/org/mozilla/geckoview/androidlegacysettings/TorLegacyAndroidSettings.java
@@ -1,0 +1,71 @@
+package org.mozilla.geckoview.androidlegacysettings;
+
+import org.mozilla.geckoview.TorSettings;
+
+public class TorLegacyAndroidSettings {
+
+  private static String PREF_USE_MOZ_PREFS = "tor_use_moz_prefs";
+
+  public static boolean unmigrated() {
+    return !Prefs.getBoolean(PREF_USE_MOZ_PREFS, false);
+  }
+
+  public static void setUnmigrated() {
+    Prefs.putBoolean(PREF_USE_MOZ_PREFS, false);
+  }
+
+  public static void setMigrated() {
+    Prefs.putBoolean(PREF_USE_MOZ_PREFS, true);
+  }
+
+  public static TorSettings loadTorSettings() {
+    TorSettings settings = new TorSettings();
+
+    // always true, tor is enabled in TB
+    settings.enabled = true;
+
+    settings.bridgesEnabled = Prefs.bridgesEnabled();
+
+    // tor-android-service CustomTorInstaller.java
+    /*
+       BridgesList is an overloaded field, which can cause some confusion.
+       The list can be:
+         1) a filter like obfs4, meek, or snowflake OR
+         2) it can be a custom bridge
+       For (1), we just pass back all bridges, the filter will occur
+         elsewhere in the library.
+       For (2) we return the bridge list as a raw stream.
+       If length is greater than 9, then we know this is a custom bridge
+    */
+    String userDefinedBridgeList = Prefs.getBridgesList();
+    boolean userDefinedBridge = userDefinedBridgeList.length() > 9;
+    // Terrible hack. Must keep in sync with topl::addBridgesFromResources.
+    if (!userDefinedBridge) {
+      settings.bridgesSource = TorSettings.BridgeSource.BuiltIn;
+      switch (userDefinedBridgeList) {
+        case "obfs4":
+        case "snowflake":
+          settings.bridgesBuiltinType =
+              TorSettings.BridgeBuiltinType.fromString(userDefinedBridgeList);
+          break;
+        case "meek":
+          settings.bridgesBuiltinType = TorSettings.BridgeBuiltinType.Meek;
+          break;
+        default:
+          settings.bridgesSource = TorSettings.BridgeSource.Invalid;
+          break;
+      }
+    } else {
+      settings.bridgesSource = TorSettings.BridgeSource.UserProvided; // user provided
+      settings.bridgeBridgeStrings = userDefinedBridgeList.split("\r\n");
+    }
+
+    // Tor Browser Android doesn't take proxy and firewall settings
+    settings.proxyEnabled = false;
+
+    settings.firewallEnabled = false;
+    settings.firewallAllowedPorts = new int[0];
+
+    return settings;
+  }
+}