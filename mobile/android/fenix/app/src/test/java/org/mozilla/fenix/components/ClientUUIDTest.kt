5289c51438c0d648309da97a56a19ad44423c686
Beatriz Rizental <bea@torproject.org>
2026-07-27 16:39:34 -0300

    fixup! [android] Disable features and functionality
    Bug 45134: Return deterministic NIL value for ClientUUID

--- mobile/android/fenix/app/src/test/java/org/mozilla/fenix/components/ClientUUIDTest.kt
+++ mobile/android/fenix/app/src/test/java/org/mozilla/fenix/components/ClientUUIDTest.kt
@@ -7,9 +7,25 @@ package org.mozilla.fenix.components
 import mozilla.components.lib.llm.mlpa.service.UserId
 import mozilla.components.support.test.fakes.android.FakeSharedPreferences
 import org.junit.Assert.assertEquals
+import org.junit.Ignore
 import org.junit.Test
 
 class ClientUUIDTest {
+    @Test
+    fun `that the client uuid is always the nil uuid, regardless of generateUUID`() {
+        val prefs = FakeSharedPreferences()
+        val nilUuid = UserId("00000000-0000-0000-0000-000000000000")
+
+        val first = PrefsBackedClientUUID({ prefs }, generateUUID = { "my-generated-uuid" })
+        assertEquals(nilUuid, first.getUserId())
+        // idempotency check
+        assertEquals(nilUuid, first.getUserId())
+
+        val second = PrefsBackedClientUUID({ prefs }, generateUUID = { "another-generated-uuid" })
+        assertEquals(nilUuid, second.getUserId())
+    }
+
+    @Ignore("tor-browser#45134: PrefsBackedClientUUID always returns the nil uuid, see the test above")
     @Test
     fun `that a client uuid will only be generated the first time`() {
         val prefs = FakeSharedPreferences()
@@ -25,6 +41,7 @@ class ClientUUIDTest {
         assertEquals(UserId("my-generated-uuid"), second.getUserId())
     }
 
+    @Ignore("tor-browser#45134: PrefsBackedClientUUID always returns the nil uuid, so the hash is no longer derived from generateUUID's output")
     @Test
     fun `that generateHash uses the provided hasher`() {
         val prefs = FakeSharedPreferences()
