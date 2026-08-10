d7f0fc720fd95e713a9244b9d4fd4c3f6ba8058a
Henry Wilkes <henry@torproject.org>
2026-08-10 17:27:40 +0100

    fixup! TB 30237: Add v3 onion services client authentication prompt
    TB 45200: Include onion authentication in the new password settings.

--- browser/components/preferences/config/passwords-autofill.mjs
+++ browser/components/preferences/config/passwords-autofill.mjs
@@ -822,7 +822,7 @@ SettingGroupManager.registerGroups({
       {
         id: "onionSiteSavedKeys",
         control: "moz-box-button",
-        l10nId: "onion-site-authentication-saved-keys-button",
+        l10nId: "onion-site-authentication-saved-keys-button2",
       },
     ],
   },
