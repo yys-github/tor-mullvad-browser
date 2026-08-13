82898109eabe2623f13881efc0cdca8899a274fd
Henry Wilkes <henry@torproject.org>
2026-08-13 13:00:05 +0100

    BB 45211: Settings UI for protections from third party applications.

--- browser/components/preferences/preferences.js
+++ browser/components/preferences/preferences.js
@@ -274,6 +274,7 @@ const CONFIG_PANES = Object.freeze({
       "networkProxy",
       "privacyPanel",
       "browsingProtection",
+      "protectionsFromThirdParty",
       "certificates",
     ],
     replaces: "privacy",
