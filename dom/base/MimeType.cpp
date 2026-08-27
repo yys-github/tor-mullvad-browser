00522c00e5c6b6a3ed7d72b49ec1757868345588
Valentin Gosu <valentin.gosu@gmail.com>
2026-08-27 18:31:14 +0000

    Bug 2063814 - Handle \ correctly in SplitMimetype  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D319097
    
    Differential Revision: https://phabricator.services.mozilla.com/D320017

--- dom/base/MimeType.cpp
+++ dom/base/MimeType.cpp
@@ -253,7 +253,13 @@ TMimeType<char_type>::SplitMimetype(const nsTSubstring<char_type>& aMimeType) {
   for (size_t i = 0; i < aMimeType.Length(); i++) {
     char_type c = aMimeType[i];
 
-    if (c == '\"' && (i == 0 || aMimeType[i - 1] != '\\')) {
+    // https://fetch.spec.whatwg.org/#collect-an-http-quoted-string : a
+    // backslash only escapes inside a quoted string, and it consumes the code
+    // point that follows it, so an escaped backslash does not escape the next
+    // character.
+    if (inQuotes && c == '\\') {
+      ++i;
+    } else if (c == '"') {
       inQuotes = !inQuotes;
     } else if (c == ',' && !inQuotes) {
       mimeTypeParts.AppendElement(Substring(aMimeType, start, i - start));
