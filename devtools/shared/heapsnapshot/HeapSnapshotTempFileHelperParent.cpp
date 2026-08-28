58a6cb7fcfd2a9fe69773ebcb2aeaec2358fa7d9
Alexandre Poirot <poirot.alex@gmail.com>
2026-08-28 00:00:32 +0000

    Bug 2062551 - Allow heapsnapshot records when DevTools are active.  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D318392
    
    Differential Revision: https://phabricator.services.mozilla.com/D321173

--- devtools/shared/heapsnapshot/HeapSnapshotTempFileHelperParent.cpp
+++ devtools/shared/heapsnapshot/HeapSnapshotTempFileHelperParent.cpp
@@ -6,6 +6,7 @@
 
 #include "mozilla/devtools/HeapSnapshot.h"
 #include "mozilla/devtools/HeapSnapshotTempFileHelperParent.h"
+#include "mozilla/dom/ChromeUtils.h"
 #include "mozilla/ErrorResult.h"
 #include "private/pprio.h"
 
@@ -23,6 +24,9 @@ static bool openFileFailure(ErrorResult& rv,
 mozilla::ipc::IPCResult
 HeapSnapshotTempFileHelperParent::RecvOpenHeapSnapshotTempFile(
     OpenHeapSnapshotTempFileResponse* outResponse) {
+  if (!dom::ChromeUtils::IsDevToolsOpened()) {
+    return IPC_FAIL_NO_REASON(this);
+  }
   auto start = TimeStamp::Now();
   ErrorResult rv;
   nsAutoString filePath;
