Kevinisagirl21/tor-browser

TB 30237: Add v3 onion services client authentication prompt
When Tor informs the browser that client authentication is needed,
temporarily load about:blank instead of about:neterror and prompt
for the user's key.

If a correctly formatted key is entered, use Tor's ONION_CLIENT_AUTH_ADD
control port command to add the key (via Torbutton's control port
module) and reload the page.

If the user cancels the prompt, display the standard about:neterror
"Unable to connect" page. This requires a small change to
browser/actors/NetErrorChild.jsm to account for the fact that the
docShell no longer has the failedChannel information. The failedChannel
is used to extract TLS-related error info, which is not applicable
in the case of a canceled .onion authentication prompt.

Add a leaveOpen option to PopupNotifications.show so we can display
error messages within the popup notification doorhanger without
closing the prompt.

Add support for onion services strings to the TorStrings module.

Add support for Tor extended SOCKS errors (Tor proposal 304) to the
socket transport and SOCKS layers. Improved display of all of these
errors will be implemented as part of bug 30025.

Also fixes bug 19757:
 Add a "Remember this key" checkbox to the client auth prompt.

 Add an "Onion Services Authentication" section within the
 about:preferences "Privacy & Security section" to allow
 viewing and removal of v3 onion client auth keys that have
 been stored on disk.

Also fixes bug 19251: use enhanced error pages for onion service errors.

toolkit/content/aboutNetError.mjs
@@ -290,6 +290,92 @@ function recordTRREventTelemetry(
   }
 }
 
+/**
+ * Initialize the onion error page.
+ *
+ * @return {boolean} Whether the page was initialized as an onion error page.
+ */
+function initOnionError() {
+  const docTitle = document.querySelector("title");
+
+  if (getCSSClass() === "onionAuthPrompt") {
+    // Only showing the authorization prompt. The page will be blank.
+    document.l10n.setAttributes(docTitle, "onion-neterror-authorization-title");
+    return true;
+  }
+
+  const onionErrors = {
+    // Tor SOCKS error 0xF0:
+    "onionServices.descNotFound": {
+      headerId: "onion-neterror-not-found-header",
+      descriptionId: "onion-neterror-not-found-description",
+    },
+    // Tor SOCKS error 0xF1:
+    "onionServices.descInvalid": {
+      headerId: "onion-neterror-unreachable-header",
+      descriptionId: "onion-neterror-unreachable-description",
+    },
+    // Tor SOCKS error 0xF2:
+    "onionServices.introFailed": {
+      headerId: "onion-neterror-disconnected-header",
+      descriptionId: "onion-neterror-disconnected-description",
+    },
+    // Tor SOCKS error 0xF3:
+    "onionServices.rendezvousFailed": {
+      headerId: "onion-neterror-connection-failed-header",
+      descriptionId: "onion-neterror-connection-failed-description",
+    },
+    // Tor SOCKS error 0xF4:
+    "onionServices.clientAuthMissing": {
+      headerId: "onion-neterror-missing-authentication-header",
+      descriptionId: "onion-neterror-missing-authentication-description",
+    },
+    // Tor SOCKS error 0xF5:
+    "onionServices.clientAuthIncorrect": {
+      headerId: "onion-neterror-incorrect-authentication-header",
+      descriptionId: "onion-neterror-incorrect-authetication-description",
+    },
+    // Tor SOCKS error 0xF6:
+    "onionServices.badAddress": {
+      headerId: "onion-neterror-invalid-address-header",
+      descriptionId: "onion-neterror-invalid-address-description",
+    },
+    // Tor SOCKS error 0xF7:
+    "onionServices.introTimedOut": {
+      headerId: "onion-neterror-timed-out-header",
+      descriptionId: "onion-neterror-timed-out-description",
+    },
+  };
+
+  if (!Object.hasOwn(onionErrors, gErrorCode)) {
+    return false;
+  }
+
+  document.body.classList.add("onion-error");
+
+  document.l10n.setAttributes(docTitle, "onion-neterror-page-title");
+  document.l10n.setAttributes(
+    document.querySelector(".title-text"),
+    onionErrors[gErrorCode].headerId
+  );
+  document.l10n.setAttributes(
+    document.getElementById("errorShortDesc"),
+    onionErrors[gErrorCode].descriptionId
+  );
+
+  const tryAgain = document.getElementById("netErrorButtonContainer");
+  tryAgain.hidden = false;
+
+  const learnMore = document.getElementById("learnMoreContainer");
+  learnMore.hidden = false;
+  const learnMoreLink = document.getElementById("learnMoreLink");
+  learnMoreLink.href = "about:manual#onion-services";
+
+  setFocus("#netErrorButtonContainer > .try-again");
+
+  return true;
+}
+
 async function initPage() {
   // We show an offline support page in case of a system-wide error,
   // when a user cannot connect to the internet and access the SUMO website.
@@ -358,6 +444,10 @@ async function initPage() {
   }
 
   document.body.classList.add("neterror");
+
+  if (initOnionError()) {
+    return;
+  }
 
   let longDesc = document.getElementById("errorLongDesc");
   const tryAgain = document.getElementById("netErrorButtonContainer");

yys-github/tor-mullvad-browser

toolkit/content/aboutNetError.mjs
@@ -293,6 +293,92 @@ function setResponseStatus(shortDesc) {
   }
 }
 
+/**
+ * Initialize the onion error page.
+ *
+ * @return {boolean} Whether the page was initialized as an onion error page.
+ */
+function initOnionError() {
+  const docTitle = document.querySelector("title");
+
+  if (getCSSClass() === "onionAuthPrompt") {
+    // Only showing the authorization prompt. The page will be blank.
+    document.l10n.setAttributes(docTitle, "onion-neterror-authorization-title");
+    return true;
+  }
+
+  const onionErrors = {
+    // Tor SOCKS error 0xF0:
+    "onionServices.descNotFound": {
+      headerId: "onion-neterror-not-found-header",
+      descriptionId: "onion-neterror-not-found-description",
+    },
+    // Tor SOCKS error 0xF1:
+    "onionServices.descInvalid": {
+      headerId: "onion-neterror-unreachable-header",
+      descriptionId: "onion-neterror-unreachable-description",
+    },
+    // Tor SOCKS error 0xF2:
+    "onionServices.introFailed": {
+      headerId: "onion-neterror-disconnected-header",
+      descriptionId: "onion-neterror-disconnected-description",
+    },
+    // Tor SOCKS error 0xF3:
+    "onionServices.rendezvousFailed": {
+      headerId: "onion-neterror-connection-failed-header",
+      descriptionId: "onion-neterror-connection-failed-description",
+    },
+    // Tor SOCKS error 0xF4:
+    "onionServices.clientAuthMissing": {
+      headerId: "onion-neterror-missing-authentication-header",
+      descriptionId: "onion-neterror-missing-authentication-description",
+    },
+    // Tor SOCKS error 0xF5:
+    "onionServices.clientAuthIncorrect": {
+      headerId: "onion-neterror-incorrect-authentication-header",
+      descriptionId: "onion-neterror-incorrect-authetication-description",
+    },
+    // Tor SOCKS error 0xF6:
+    "onionServices.badAddress": {
+      headerId: "onion-neterror-invalid-address-header",
+      descriptionId: "onion-neterror-invalid-address-description",
+    },
+    // Tor SOCKS error 0xF7:
+    "onionServices.introTimedOut": {
+      headerId: "onion-neterror-timed-out-header",
+      descriptionId: "onion-neterror-timed-out-description",
+    },
+  };
+
+  if (!Object.hasOwn(onionErrors, gErrorCode)) {
+    return false;
+  }
+
+  document.body.classList.add("onion-error");
+
+  document.l10n.setAttributes(docTitle, "onion-neterror-page-title");
+  document.l10n.setAttributes(
+    document.querySelector(".title-text"),
+    onionErrors[gErrorCode].headerId
+  );
+  document.l10n.setAttributes(
+    document.getElementById("errorShortDesc"),
+    onionErrors[gErrorCode].descriptionId
+  );
+
+  const tryAgain = document.getElementById("netErrorButtonContainer");
+  tryAgain.hidden = false;
+
+  const learnMore = document.getElementById("learnMoreContainer");
+  learnMore.hidden = false;
+  const learnMoreLink = document.getElementById("learnMoreLink");
+  learnMoreLink.href = "about:manual#features__onion-services";
+
+  setFocus("#netErrorButtonContainer > .try-again");
+
+  return true;
+}
+
 // Returns pageTitleId, bodyTitle, bodyTitleId, and longDesc as an object
 function initTitleAndBodyIds(baseURL, isTRROnlyFailure) {
   let bodyTitle = document.querySelector(".title-text");
@@ -471,6 +557,10 @@ function initPage() {
         netErrorInfo
       );
     } catch (ex) {}
+  }
+
+  if (initOnionError()) {
+    return;
   }
 
   const tryAgain = document.getElementById("netErrorButtonContainer");