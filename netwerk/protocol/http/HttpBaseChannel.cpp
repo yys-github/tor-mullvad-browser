0497d2bb55532f1d56d1a7f9e72b685e6db95f5e
Andreas Farre <farre@mozilla.com>
2026-08-31 16:31:42 +0000

    Bug 2029482 - Don't make ORB exemptions for DevTools.  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D319804
    
    Differential Revision: https://phabricator.services.mozilla.com/D322302

--- netwerk/protocol/http/HttpBaseChannel.cpp
+++ netwerk/protocol/http/HttpBaseChannel.cpp
@@ -3316,13 +3316,6 @@ bool HttpBaseChannel::ShouldBlockOpaqueResponse() const {
     return false;
   }
 
-  bool isInDevToolsContext;
-  mLoadInfo->GetIsInDevToolsContext(&isInDevToolsContext);
-  if (isInDevToolsContext) {
-    LOGORB("No block: Request created by devtools");
-    return false;
-  }
-
   return true;
 }
 
