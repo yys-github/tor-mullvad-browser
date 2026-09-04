66987b6508da727c7c1c37c8dc4c4ab45846a63b
clairehurst <clairehurst@torproject.org>
2026-09-03 19:09:45 -0600

    fixup! [android] Disable features and functionality
    TB 45252: Remove the change wallpaper menu item

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/MainMenu.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/menu/compose/MainMenu.kt
@@ -330,11 +330,6 @@ fun MainMenu(
             )
 
             if (accessPoint == MenuAccessPoint.Home) {
-                MenuItem(
-                    label = stringResource(id = R.string.browser_menu_change_wallpaper),
-                    beforeIconPainter = painterResource(id = iconsR.drawable.mozac_ic_wallpaper_24),
-                    onClick = onWallpaperButtonClick,
-                )
             }
 
             MenuItem(
