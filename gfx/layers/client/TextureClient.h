ca2da456d746c357ef0f382c06e46e2184e5c923
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2026-09-04 19:25:56 +0000

    Bug 2068438  a=dmeehan DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D323156
    
    Differential Revision: https://phabricator.services.mozilla.com/D323435

--- gfx/layers/client/TextureClient.h
+++ gfx/layers/client/TextureClient.h
@@ -621,6 +621,13 @@ class TextureClient : public AtomicRefCountedWithFinalize<TextureClient> {
   TextureData* GetInternalData() { return mData; }
   const TextureData* GetInternalData() const { return mData; }
 
+  /**
+   * Serializes the underlying TextureData into aDescriptor. Returns false if
+   * the TextureData has already been destroyed. Safe to call from any thread
+   * concurrently with Destroy().
+   */
+  bool ToSurfaceDescriptor(SurfaceDescriptor& aDescriptor);
+
   uint64_t GetSerial() const { return mSerial; }
   void GetSurfaceDescriptorRemoteDecoder(
       SurfaceDescriptorRemoteDecoder* aOutDesc);
@@ -719,16 +726,6 @@ class TextureClient : public AtomicRefCountedWithFinalize<TextureClient> {
   friend class AtomicRefCountedWithFinalize<TextureClient>;
 
  protected:
-  /**
-   * Should only be called *once* per texture, in TextureClient::InitIPDLActor.
-   * Some texture implementations rely on the fact that the descriptor will be
-   * deserialized.
-   * Calling ToSurfaceDescriptor again after it has already returned true,
-   * or never constructing a TextureHost with aDescriptor may result in a memory
-   * leak (see TextureClientD3D9 for example).
-   */
-  bool ToSurfaceDescriptor(SurfaceDescriptor& aDescriptor);
-
   void LockActor() const;
   void UnlockActor() const;
 
