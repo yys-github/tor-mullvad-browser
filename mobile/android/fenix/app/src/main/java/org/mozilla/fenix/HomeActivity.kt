c589157b5cd43356ec6a52a64362fb3a60f94e7a
Dan Ballard <dan@mindstab.net>
2026-09-09 19:51:00 -0700

    fixup! TB 34378: [android] Port external helper app prompting
    TB40627: Fix android share by making new intents with new request codes and not caching the dialog

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/HomeActivity.kt
@@ -438,8 +438,6 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, Crash
         }
     }
 
-    private var dialog: RedirectDialogFragment? = null
-
     private val providerStoppedViewModel: ProviderStoppedViewModel by viewModels()
 
     private val urlQuickLoadViewModel: UrlQuickLoadViewModel by viewModels()
@@ -991,15 +989,9 @@ open class HomeActivity : LocaleAwareAppCompatActivity(), NavHostActivity, Crash
 
     // Copied from mozac AppLinksFeature.kt
     internal fun getOrCreateDialog(): RedirectDialogFragment {
-        val existingDialog = dialog
-        if (existingDialog != null) {
-            return existingDialog
-        }
-
         SimpleRedirectDialogFragment.newInstance(
                 getString(appLinksR.string.mozac_feature_applinks_normal_confirm_dialog_title),
             ).also {
-                dialog = it
                 return it
             }
     }
