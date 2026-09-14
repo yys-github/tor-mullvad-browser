Kevinisagirl21/tor-browser

TB 40015: [android] Port padlock states for .onion services
Originally, android-components#40015.

mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/browser/toolbar/display/DisplayToolbar.kt
@@ -488,6 +488,7 @@ class DisplayToolbar internal constructor(
         @ColorInt val color = when (siteSecurity) {
             Toolbar.SiteSecurity.INSECURE -> colors.securityIconInsecure
             Toolbar.SiteSecurity.SECURE -> colors.securityIconSecure
+            Toolbar.SiteSecurity.ONION -> colors.securityIconSecure
         }
         if (color == Color.TRANSPARENT && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             views.securityIndicator.clearColorFilter()

mobile/android/android-components/components/feature/toolbar/src/main/java/mozilla/components/feature/toolbar/ToolbarPresenter.kt
@@ -18,6 +18,7 @@ import mozilla.components.concept.toolbar.Toolbar.Highlight
 import mozilla.components.concept.toolbar.Toolbar.SiteTrackingProtection
 import mozilla.components.feature.toolbar.internal.URLRenderer
 import mozilla.components.lib.state.ext.flowScoped
+import mozilla.components.support.ktx.kotlin.isOnionUrl
 
 /**
  * Presenter implementation for a toolbar implementation in order to update the toolbar whenever
@@ -69,7 +70,11 @@ class ToolbarPresenter(
             toolbar.displayProgress(tab.content.progress)
 
             toolbar.siteSecure = if (tab.content.securityInfo.secure) {
-                Toolbar.SiteSecurity.SECURE
+                if (tab.content.url.isOnionUrl()) {
+                    Toolbar.SiteSecurity.ONION
+                } else {
+                    Toolbar.SiteSecurity.SECURE
+                }
             } else {
                 Toolbar.SiteSecurity.INSECURE
             }

yys-github/tor-mullvad-browser

mobile/android/android-components/components/browser/toolbar/src/main/java/mozilla/components/browser/toolbar/display/DisplayToolbar.kt
@@ -540,6 +540,7 @@ class DisplayToolbar internal constructor(
             Toolbar.SiteInfo.INSECURE -> colors.siteInfoIconInsecure
             Toolbar.SiteInfo.SECURE -> colors.siteInfoIconSecure
             Toolbar.SiteInfo.LOCAL_PDF -> colors.siteInfoIconLocalPdf
+            Toolbar.SiteInfo.ONION -> colors.siteInfoIconSecure
         }
         if (color == Color.TRANSPARENT) {
             views.siteInfoIndicator.clearColorFilter()

mobile/android/android-components/components/feature/toolbar/src/main/java/mozilla/components/feature/toolbar/ToolbarPresenter.kt
@@ -21,6 +21,7 @@ import mozilla.components.concept.toolbar.Toolbar.SiteTrackingProtection
 import mozilla.components.feature.toolbar.internal.URLRenderer
 import mozilla.components.lib.state.ext.flowScoped
 import mozilla.components.support.ktx.kotlin.isContentUrl
+import mozilla.components.support.ktx.kotlin.isOnionUrl
 
 /**
  * Presenter implementation for a toolbar implementation in order to update the toolbar whenever
@@ -75,7 +76,11 @@ class ToolbarPresenter(
             toolbar.siteInfo = if (tab.content.url.isContentUrl()) {
                 Toolbar.SiteInfo.LOCAL_PDF
             } else if (tab.content.securityInfo.isSecure) {
-                Toolbar.SiteInfo.SECURE
+                if (tab.content.url.isOnionUrl()) {
+                    Toolbar.SiteInfo.ONION
+                } else {
+                    Toolbar.SiteInfo.SECURE
+                }
             } else {
                 Toolbar.SiteInfo.INSECURE
             }