757a52aea56e84600e9505038a1d7bd9bf332566
Henry Wilkes <henry@torproject.org>
2026-09-16 11:53:26 +0100

    fixup! BB 41930: Remove the UI to customize accept_languages.
    TB 45177: Remove websiteSpoofEnglishControl.

--- browser/components/preferences/main.js
+++ browser/components/preferences/main.js
@@ -814,7 +814,6 @@ var gMainPane = {
     initSettingGroup("fonts");
     initSettingGroup("browserLanguage");
     initSettingGroup("websiteLanguage");
-    initSettingGroup("websiteSpoofEnglishControl");
     initSettingGroup("browsing");
     initSettingGroup("keyboardAndScrolling");
     initSettingGroup("motionAndLink");
