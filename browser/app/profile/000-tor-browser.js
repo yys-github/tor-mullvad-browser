e566e082446ff9bee607c2a69a457cae61e2b454
Beatriz Rizental <bea@torproject.org>
2026-09-23 15:53:29 -0300

    fixup! TB 40562: Added Tor Browser preferences to 000-tor-browser.js
    Bug 45237: Fix pref typo

--- browser/app/profile/000-tor-browser.js
+++ browser/app/profile/000-tor-browser.js
@@ -143,7 +143,7 @@ pref("extensions.torlauncher.bridgedb_targets", "https://1723079976.rsc.cdn77.or
 pref("extensions.torlauncher.moat_service", "https://bridges.torproject.org/moat");
 
 // tor-browser#45237: Disable multiple profiles feature
-pref("browser.profiles.enable", false);
+pref("browser.profiles.enabled", false);
 
 // Log levels
 pref("browser.new_identity.log_level", "Info");
